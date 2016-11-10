284346da1ec043f2fa47420f282ebc19be31376c
Lars Lange <lars.lange@student.hpi.de>
2016-11-10 14:10:26 +0100

    minor changes

--- src/modules/settings/components/privacy.jsx
+++ src/modules/settings/components/privacy.jsx
@@ -14,11 +14,12 @@ class SectionPrivacy extends React.Component {
 				<div className="container-fluid">
 					<div className="row">
 						<div className="col-sm-12 no-padding">
-							<h5>Benachrichtigungen</h5>
+							<h5>Datenschutz und Sichtbarkeit</h5>
 						</div>
 					</div>
 					<div className="row">
 						<div className="col-sm-8 no-padding">
+							Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 							<table>
 								<thead>
 									<tr>
