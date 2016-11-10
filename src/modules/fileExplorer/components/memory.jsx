1b943f5666bb6442f6f5c36f1e081efa1432ad0e
Lars Lange <lars.lange@student.hpi.de>
2016-11-10 17:24:44 +0100

    change visuals

--- src/modules/fileExplorer/components/memory.jsx
+++ src/modules/fileExplorer/components/memory.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router';
+
 require('../styles/memory.scss');
 
 class Memory extends React.Component {
@@ -10,18 +12,24 @@ class Memory extends React.Component {
 
 	/* Mock data */
 	getData() {
-		return [{
-
-		}];
+		return [{}];
 	}
 
 	render() {
 		return (
-			<section>
+			<section className="memory">
 				<div className="container-fluid">
 					<div className="row">
-						<div className="col-sm-12 no-padding memory">
-              <p><i className="fa fa-floppy-o" aria-hidden="true"></i> <b>3.10 MB</b> verwendet von <b>5 GB</b></p>
+						<div className="col-sm-4 no-padding">
+							<Link href="#">
+								<i className="fa fa-upload" aria-hidden="true"></i>Hochladen
+							</Link>
+						</div>
+						<div className="col-sm-8 no-padding rightSide">
+							<p>
+								<i className="fa fa-floppy-o" aria-hidden="true"></i>
+								<b> 3.10 MB</b> verwendet von <b>5 GB</b>
+							</p>
 						</div>
 					</div>
 				</div>
