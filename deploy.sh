40677cb5e1ed968a94424d26c86003b7bba9948b
lars@Server <lars.lange@student.hpi.de>
2016-11-17 09:47:25 +0100

    added telegram messages to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 cd ~/schulcloud-client/
+curl -s -X POST https://api.telegram.org/bot$BOT_ID/sendMessage -d text="$NODE_ENV Server - updating" -d chat_id=$CHAT_ID
 git pull
 npm install
 webpack -d
 forever restart server.js
+curl -s -X POST https://api.telegram.org/bot$BOT_ID/sendMessage -d text="$NODE_ENV Server - update done" -d chat_id=$CHAT_ID
