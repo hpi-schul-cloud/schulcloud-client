5e7d96d88117a1c9c33a40728e9b95a72cbc7336
davidkreidler <david.kreidler@student.hpi.de>
2016-12-06 14:03:53 +0100

    no icons, static image, flex year

--- src/modules/static/components/footer.jsx
+++ src/modules/static/components/footer.jsx
@@ -13,12 +13,12 @@ class Footer extends React.Component {
 			<footer>
 				<div className="imprint">
 					<div className="center">
-						<img src="https://hpi.de/fileadmin/templates/img/hpi_header_logo_pos_srgb.png"></img>
-						<a>2016 Schul-Cloud </a><i className="fa fa-cloud" aria-hidden="true"></i>
-						<a>&emsp;</a>
+						<img src="../../../images/hpi-logo.png" width="235" height="44"></img>
+						<a>{new Date().getFullYear()} Schul-Cloud </a><i className="fa fa-cloud" aria-hidden="true"></i>
+
 						<nobr>
-							<a href="https://hpi.de/impressum.html" target="blank"><i className="fa fa-info" aria-hidden="true"></i> Impressum </a>
-							<a href="mailto:hpi-info@hpi.de?subject=Schul_Cloud%20Anfrage"><i className="fa fa-at" aria-hidden="true"></i> Kontakt</a>
+							&emsp;- <a href="https://hpi.de/impressum.html" target="blank">Impressum </a>
+							 - <a href="mailto:hpi-info@hpi.de?subject=Schul_Cloud%20Anfrage"> Kontakt</a>
 						</nobr>
 					</div>
 				</div>
