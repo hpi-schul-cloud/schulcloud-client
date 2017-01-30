c27776551157400b80b8fa88039f2c0089fb4e69
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-01-30 14:53:12 +0100

    add alert on file deletion

--- src/modules/file-explorer/components/files.jsx
+++ src/modules/file-explorer/components/files.jsx
@@ -16,9 +16,36 @@ class Files extends React.Component {
 		this.props.actions.delete(file);
 	}
 
-	getFileUI(file) {
+	getFileDeleteModalUI(id, file) {
 		return (
-			<div className="col-sm-6 col-xs-12 col-md-4" key={file.name}>
+			<div className="modal fade" id={`deleteFileModal${id}`} role="dialog" aria-labelledby="myModalLabel">
+				<div className="modal-dialog" role="document">
+					<div className="modal-content">
+						<div className="modal-header">
+							<button type="button" className="close" data-dismiss="modal" aria-label="Close"><span
+								aria-hidden="true">&times;</span></button>
+							<h4 className="modal-title" id="myModalLabel">Datei löschen</h4>
+						</div>
+						<div className="modal-body">
+							<p>Möchtest du die Datei wirklich löschen?</p>
+							<span>
+								<button type="button" className="btn btn-default" data-dismiss="modal" aria-label="Close">
+									Abbrechen
+								</button>
+								<button onClick={this.handleOnDeleteClick.bind(this, file)} type="button" className="btn btn-primary" data-dismiss="modal" aria-label="Close">
+									Löschen
+								</button>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	getFileUI(id, file) {
+		return (
+			<div className="col-sm-6 col-xs-12 col-md-4" key={`file${id}`}>
 				<div className="card file">
 					<div className="card-block">
 							<div className="card-title">
@@ -29,21 +56,24 @@ class Files extends React.Component {
 							</div>
 							<div className="card-text">
 								<i className="fa fa-cloud-download" aria-hidden="true" onClick={this.handleOnDownloadClick.bind(this, file)}/>
-								<i className="fa fa-trash-o" aria-hidden="true" onClick={this.handleOnDeleteClick.bind(this, file)}/>
+								<i className="fa fa-trash-o" aria-hidden="true" data-toggle="modal" data-target={`#deleteFileModal${id}`}/>
 							</div>
 					</div>
 				</div>
+				{ this.getFileDeleteModalUI(id, file) }
 			</div>
 		);
 	}
 
 	getFilesUI() {
+		let idCount = 0;
 		return (
-		<div>
-			{this.props.files.map((file) => {
-				return this.getFileUI(file);
-			})}
-		</div>
+			<div>
+				{this.props.files.map((file) => {
+					idCount++;
+					return this.getFileUI(idCount, file);
+				})}
+			</div>
 		);
 	}
 
