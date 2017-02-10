d38468ee403d7b33bcea03e9411a8e4938456d05
Nico Knoll <mail@nico.is>
2017-02-10 13:17:51 +0100

    Use Calendar service in client

--- src/modules/timetable/containers/calendar.js
+++ src/modules/timetable/containers/calendar.js
@@ -1,7 +1,11 @@
 import {render} from 'react-dom';
 import {compose} from 'react-komposer';
 
+import { Server } from '../../core/helpers/';
+
 import component from '../components/calendar';
+
+const calendarService = Server.service('/calendar');
 
 const composer = (props, onData) => {
 	let view = location.hash.replace('#', '');
@@ -9,12 +13,29 @@ const composer = (props, onData) => {
 		view = 'month';
 	}
 
-	let componentData = {
-		events: [],
-		view
-	};
+	calendarService.find().then(data => {
+		const events = data.map(event => {
+			return {
+				id: event.id,
+				title: event.summary,
+				start: event.dtstart,
+				end: event.dtend,
+				cancelled: event.cancelled,
+				//url: '',
 
-	onData(null, componentData);
+				/* custom */
+				description: event.description,
+				location: event.location,
+			}
+		});
+
+		let componentData = {
+			events,
+			view
+		};
+
+		onData(null, componentData);
+	});
 };
 
 export default compose(composer)(component);
