0ceb85de0ed526a7e8d2051902753d7ccb4091ec
Pit Wegner <pit.wegner@student.hpi.de>
2017-03-13 10:38:47 +0100

    added news administration

---
+++ src/modules/news/permissions.js
@@ -0,0 +1,3 @@
+export default {
+	VIEW: 'DASHBOARD_VIEW'
+};
---
+++ src/modules/news/routes.jsx
@@ -0,0 +1,10 @@
+import Permissions from './permissions';
+import Dashboard from './containers/dashboard';
+
+export default [
+	{
+		path: '/dashboard/',
+		name: 'dashboard',
+		component: Dashboard
+	}
+];
