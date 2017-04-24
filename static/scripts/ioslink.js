155c35fed659022256cf31cccc06151c63cbe07c
davidkreidler <david.kreidler@student.hpi.de>
2017-04-24 12:15:35 +0200

    fix link opening
    only on ios device open in same window

--- static/scripts/ioslink.js
+++ static/scripts/ioslink.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
         $(function () {
-        $('a').click(function () {
-            document.location = $(this).attr('href');
-            return false;
-        });
-    });
+            if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream){
+                $('a').click(function () {
+                    document.location = $(this).attr('href');
+                    return false;
+                });
+            }});
 });
