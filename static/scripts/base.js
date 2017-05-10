723938475deb9ea14e5bdfc7ffe7872ed5f07e0a
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-05-10 14:18:37 +0200

    refactor modal-populating (#217)

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -21,6 +21,57 @@ function updateQueryStringParameter(uri, key, value) {
     }
 }
 
+function populateModalForm(modal, data) {
+
+    var $title = modal.find('.modal-title');
+    var $btnSubmit = modal.find('.btn-submit');
+    var $btnClose = modal.find('.btn-close');
+    var $form = modal.find('.modal-form');
+
+    $title.html(data.title);
+    $btnSubmit.html(data.submitLabel);
+    $btnClose.html(data.closeLabel);
+
+    if (data.action) {
+        $form.attr('action', data.action);
+    }
+
+    // fields
+    $('[name]', $form).not('[data-force-value]').each(function () {
+        var value = (data.fields || {})[$(this).prop('name').replace('[]', '')] || '';
+        switch ($(this).prop("type")) {
+            case "radio":
+            case "checkbox":
+                $(this).each(function () {
+                    if (($(this).attr('name') == $(this).prop('name')) && value) {
+                        $(this).attr("checked", value);
+                    } else {
+                        $(this).removeAttr("checked");
+                    }
+                });
+                break;
+            case "datetime-local":
+                $(this).val(value.slice(0, 16)).trigger("chosen:updated");
+                break;
+            case "date":
+                $(this).val(value.slice(0, 10)).trigger("chosen:updated");
+                break;
+            case "color":
+                $(this).attr("value", value);
+                $(this).attr("placeholder", value);
+                break;
+            default:
+                if ($(this).prop('nodeName') == "TEXTAREA" && $(this).hasClass("customckeditor")) {
+                    if (CKEDITOR.instances.description) {
+                        CKEDITOR.instances.description.setData(value);
+                    }
+                } else {
+                    $(this).val(value).trigger("chosen:updated");
+                }
+        }
+    });
+}
+
 $(document).ready(function () {
     // notification stuff
     var $notification = $('.notification');
