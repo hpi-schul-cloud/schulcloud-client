601ec2cc52e57bd4390d8358c3530872b73a0067
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-05-10 14:55:00 +0200

    remove unnecessary modal-function

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -19,60 +19,6 @@ $(document).ready(function () {
         }
     }
 
-    var populateModalForm = function(modal, data) {
-
-        var $title = modal.find('.modal-title');
-        var $btnSubmit = modal.find('.btn-submit');
-        var $btnClose = modal.find('.btn-close');
-        var $form = modal.find('.modal-form');
-
-        $title.html(data.title);
-        $btnSubmit.html(data.submitLabel);
-        $btnClose.html(data.closeLabel);
-
-        if(data.action) {
-            console.log(data.action);
-            $form.attr('action', data.action);
-        }
-
-        // fields
-        $('[name]', $form).not('[data-force-value]').each(function () {
-            var value = (data.fields || {})[$(this).prop('name').replace('[]', '')] || '';
-            switch ($(this).prop("type")) {
-                case "radio":
-                    if(typeof(value) === "boolean"){value = value?"1":"0";}
-                    if(value === ""){value = "0";}
-                    if (($(this).attr('name') == $(this).prop('name'))&&($(this).attr('value')==value)){
-                        $(this).attr("checked","checked");
-                    }else{
-                        $(this).removeAttr("checked");
-                    }
-                    break;
-                case "checkbox":
-                    $(this).each(function () {
-                        if (($(this).attr('name') == $(this).prop('name'))){
-                            $(this).attr("checked", value);
-                        }else{
-                            $(this).removeAttr("checked");
-                        }
-                    });
-                    break;
-                case "datetime-local":
-                    $(this).val(value.slice(0,16)).trigger("chosen:updated");
-                    break;
-                case "date":
-                    $(this).val(value.slice(0,10)).trigger("chosen:updated");
-                    break;
-                case "color":
-                    $(this).attr("value", value);
-                    $(this).attr("placeholder", value);
-                    break;
-                default:
-                    $(this).val(value).trigger("chosen:updated");
-            }
-        });
-    };
-
     $calendar.fullCalendar({
         defaultView: view || 'month',
         editable: false,
