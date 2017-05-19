617b527c5d64985314c1ff56ec1de22ee0c4a37a
Pit Wegner <pit.wegner@student.hpi.de>
2017-05-19 14:19:37 +0200

    fixed time formatting with momentjs

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -6,6 +6,7 @@ const express = require('express');
 const router = express.Router();
 const authHelper = require('../helpers/authentication');
 const api = require('../api');
+const moment = require("moment");
 
 // secure routes
 router.use(authHelper.authChecker);
@@ -14,44 +15,8 @@ router.get('/', function (req, res, next) {
     api(req).get('/news/', {
     }).then(news => {
         news = news.data.map(news => {
-
             news.url = '/news/' + news._id;
-
-            function formattimepart(s) {
-                return (s < 10) ? "0" + s : s;
-            }
-
-            var dateRaw = new Date(news.displayAt);
-            var date = new Date(dateRaw.getTime());
-            var dateF = formattimepart(date.getDate()) + "." + formattimepart(date.getMonth() + 1) + "." + date.getFullYear();
-            var timeF = formattimepart(date.getHours()) + ":" + formattimepart(date.getMinutes());
-
-            var now = new Date();
-            var past = (now - date);
-            var pastDays = Math.floor(past / (1000 * 60 * 60 * 24));
-            var pastHours = Math.floor((past % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var pastMinutes = Math.floor(((past % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) / (1000 * 60));
-            var pastString;
-            if (pastDays > 5 || past < 0) {
-                pastString = (dateF + " (" + timeF + ")")
-            }
-            else if (pastDays > 1) {
-                pastString = "vor " + pastDays + " Tagen"
-            }
-            else if (pastDays == 1) {
-                pastString = "vor 1 Tag " + pastHours + ((pastHours == 1) ? " Stunde" : " Stunden")
-            }
-            else if (pastHours > 2) {
-                pastString = "vor " + pastHours + " Stunden"
-            }
-            else if (pastHours >= 1) {
-                pastString = "vor " + pastHours + ((pastHours == 1) ? " Stunde " : " Stunden ") + pastMinutes + ((pastMinutes == 1) ? " Minute" : " Minuten")
-            }
-            else {
-                pastString = "vor " + pastMinutes + ((pastMinutes == 1) ? " Minute" : " Minuten")
-            }
-
-            news.timeString = pastString;
+            news.timeString = moment(news.displayAt).fromNow();
             return news;
         });
         function sortFunction(a, b) {
--- controllers/news.js
+++ controllers/news.js
@@ -8,6 +8,7 @@ const marked = require('marked');
 const api = require('../api');
 const authHelper = require('../helpers/authentication');
 const handlebars = require("handlebars");
+const moment = require("moment");
 
 router.use(authHelper.authChecker);
 
@@ -28,51 +29,14 @@ router.all('/', function (req, res, next) {
     api(req).get('/news/', {
     }).then(news => {
         news = news.data.map(news => {
-
             news.url = '/news/' + news._id;
-
-            function formattimepart(s) {
-                return (s < 10) ? "0" + s : s;
-            }
-
-            var dateRaw = new Date(news.displayAt);
-            var date = new Date(dateRaw.getTime());
-            var dateF = formattimepart(date.getDate()) + "." + formattimepart(date.getMonth() + 1) + "." + date.getFullYear();
-            var timeF = formattimepart(date.getHours()) + ":" + formattimepart(date.getMinutes());
-
-            var now = new Date();
-            var past = (now - date);
-            var pastDays = Math.floor(past / (1000 * 60 * 60 * 24));
-            var pastHours = Math.floor((past % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var pastMinutes = Math.floor(((past % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) / (1000 * 60));
-            var pastString;
-            if (pastDays > 5 || past < 0) {
-                pastString = (dateF + " (" + timeF + ")")
-            }
-            else if (pastDays > 1) {
-                pastString = "vor " + pastDays + " Tagen"
-            }
-            else if (pastDays == 1) {
-                pastString = "vor 1 Tag " + pastHours + ((pastHours == 1) ? " Stunde" : " Stunden")
-            }
-            else if (pastHours > 2) {
-                pastString = "vor " + pastHours + " Stunden"
-            }
-            else if (pastHours >= 1) {
-                pastString = "vor " + pastHours + ((pastHours == 1) ? " Stunde " : " Stunden ") + pastMinutes + ((pastMinutes == 1) ? " Minute" : " Minuten")
-            }
-            else {
-                pastString = "vor " + pastMinutes + ((pastMinutes == 1) ? " Minute" : " Minuten")
-            }
-
-            news.timeString = pastString;
+            news.timeString = moment(news.displayAt).fromNow();
             return news;
         });
         function sortFunction(a, b) {
             if (a.displayAt === b.displayAt) {
                 return 0;
-            }
-            else {
+            } else {
                 return (a.displayAt < b.displayAt) ? 1 : -1;
             }
         }
@@ -85,42 +49,7 @@ router.get('/:newsId', function (req, res, next) {
     api(req).get('/news/'+req.params.newsId, {
     }).then(news => {
         news.url = '/news/' + news._id;
-
-        function formattimepart(s) {
-            return (s < 10) ? "0" + s : s;
-        }
-
-        var dateRaw = new Date(news.displayAt);
-        var date = new Date(dateRaw.getTime());
-        var dateF = formattimepart(date.getDate()) + "." + formattimepart(date.getMonth() + 1) + "." + date.getFullYear();
-        var timeF = formattimepart(date.getHours()) + ":" + formattimepart(date.getMinutes());
-
-        var now = new Date();
-        var past = (now - date);
-        var pastDays = Math.floor(past / (1000 * 60 * 60 * 24));
-        var pastHours = Math.floor((past % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var pastMinutes = Math.floor(((past % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) / (1000 * 60));
-        var pastString;
-        if (pastDays > 5 || past < 0) {
-            pastString = (dateF + " (" + timeF + ")")
-        }
-        else if (pastDays > 1) {
-            pastString = "vor " + pastDays + " Tagen"
-        }
-        else if (pastDays == 1) {
-            pastString = "vor 1 Tag " + pastHours + ((pastHours == 1) ? " Stunde" : " Stunden")
-        }
-        else if (pastHours > 2) {
-            pastString = "vor " + pastHours + " Stunden"
-        }
-        else if (pastHours >= 1) {
-            pastString = "vor " + pastHours + ((pastHours == 1) ? " Stunde " : " Stunden ") + pastMinutes + ((pastMinutes == 1) ? " Minute" : " Minuten")
-        }
-        else {
-            pastString = "vor " + pastMinutes + ((pastMinutes == 1) ? " Minute" : " Minuten")
-        }
-
-        news.timeString = pastString;
+        news.timeString = moment(news.displayAt).fromNow();
         res.render('news/newsEntry', {title: news.title, news});
     });
 });
