b6111034b5cac1a55435cf8906fd8abd7b387c65
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-05-30 19:12:45 +0200

    tooltip bei augenfunktion in themenübersicht

--- static/scripts/courses.js
+++ static/scripts/courses.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
     $('.btn-hidden-toggle').click(function (e) {
         e.stopPropagation();
         e.preventDefault();
+        var $hiddenToggleBtn = $(this);
         var $hiddenToggleIcon = $(this).find('.fa');
         $.ajax({
             method: 'PATCH',
@@ -11,12 +13,13 @@ $(document).ready(function () {
                 if (result.hidden) {
                     $hiddenToggleIcon.addClass('fa-eye-slash');
                     $hiddenToggleIcon.removeClass('fa-eye');
+                    $hiddenToggleBtn.attr('data-original-title', "Thema entsperren");
                 } else {
                     $hiddenToggleIcon.removeClass('fa-eye-slash');
                     $hiddenToggleIcon.addClass('fa-eye');
+                    $hiddenToggleBtn.attr('data-original-title', "Thema sperren");
                 }
             }
         });
-    })
-
+    });
 });
