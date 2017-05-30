66e0993defe5d116b624e9f7bbef61c9699452a6
Nico Knoll <mail@nico.is>
2017-05-30 17:49:57 +0200

    Fixed repeating events in dashboard

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -8,35 +8,6 @@ const api = require('../api');
 const moment = require("moment");
 const recurringEventsHelper = require('../helpers/recurringEvents');
 
-/**
- * handle recurring events for fullcalendar.js
- * @param event {Event} - a event which could contain a recurring value
- * @returns events [] - new set of events
- */
-const mapRecurringEvent = (event) => {
-    if (event.included && event.included[0].attributes.freq == 'WEEKLY') {
-        return recurringEventsHelper.createRecurringEvents(event);
-    }
-
-    return [event];
-};
-
-/**
- * maps properties of a event to fit fullcalendar, e.g. url and color
- * @param event
- */
-const mapEventProps = (event, req) => {
-    if (event["x-sc-courseId"]) {
-        return api(req).get('/courses/' + event["x-sc-courseId"]).then(course => {
-            event.url = event["x-sc-courseTimeId"] ? '/courses/' + course._id : '';
-            event.color = course.color;
-            return event;
-        });
-    }
-
-    return event;
-};
-
 // secure routes
 router.use(require('../helpers/authentication').authChecker);
 
@@ -53,8 +24,8 @@ router.get('/events/', function (req, res, next) {
             all: true
         }
     }).then(events => {
-        Promise.all(events.map(event => mapEventProps(event, req))).then(events => {
-            events = [].concat.apply([], events.map(mapRecurringEvent));
+        Promise.all(events.map(event => recurringEventsHelper.mapEventProps(event, req))).then(events => {
+            events = [].concat.apply([], events.map(recurringEventsHelper.mapRecurringEvent));
             return res.json(events);
         });
     }).catch(err => {
