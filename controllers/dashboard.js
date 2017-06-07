75fd19091edd343ef42171b843a581c44bbd68a3
Nico Knoll <mail@nico.is>
2017-06-07 10:16:31 +0200

    Dashboard stuff (#283)
    * Dashboard stuff
    
    * location

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -92,53 +92,54 @@ router.get('/', function (req, res, next) {
             $populate: ['courseId']
         }
     }).then(data => data.data.filter(assignment => {
-        if (new Date(assignment.availableDate).getTime() > Date.now()
-            && assignment.teacherId != res.locals.currentUser._id) {
-            return;
-        }
-        if (assignment.courseId != null) {
-            if (assignment.courseId.userIds.indexOf(res.locals.currentUser._id) == -1
+            if (new Date(assignment.availableDate).getTime() > Date.now()
                 && assignment.teacherId != res.locals.currentUser._id) {
                 return;
             }
-            if (!assignment.private) {
-                assignment.userIds = assignment.courseId.userIds;
+            if (assignment.courseId != null) {
+                if (assignment.courseId.userIds.indexOf(res.locals.currentUser._id) == -1
+                    && assignment.teacherId != res.locals.currentUser._id) {
+                    return;
+                }
+                if (!assignment.private) {
+                    assignment.userIds = assignment.courseId.userIds;
+                }
+                assignment.color = (assignment.courseId.color.length != 7) ? "#1DE9B6" : assignment.courseId.color;
+            } else {
+                assignment.color = "#1DE9B6";
+                assignment.private = true;
             }
-            assignment.color = (assignment.courseId.color.length != 7) ? "#1DE9B6" : assignment.courseId.color;
-        } else {
-            assignment.color = "#1DE9B6";
-            assignment.private = true;
-        }
-        if (assignment.private
-            && assignment.teacherId != res.locals.currentUser._id) {
-            return;
-        }
+            if (assignment.private
+                && assignment.teacherId != res.locals.currentUser._id) {
+                return;
+            }
 
-        return true;
-    }).map(assignment => {
-        assignment.url = '/homework/' + assignment._id;
-        return assignment;
-    }));
+            return true;
+        }).map(assignment => {
+            assignment.url = '/homework/' + assignment._id;
+            return assignment;
+        }));
+
 
     // TODO: Replace with real news service
     const newsPromise = new Promise((resolve, reject) => {
         resolve([{
-            name: 'Willkommen im Schuljahr 2016!',
+            name: 'Willkommen im Schuljahr!',
             summary: `Die Schulleitung heißt alle (neuen) Schüler im neuen Schuljahr 
                 willkommen. Auch dieses Jahr haben wir wieder viele neue Veranstaltungen für 
                 Euch geplant. Unter anderem ein Besuch bei der UNESCO und einen Schüleraustausch 
                 mit einer Schule im Silicon Valley der Klasse 10. Viel Erfolg wünscht Euch das 
                 Lehrerkolleg!`,
-            date: new Date(),
+            date: new Date(2017, 4, 29, 10),
             url: ''
         },{
             name: 'Preisverleihung Sommerfestspiele',
             summary: `Auch in diesem Jahr gab es wieder herausragende sportliche
                 Leistungen. Unter Anderem ein neuer Schulrekord im 100m-Lauf bei den Mädchen und
                 beim Weitsprung der Jungen. Auch für die Versorgung mit Wasser und Energieriegel
-                wurde durch den Förderverein gesorgt. Die Urkundenübergabe findet am 30. Juni um
+                wurde durch den Förderverein gesorgt. Die Urkundenübergabe findet am 30. Juli um
                 13 Uhr in der Sporthalle statt.`,
-            date: new Date(),
+            date: new Date(2017, 4, 25, 10),
             url: ''
         },{
             name: 'Einführung der Schul-Cloud',
@@ -147,7 +148,7 @@ router.get('/', function (req, res, next) {
                 ermöglicht es unter Anderem, sich mit den bestehenden Moodle-Accounts
                 anzumelden, Office zu nutzen und auf Bildungsangebote zuzugreifen. Ebenfalls ist
                 es möglich, aktualisierte Stundenpläne und Aufgaben einzusehen.`,
-            date: new Date(),
+            date: new Date(2017, 4, 20, 10),
             url: ''
         }]);
     });
@@ -159,11 +160,20 @@ router.get('/', function (req, res, next) {
         homeworksPromise,
         newsPromise
     ]).then(([events, homeworks, news]) => {
+
+        homeworks.sort((a,b) => {
+            if(a.dueDate > b.dueDate) {
+                return 1;
+            } else {
+                return -1;
+            }
+        })
+
         res.render('dashboard/dashboard', {
             title: 'Übersicht',
             events,
             eventsDate: moment().format('dddd, DD. MMMM YYYY'),
-            homeworks: _.chunk(homeworks, 3),
+            homeworks: _.chunk(homeworks.slice(0, 6), 3),
             news,
             hours,
             currentTimePercentage,
