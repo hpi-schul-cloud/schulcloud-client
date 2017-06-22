30b60fb464460798049bfa55d3622e88d4e1d82c
adrianjost <adrianjost@hackedit.de>
2017-06-22 16:50:25 +0200

    Bewertung kann nun ohne Pagereload gespeichert werden

--- static/scripts/homework.js
+++ static/scripts/homework.js
@@ -34,6 +34,34 @@ $(document).ready(function() {
         $modals.modal('hide');
     });
 
+    function ajaxForm(element){
+        const submitbutton = element.find('[type=submit]')[0];
+        submitbuttontext = submitbutton.innerHTML || submitbutton.value;
+        submitbutton.innerHTML = submitbuttontext+' <div class="loadingspinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>'
+        submitbutton.disabled = true;
+        
+        const url     = element.attr("action");
+        const method  = element.attr("method");
+        // update value of ckeditor instances
+        ckeditorInstance = element.find('textarea.customckeditor').attr("id");
+        CKEDITOR.instances[ckeditorInstance].updateElement();
+        const content = element.serialize();
+        $.ajax({
+            type: method,
+            url: url,
+            data: content
+        }).done(function(r) {
+            submitbutton.innerHTML = submitbuttontext;
+            submitbutton.disabled = false;
+        });
+    }
+
+    $('.evaluation #comment form').on("submit",function(e){
+        if(e){e.preventDefault();}
+        ajaxForm($(this));
+        return false;
+    });
+    
     $('#sortselection').on('change', function(e){
         window.location = window.location.pathname + "?sort=" + escape( $('#sortselection').val());
     });
