bff42402e2d661a022a42fd6293347b5ffe17e9f
Alexander Kremer <alex@kremer.io>
2017-07-04 14:23:51 +0200

    Switch data to JSON and Added neXenio & GeoGebra

--- controllers/partner.js
+++ controllers/partner.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const _orderBy = require('lodash').orderBy;
 const authHelper = require('../helpers/authentication');
+
+// Partners
+const partners = require('../helpers/partner/partners.json');
+
+// Sort Content-Partners & Sponsors by Name
+partners.content_partners = _orderBy(partners.content_partners,
+    [x => x.name.toLowerCase()], ['asc']);
+partners.sponsors = _orderBy(partners.sponsors,
+    [x => x.name.toLowerCase()], ['asc']);
 
 // secure routes
 router.use(authHelper.authChecker);
 
 router.get('/', function (req, res, next) {
     res.render('partner/partner', {
-        title: 'Partner'
+        title: 'Partner',
+        partners: partners
     });
 });
-
 
 module.exports = router;
