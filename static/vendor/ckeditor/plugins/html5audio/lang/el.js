db1de88144401ac653d2c55faba81cfd9f7d6fac
Langleu <lars.lange@student.hpi.de>
2017-07-07 13:37:07 +0200

    add audio and add to configs

---
+++ static/vendor/ckeditor/plugins/html5audio/lang/el.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.setLang( 'html5audio', 'el', {
+    button: 'Προσθέστε HTML5 ήχο',
+    title: 'HTML5 ήχος',
+    infoLabel: 'Πληροφορίες ήχου',
+    urlMissing: 'Η πηγή URL ήχου απουσιάζει.',
+    audioProperties: 'Ιδιότητες ήχου',
+    upload: 'Upload',
+    btnUpload: 'Αποστολή στον διακομιστή',
+    advanced: 'Προχωρημένα',
+    autoplay: 'Αυτόματη αναπαραγωγή;',
+    yes: 'Ναι',
+    no: 'Όχι'
+} );
