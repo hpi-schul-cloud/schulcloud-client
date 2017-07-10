bf73a45e7c827c9512bdee5da0267e6baeee38c6
Langleu <lars.lange@student.hpi.de>
2017-07-10 16:13:41 +0200

    changelog

---
+++ controllers/help.js
@@ -0,0 +1,31 @@
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const authHelper = require('../helpers/authentication');
+const api = require('../api');
+const showdown  = require('showdown');
+const converter = new showdown.Converter();
+const moment = require('moment');
+
+// secure routes
+router.use(authHelper.authChecker);
+
+router.get('/', function (req, res, next) {
+    api(req).get('/releases')
+        .then(releases => {
+            releases.data = _.sortBy(releases.data, 'createdAt').reverse();
+            releases.data.map(release => {
+                release.body = converter.makeHtml(release.body);
+                release.publishedAt = moment(release.publishedAt).format('ddd, ll');
+            });
+            //let newRelease = releases.data.shift();
+
+            res.render('help/help', {
+                title: 'Hilfe',
+                release: releases.data
+            });
+        });
+});
+
+
+module.exports = router;
