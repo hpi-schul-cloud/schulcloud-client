9454dac04e8edb0fcbf920da0e03be5f75524ecb
Lars Lange <langleu90@gmail.com>
2017-07-25 14:50:19 +0200

    quickfix for async tabs (#335)
    * move datepicker back do overview.hbs, as it destroys the async tabs
    
    * fix ckeditor not being filled in with content

--- static/scripts/homework.js
+++ static/scripts/homework.js
@@ -33,31 +33,6 @@ $(document).ready(function() {
     $modals.find('.close, .btn-close').on('click', function() {
         $modals.modal('hide');
     });
-    
-    $.datetimepicker.setLocale('de');
-    $('input[data-datetime]').datetimepicker({
-        format:'d.m.Y H:i',
-        mask: '39.19.9999 29:59',
-        onShow:function( ct ,input){
-            if(input[0].id == "availableDate"){
-                let due = $(input[0].parentNode.parentNode).find("#dueDate").val().split(" ");
-                this.setOptions({
-                    minDate:0,
-                    maxDate:(due && due[0])?due[0]:false,
-                    formatDate:'d.m.Y',
-                });
-            }
-            else if(input[0].id == "dueDate"){
-                let available = $(input[0].parentNode.parentNode).find("#availableDate").val().split(" ");
-                this.setOptions({
-                    minDate:(available && available[0])?available[0]:0,
-                    maxDate:false,
-                    formatDate:'d.m.Y'
-                });
-            }
-        }
-    });
-
 
     function ajaxForm(element, after){
         const submitButton = element.find('[type=submit]')[0];
@@ -79,7 +54,7 @@ $(document).ready(function() {
             url: url,
             data: content,
             context: element
-        })
+        });
         request.done(function(r) {
             submitButton.innerHTML = submitButtonText;
             submitButton.disabled = false;
