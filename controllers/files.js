f42ad65ca2afac9e939f6f34084589feb10ddee5
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-08-08 13:55:52 +0200

    little fix

--- controllers/files.js
+++ controllers/files.js
@@ -16,31 +16,6 @@ const shortid = require('shortid');
 const upload = multer({storage: multer.memoryStorage()});
 const _ = require('lodash');
 
-const thumbs = {
-    default: "/images/thumbs/default.png",
-    psd: "/images/thumbs/psds.png",
-    txt: "/images/thumbs/txts.png",
-    doc: "/images/thumbs/docs.png",
-    png: "/images/thumbs/pngs.png",
-    mp4: "/images/thumbs/mp4s.png",
-    mp3: "/images/thumbs/mp3s.png",
-    aac: "/images/thumbs/aacs.png",
-    avi: "/images/thumbs/avis.png",
-    gif: "/images/thumbs/gifs.png",
-    html: "/images/thumbs/htmls.png",
-    js: "/images/thumbs/jss.png",
-    mov: "/images/thumbs/movs.png",
-    xls: "/images/thumbs/xlss.png",
-    xlsx: "/images/thumbs/xlss.png",
-    pdf: "/images/thumbs/pdfs.png",
-    flac: "/images/thumbs/flacs.png",
-    jpg: "/images/thumbs/jpgs.png",
-    jpeg: "/images/thumbs/jpgs.png",
-    docx: "/images/thumbs/docs.png",
-    ai: "/images/thumbs/ais.png",
-    tiff: "/images/thumbs/tiffs.png"
-};
-
 const filterOptions = [
     {key: 'pics', label: 'Bilder'},
     {key: 'videos', label: 'Videos'},
