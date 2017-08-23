6fcaec724ea913ab6669f521f73f3b0af4477bc7
Langleu <lars.lange@student.hpi.de>
2017-08-23 14:20:16 +0200

    track schoolid, gender and role

--- static/scripts/piwik/piwik.js
+++ static/scripts/piwik/piwik.js
@@ -1,6 +1,15 @@
 var _paq = _paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
+$.get('/account/user', (res) => {
+    if (typeof (res || {}).firstName === 'string') {
+        _paq.push(['setCustomDimension', customDimensionId = 1, customDimensionValue = res.schoolId]);
+        _paq.push(['setCustomDimension', customDimensionId = 2, customDimensionValue = res.gender]);
+        _paq.push(['setCustomDimension', customDimensionId = 4, customDimensionValue = res.roles[0].name]);
+    }
+
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+});
+
 (function () {
     var u = "//open.hpi.de/piwik/";
     _paq.push(['setTrackerUrl', u + 'piwik.php']);
