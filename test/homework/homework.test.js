fbd46e5bff5d0987e49d97cb128b33301e791e5b
adrianjost <homepage@hackedit.de>
2017-09-27 15:31:17 +0200

    first basic tests

---
+++ test/homework/homework.test.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const assert = require('assert');
+const app = require('../../app');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const loginHelper = require('../helper/login-helper');
+chai.use(chaiHttp);
+
+describe('Homework tests', function () {
+    this.timeout(10000);
+    before(function (done) {
+        this.server = app.listen(3031);
+        this.server.once('listening', () => {
+            loginHelper.login(app).then(res => {
+                this.agent = res.agent;
+                done();
+            });
+        });
+    });
+
+    after(function (done) {
+        this.server.close(done);
+    });
+
+    it('GET /homework', function () {
+        return new Promise((resolve, reject) => {
+            this.agent
+                .get('/homeworks/')
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.text).to.contain('Aufgaben');
+                    resolve();
+                });
+        });
+    });
+
+    it('GET /homework/asked', function () {
+        return new Promise((resolve, reject) => {
+            this.agent
+                .get('/homeworks/asked')
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.text).to.contain('Gestellte Aufgaben');
+                    resolve();
+                });
+        });
+    });
+
+    it('GET /homework/private', function () {
+        return new Promise((resolve, reject) => {
+            this.agent
+                .get('/homeworks/private')
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.text).to.contain('Meine Aufgaben');
+                    resolve();
+                });
+        });
+    });
+
+    it('GET /homework/archive', function () {
+        return new Promise((resolve, reject) => {
+            this.agent
+                .get('/homeworks/archive')
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.text).to.contain('Archivierte Aufgaben');
+                    resolve();
+                });
+        });
+    });
+});
