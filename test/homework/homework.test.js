f672cd843d7089afb038be8ed220799d222bd425
adrianjost <homepage@hackedit.de>
2017-10-02 12:13:15 +0200

    more tests (not working, permission bug in homework code found)

--- test/homework/homework.test.js
+++ test/homework/homework.test.js
@@ -24,6 +24,7 @@ describe('Homework tests', function () {
         this.server.close(done);
     });
 
+    // OVERVIEW PAGES
     it('GET /homework', function () {
         return new Promise((resolve, reject) => {
             this.agent
@@ -31,11 +32,11 @@ describe('Homework tests', function () {
                 .end((err, res) => {
                     expect(res.statusCode).to.equal(200);
                     expect(res.text).to.contain('Aufgaben');
+                    expect(res.text).to.contain('Mathe');
                     resolve();
                 });
         });
     });
-
     it('GET /homework/asked', function () {
         return new Promise((resolve, reject) => {
             this.agent
@@ -43,11 +44,12 @@ describe('Homework tests', function () {
                 .end((err, res) => {
                     expect(res.statusCode).to.equal(200);
                     expect(res.text).to.contain('Gestellte Aufgaben');
+                    expect(res.text).to.not.contain('btn btn-secondary btn-sm btn-edit');
+                    expect(res.text).to.not.contain('btn btn-secondary btn-sm btn-delete');
                     resolve();
                 });
         });
     });
-
     it('GET /homework/private', function () {
         return new Promise((resolve, reject) => {
             this.agent
@@ -55,11 +57,12 @@ describe('Homework tests', function () {
                 .end((err, res) => {
                     expect(res.statusCode).to.equal(200);
                     expect(res.text).to.contain('Meine Aufgaben');
+                    expect(res.text).to.contain('btn btn-secondary btn-sm btn-edit');
+                    expect(res.text).to.contain('btn btn-secondary btn-sm btn-delete');
                     resolve();
                 });
         });
     });
-
     it('GET /homework/archive', function () {
         return new Promise((resolve, reject) => {
             this.agent
@@ -68,6 +71,52 @@ describe('Homework tests', function () {
                     expect(res.statusCode).to.equal(200);
                     expect(res.text).to.contain('Archivierte Aufgaben');
                     expect(res.text).to.contain('Keine Aufgaben.');
+                    resolve();
+                });
+        });
+    });
+    
+    // CREATE & EDIT TASKS
+    it('GET /homework/new', function () {
+        return new Promise((resolve, reject) => {
+            this.agent
+                .get('/homework/archive')
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.text).to.contain('Aufgabe hinzufügen');
+                    expect(res.text).to.contain('Titel');
+                    expect(res.text).to.contain('Kurs');
+                    expect(res.text).to.contain('Thema');
+                    expect(res.text).to.contain('Aufgabenstellung');
+                    expect(res.text).to.contain('Bearbeitungszeit');
+                    resolve();
+                });
+        });
+    });
+    // users task
+    it('GET /homework/{users task}/edit', function () {
+        return new Promise((resolve, reject) => {
+            this.agent
+                .get('/homework/59cce2c61113d1132c98dc06/edit')
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.text).to.contain("Private Aufgabe von Ida - mit Kurs, abgelaufen");
+                    expect(res.text).to.contain("<span>Mathe</span>");
+                    expect(res.text).to.contain("<p>Aufgabenbeschreibung</p>");
+                    expect(res.text).to.contain('value="20.09.2016 13:00"');
+                    expect(res.text).to.contain('value="28.07.2017 15:00"');
+                    resolve();
+                });
+        });
+    });
+    // teachers task
+    it('GET /homework/{teachers task}/edit', function () {
+        return new Promise((resolve, reject) => {
+            this.agent
+                .get('/homework/59cce3f6c6abf042248e888d/edit')
+                .end((err, res) => {
+                    expect(res.statusCode).to.not.equal(200);
+                    expect(res.text).to.not.contain("Aufgabe an Ida (Mathe) - offen");
                     resolve();
                 });
         });
