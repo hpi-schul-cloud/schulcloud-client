72f016ba60d19daa727dee6d16566b0844e2f991
adrianjost <homepage@hackedit.de>
2017-11-29 17:35:29 +0100

    794-autoprefixer-für-css-installieren

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const watch = require('gulp-watch');
 const optimizejs = require('gulp-optimize-js');
 const concat = require('gulp-concat');
 const count = require('gulp-count');
+const autoprefixer = require('gulp-autoprefixer');
 
 const baseScripts = [
     './static/scripts/jquery/jquery.min.js',
@@ -65,6 +66,9 @@ const buildStyles = (isWatch) => {
     return getGulpTask('./static/styles/**/*.{css,sass,scss}', isWatch)
         .pipe(sass())
         .pipe(minify)
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions']
+        }))
         .pipe(gulp.dest('./build/styles'))
 };
 
