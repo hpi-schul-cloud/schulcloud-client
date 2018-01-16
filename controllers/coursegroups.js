d574f37bcd9cd2d53fedbb94cb6033fcad8bcc19
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2018-01-16 12:11:46 +0100

    add courseGroup details page

--- controllers/coursegroups.js
+++ controllers/coursegroups.js
@@ -5,56 +5,56 @@ const api = require('../api');
 const authHelper = require('../helpers/authentication');
 
 const getSelectOptions = (req, service, query, values = []) => {
-  return api(req).get('/' + service, {
-      qs: query
-  }).then(data => {
-      return data.data;
-  });
+    return api(req).get('/' + service, {
+        qs: query
+    }).then(data => {
+        return data.data;
+    });
 };
 
 const markSelected = (options, values = []) => {
-  return options.map(option => {
-      option.selected = values.includes(option._id);
-      return option;
-  });
+    return options.map(option => {
+        option.selected = values.includes(option._id);
+        return option;
+    });
 };
 
 const editCourseGroupHandler = (req, res, next) => {
-  let courseGroupId = req.params.courseGroupId;
-  let courseId = req.params.courseId;
+    let courseGroupId = req.params.courseGroupId;
+    let courseId = req.params.courseId;
 
-  let courseGroupPromise, action, method;
-  if (courseGroupId) {
-      action = `/courses/${courseId}/groups/${courseGroupId}`;
-      method = 'patch';
-      courseGroupPromise = api(req).get('/courseGroups/', {
-          qs: {
-              $populate: ['userIds']
-          }
-      });
-  } else {
-      action = `/courses/${courseId}/groups`;
-      method = 'post';
-      courseGroupPromise = Promise.resolve({});
-  }
+    let courseGroupPromise, action, method;
+    if (courseGroupId) {
+        action = `/courses/${courseId}/groups/${courseGroupId}`;
+        method = 'patch';
+        courseGroupPromise = api(req).get('/courseGroups/', {
+            qs: {
+                $populate: ['userIds']
+            }
+        });
+    } else {
+        action = `/courses/${courseId}/groups`;
+        method = 'post';
+        courseGroupPromise = Promise.resolve({});
+    }
 
-  const studentsPromise = getSelectOptions(req, 'users', {roles: ['student', 'demoStudent'], $limit: 1000});
-  Promise.all([
-      courseGroupPromise,
-      studentsPromise
-  ]).then(([courseGroup, students]) => {
-      students = students.filter(s => s.schoolId === res.locals.currentSchool);
-      res.render('courses/edit-courseGroup', {
-          action,
-          method,
-          courseGroup,
-          courseId,
-          students: markSelected(students, _.map(courseGroup.userIds, '_id')),
-          title: req.params.courseGroupId ? 'Schülergruppe bearbeiten' : 'Schülergruppe anlegen',
-          submitLabel: req.params.courseGroupId ? 'Änderungen speichern' : 'Schülergruppe anlegen',
-          closeLabel: 'Abbrechen'
-      });
-  });
+    const studentsPromise = getSelectOptions(req, 'users', { roles: ['student', 'demoStudent'], $limit: 1000 });
+    Promise.all([
+        courseGroupPromise,
+        studentsPromise
+    ]).then(([courseGroup, students]) => {
+        students = students.filter(s => s.schoolId === res.locals.currentSchool);
+        res.render('courses/edit-courseGroup', {
+            action,
+            method,
+            courseGroup,
+            courseId,
+            students: markSelected(students, _.map(courseGroup.userIds, '_id')),
+            title: req.params.courseGroupId ? 'Schülergruppe bearbeiten' : 'Schülergruppe anlegen',
+            submitLabel: req.params.courseGroupId ? 'Änderungen speichern' : 'Schülergruppe anlegen',
+            closeLabel: 'Abbrechen'
+        });
+    });
 };
 
 // secure routes
@@ -62,13 +62,62 @@ router.use(authHelper.authChecker);
 
 router.get('/add', editCourseGroupHandler);
 
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
     api(req).post('/courseGroups/', {
         json: req.body // TODO: sanitize
     }).then(courseGroup => {
         res.redirect('/courses/' + req.params.courseId);
     }).catch(err => {
         res.sendStatus(500);
+    });
+});
+
+router.get('/:courseGroupId/', function(req, res, next) {
+    Promise.all([
+        api(req).get('/courseGroups/' + req.params.courseGroupId, {
+            qs: {
+                $populate: ['userIds']
+            }
+        }),
+        api(req).get('/lessons', {
+            qs: {
+                courseGroupId: req.params.courseGroupId,
+                $sort: 'position'
+            }
+        }),
+        // just for teacher details
+        api(req).get('/courses/' + req.params.courseId, {
+            qs: {
+                $populate: ['teacherIds']
+            }
+        })
+    ]).then(([courseGroup, lessons, course]) => {
+        lessons = lessons.data || [];
+
+        // get display names for teachers and students
+        _.each(courseGroup.userIds, u => u.displayName = `${u.firstName} ${u.lastName}`);
+        _.each(course.teacherIds, t => t.displayName = `${t.firstName} ${t.lastName}`);
+
+        res.render('courses/courseGroup', Object.assign({}, courseGroup, {
+            course,
+            title: courseGroup.name,
+            lessons,
+            breadcrumb: [{
+                    title: 'Meine Kurse',
+                    url: '/courses'
+                },
+                {
+                    title: course.name,
+                    url: '/courses/' + course._id
+                }
+            ],
+        }));
+    });
+});
+
+router.delete('/:courseGroupId', function(req, res, next) {
+    api(req).delete('/courseGroups/' + req.params.courseGroupId).then(_ => {
+        res.sendStatus(200);
     });
 });
 
