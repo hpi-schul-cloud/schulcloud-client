b8858c1862f54178ab43dfad90bf83ae81e7a35f
Langleu <langleu90@gmail.com>
2018-01-25 11:31:37 +0100

    switch to saucelabs

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -6,12 +6,7 @@ const SCREENSHOT_PATH = path.join(__dirname, "/screenshots/");
 
 // we use a nightwatch.conf.js file so we can include comments and helper functions
 module.exports = {
-  "src_folders": [
-      "test/nightwatch/login",
-      "test/nightwatch/homework",
-      "test/nightwatch/news",
-      "test/nightwatch/courses"
-  ],
+  "src_folders": ["test/nightwatch"],
   "output_folder": "./reports", // reports (test outcome) output by nightwatch
   "selenium": {
     "start_process": true, // tells nightwatch to start/stop the selenium process
