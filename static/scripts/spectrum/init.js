6f6e2bbeffe604e04096bb03e6636ff79606e30f
adrianjost <homepage@hackedit.de>
2018-02-20 17:12:39 +0100

    new colors

--- static/scripts/spectrum/init.js
+++ static/scripts/spectrum/init.js
@@ -4,24 +4,40 @@ $(document).ready(function () {
         showPalette: true,
         hideAfterPaletteSelect: true,
         palette: [
-            ['#F44336',
-                '#EC407A',
-                '#B71C1C',
-                '#AD1457',
-                '#BA68C8',
-                '#4A148C'],
-            ['#9C27B0',
-                '#673AB7',
-                '#304FFE',
-                '#2196F3',
-                '#03A9F4',
-                '#00BCD4'],
-            ['#009688',
-                '#4CAF50',
-                '#8BC34A',
-                '#C0CA33',
-                '#607D8B',
-                '#795548']
+            //dark
+            [   '#9a0007',
+                '#bb4d00',
+                '#c17900',
+                '#8c9900',
+                '#005005',
+                '#005662',
+                '#004ba0',
+                '#002984',
+                '#29434e',
+                '#4b2c20'],
+            //normal
+            [   '#d32f2f',  // rot
+                '#f57c00',  // orange
+                '#f9a825',  // gelb
+                '#c0ca33',  // gelb-grün
+                '#2e7d32',  // grün
+                '#00838f',  // türkis
+                '#1976d2',  // blau
+                '#3f51b5',  // lila
+                '#546e7a',  // blau-grau
+                '#795548'], // braun
+            //light
+            [   '#ff6659',
+                '#ffad42',
+                '#ffd95a',
+                '#b2ca70',
+                '#60ad5e',
+                '#4fb3bf',
+                '#6d9aff',
+                '#757de8',
+                '#819ca9',
+                '#a98274']
+
         ],
         change: function(color) {
             $(this).val(color.toHexString());
