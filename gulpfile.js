c568fcc270dcc8bf6661a262e1e1d5f5da11912b
adrianjost <homepage@hackedit.de>
2018-02-25 19:34:41 +0100

    added documentation + using sass import instead of css import

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task('styles', () => {
     var themeFile = `./theme/${themeName()}/style.scss`;
     beginPipe('./static/styles/**/*.{css,sass,scss}')
         .pipe(sassGrapher.ancestors())
-        .pipe(header(fs.readFileSync(themeFile, 'utf8')+"// don't remove me! "))
+        .pipe(header(fs.readFileSync(themeFile, 'utf8')+"// don't remove me! \n")) // READ: https://github.com/schul-cloud/schulcloud-client/pull/588
         .pipe(sass({sourceMap: false}))
         .pipe(minify())
         .pipe(autoprefixer({ browsers: ['last 3 major versions'] }))
