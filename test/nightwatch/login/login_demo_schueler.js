1730a83362e24f5872efb91594518534fda21ca9
Langleu <langleu90@gmail.com>
2018-03-21 10:39:25 +0100

    clear cookies before new run

--- test/nightwatch/login/login_demo_schueler.js
+++ test/nightwatch/login/login_demo_schueler.js
@@ -3,6 +3,7 @@ const sauce = require('../../sauce');
 
 module.exports = {
     'Schul-Cloud Reachable': function (browser) {
+        browser.deleteCookies();
         browser
             .url(base_url + 'login/')
             .waitForElementVisible('body', 1000);
