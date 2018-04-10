82baca6ea1543331e7ec048017604b756469b653
dominikglandorf <do.gl@gmx.de>
2018-04-10 16:45:13 +0200

    use server as hydra proxy

--- controllers/consent.js
+++ controllers/consent.js
@@ -1,46 +1,7 @@
 const express = require('express')
-const Hydra = require('ory-hydra-sdk')
-const router = express.Router()
-const OAuth2 = require('simple-oauth2')
-const qs = require('querystring')
-const api = require('../api')
-const auth = require('../helpers/authentication')
-
-const scope = 'hydra.consent'
-const oauth2 = OAuth2.create({
-  client: {
-    id: qs.escape('consent-app'),
-    secret: qs.escape('consent-secret')
-  },
-  auth: {
-    tokenHost: endpoint = 'http://ory-hydra-example--hydra:4444',
-    authorizePath: authorizePath = '/oauth2/auth',
-    tokenPath: tokenPath = '/oauth2/token'
-  },
-  options: {
-    useBodyAuth: false,
-    useBasicAuthorizationHeader: true
-  }
-})
-
-
-Hydra.ApiClient.instance.basePath = 'http://ory-hydra-example--hydra:4444'
-
-const hydra = new Hydra.OAuth2Api()
-
-const refreshToken = () => oauth2.clientCredentials
-  .getToken({ scope })
-  .then((result) => {
-    const token = oauth2.accessToken.create(result);
-    const hydraClient = Hydra.ApiClient.instance
-    hydraClient.authentications.oauth2.accessToken = token.token.access_token
-    return Promise.resolve(token)
-  })
-  .catch((error) => {
-    console.error('Could not refresh access token', error.message);
-  });
-
-refreshToken().then()
+const router = express.Router();
+const auth = require('../helpers/authentication');
+const api = require('../api');
 
 // A simple error helper
 const catcher = (w) => (error) => {
@@ -61,73 +22,37 @@ const resolver = (resolve, reject) => (error, data, response) => {
 
 // This get's executed when we want to tell hydra that the user is authenticated and that he authorized the application
 const resolveConsent = (r, w, consent, grantScopes = []) => {
-  const subject = w.locals.currentUser._id
-  const idTokenExtra = {}
-
   // Sometimes the body parser doesn't return an array, so let's fix that.
   if (!Array.isArray(grantScopes)) {
     grantScopes = [grantScopes]
   }
 
-  // TODO: include users pseudonym specific for consumer
-  // This is the openid 'profile' scope which should include some user profile data. (optional)
-  if (grantScopes.indexOf('pseudonym') >= 0) {
-    idTokenExtra.pseudonym = 'achdbsd'
-  }
-
-  refreshToken().then(() => {
-    // Do not return this directly, otherwise `then()` will be called, causing superagent to fail with the double
-    // callback bug.
-    hydra.getOAuth2ConsentRequest(r.query.consent,
-      resolver(
-        (consentRequest) => hydra.acceptOAuth2ConsentRequest(r.query.consent, {
-            subject,
-            grantScopes,
-            idTokenExtra,
-            accessTokenExtra: {}
-          },
-          resolver(() => w.redirect(consentRequest.redirectUrl), catcher(w))
-        ),
-        catcher(w)
-      )
-    )
-  })
+  return api(r).patch('/oauth2proxy/consentRequest/' + r.query.consent, {
+    json: {
+      subject: w.locals.currentUser._id,
+      grantScopes
+    }
+  }).then(consentRequest => w.redirect(consentRequest.redirectUrl));
 }
 
 router.get('/consent', auth.authChecker, (r, w) => {
   // This endpoint is hit when hydra initiates the consent flow
   if (r.query.error) {
     // An error occurred (at hydra)
-    return w.render('error', { error: { name: r.query.error, message: r.query.error_description } })
+    return w.send(`${r.query.error}<br />${r.query.error_description}`);
   }
 
-  refreshToken().then(() => {
-    // Do not return this directly, otherwise `then()` will be called, causing superagent to fail with the double
-    // callback bug.
-    hydra.getOAuth2ConsentRequest(r.query.consent, resolver((consentRequest) => {
-      // consentRequest contains informations such as requested scopes, client id, ...
-      console.log(consentRequest);
-      // Here you could, for example, allow clients to force a user's consent. Since you're able to
-      // say which scopes a client can request in hydra, you could allow this for a few highly priviledged clients!
-      //
-      // if (consentRequest.scp.find((s) => s === 'force-consent')) {
-      //   resolveConsent(r, w, r.query.consent, consentRequest.requestedScopes)
-      //   return Promise.resolve()
-      // }
-
-      // render the consent screen
-      return w.render('consent/consent', {
-        inline: true,
-        title: 'Login mit Schul-Cloud',
-        subtitle: '',
-        client: consentRequest.clientId,
-        action: `/consent/consent?consent=${r.query.consent}`,
-        buttonLabel: 'Akzeptieren',
-        scopes: consentRequest.requestedScopes
-      });
-
-    }, catcher(w)))
-  })
+  return api(r).get('/oauth2proxy/consentRequest/' + r.query.consent).then(consentRequest => {
+    return w.render('consent/consent', {
+      inline: true,
+      title: 'Login mit Schul-Cloud',
+      subtitle: '',
+      client: consentRequest.clientId,
+      action: `/consent/consent?consent=${r.query.consent}`,
+      buttonLabel: 'Akzeptieren',
+      scopes: consentRequest.requestedScopes
+    })
+  }).catch(error => console.log(error))
 })
 
 router.post('/consent', auth.authChecker, (r, w) => {
