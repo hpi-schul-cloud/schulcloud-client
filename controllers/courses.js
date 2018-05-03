acc261c2d8ed7e15915b4b62b7dcdc4f4c2ed4a7
schurigh <hannes.schurig@hpi.de>
2018-05-03 16:37:06 +0200

    fix course listing, show only users courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -157,14 +157,7 @@ router.get('/', function(req, res, next) {
                 substitutionIds: res.locals.currentUser._id
             }
         }),
-        api(req).get('/courses/', {
-            qs: {
-                $or: [
-                    { userIds: res.locals.currentUser._id },
-                    { teacherIds: res.locals.currentUser._id }
-                ]
-            }
-        })
+        api(req).get('/courses/', {})
     ]).then(([substitutionCourses, courses]) => {
         substitutionCourses = substitutionCourses.data.map(course => {
             course.url = '/courses/' + course._id;
