7039b42cba578616ee90cad39af4135e78b056a6
Katrin Klein <katrin_klein@icloud.com>
2018-05-16 13:00:22 +0200

    Revert "submit problem multiple"
    This reverts commit 292bb38531e71a568abe9e1dc771fe42d622801b.

--- static/scripts/loggedin.js
+++ static/scripts/loggedin.js
@@ -132,7 +132,6 @@ $(document).ready(function () {
         });
 
         $feedbackModal.find('.modal-form').on('submit', sendFeedback.bind(this, $feedbackModal));
-        
         $feedbackModal.modal('show');
         $feedbackModal.find('#title-area').html(area);
     });
@@ -146,11 +145,10 @@ $(document).ready(function () {
             closeLabel: 'Abbrechen',
             submitLabel: 'Senden'
         });
-        
+
+        $problemModal.find('.modal-form').on('submit', sendFeedback.bind(this, $problemModal));
         $problemModal.modal('show');
     });
-    
-    $problemModal.find('.modal-form').on('submit', sendFeedback.bind(this, $problemModal));
 
     $modals.find('.close, .btn-close').on('click', function () {
         $modals.modal('hide');
