f112ee3446f3ed0b6d88bc0799f8b22882b969b4
Jan Renz <jan.renz@gmail.com>
2018-05-25 18:00:54 +0200

    Theming on build time (#688)
    * fix footer in safari, fixes course add button
    
    * theme specific build dir
    
    * fix volume mounting

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build/'+themeName)));
 
 const sessionStore = new session.MemoryStore;
 app.use(session({
