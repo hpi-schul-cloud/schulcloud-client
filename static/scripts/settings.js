4cee83fede3cd0aae953c46e2e02a34608f67beb
Langleu <langleu90@gmail.com>
2018-06-26 11:23:16 +0200

    fix modal hiding behind fade

--- static/scripts/settings.js
+++ static/scripts/settings.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
         e.preventDefault();
         var $buttonContext = $(this);
 
-        $deleteModal.modal('show');
+        $deleteModal.appendTo('body').modal('show');
         $deleteModal.find('.modal-title').text("Bist du dir sicher, dass du '" + $buttonContext.data('device-name') + "' löschen möchtest?");
 
         $deleteModal.find('.btn-submit').unbind('click').on('click', function() {
--- static/scripts/socket.js
+++ static/scripts/socket.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
                 url: "/account/preferences",
                 data: { attribute: { key: "releaseDate", value: res.createdAt } }
             });
-            $('.feature-modal').modal('show');
+            $('.feature-modal').appendTo('body').modal('show');
         }
     });
 });
