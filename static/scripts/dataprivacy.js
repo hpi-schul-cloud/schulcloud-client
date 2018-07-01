6f202852e8b57cf0f946d7432d1a310de62a8b26
adrianjost <homepage@hackedit.de>
2018-07-01 09:36:31 +0200

    improved input validation

--- static/scripts/dataprivacy.js
+++ static/scripts/dataprivacy.js
@@ -41,8 +41,11 @@ function setSelectionByIndex(index, event){
         document.querySelector('.form input[type="radio"]:nth-of-type(' + index + ')').checked = true;
         updateButton(index);
     }else{
-        event.preventDefault();
         document.querySelector(`section[data-panel="section-${getSelectionIndex()}"]`).classList.add("showInvalid");
+        if(event){
+            event.preventDefault();
+            document.querySelector(".content-wrapper").scrollTo(0,0);
+        }
     }
 }
 function updateButton(selectedIndex){
@@ -76,14 +79,15 @@ function nextSection(event){
 }
 function prevSection(event) {
     const selectedIndex = getSelectionIndex() - 1;
-    setSelectionByIndex(selectedIndex, event);
+    setSelectionByIndex(selectedIndex); // don't give this method to prevent, to don't prevent on invalid state (back should always be possible)
+}
+function goToSection(event){
+    const selectedIndex = document.querySelectorAll('.form .stages label').indexOf(this) + 1;
+    setSelectionByIndex(selectedIndex, (selectedIndex >= getSelectionIndex())?event:undefined);
 }
 window.addEventListener('DOMContentLoaded', ()=>{
     // Stepper
-    document.querySelectorAll('.form .stages label').addEventListener("click", function(event) {
-        const selectedIndex = document.querySelectorAll('.form .stages label').indexOf(this) + 1;
-        setSelectionByIndex(selectedIndex, event);
-    });
+    document.querySelectorAll('.form .stages label').addEventListener("click", goToSection);
     document.querySelector('.form #prevSection').addEventListener("click", prevSection);
     document.querySelector('.form #nextSection').addEventListener("click", nextSection);
 });
@@ -103,22 +107,31 @@ window.addEventListener('DOMContentLoaded', ()=>{
     document.querySelectorAll(".linked").addEventListener("change keyup paste click", linkInputs);
 });
 
+/* DATE PICKER */
+function readPickerConfig(input){
+    return {
+        format: ($(input).data('datetime')?'d.m.Y H:i':'d.m.Y'),
+        mask: ($(input).data('datetime')?'39.19.9999 29:59':'39.19.9999'),
+        timepicker: ($(input).data('datetime') || false),
+        startDate: ($(input).data('startDate') || false),
+        minDate: ($(input).data('minDate') || 'yesterday'), //yesterday is minimum date(for today use 0 or -1970/01/01)
+        maxDate: ($(input).data('maxDate') || 'tomorrow')  //tomorrow is maximum date calendar
+    }
+}
+window.addEventListener('load', ()=>{
+    // https://xdsoft.net/jqplugins/datetimepicker/
+    $.datetimepicker.setLocale('de');
+    document.querySelectorAll('input[data-date], input[data-datetime]').forEach((input)=>{
+        $(input).datetimepicker(readPickerConfig(input));
+        input.setAttribute("autocomplete","off");
+        // modified regex from: https://www.regextester.com/97612
+        input.setAttribute("pattern",$(input).data('datetime')?
+            `(3[01]|[12][0-9]|0?[1-9])\\.(1[012]|0?[1-9])\\.((?:19|20)[0-9]{2})[[:space:]][0-2][0-3]:[0-5][0-9]`:
+            `(3[01]|[12][0-9]|0?[1-9])\\.(1[012]|0?[1-9])\\.((?:19|20)[0-9]{2})`);
+    })
+});
 
 /* OTHER STUFF */
 window.addEventListener('load', ()=>{
-    // TIME Picker
-    // https://xdsoft.net/jqplugins/datetimepicker/
-    $.datetimepicker.setLocale('de');
-    $('input[data-datetime]').datetimepicker({ 
-        format:'d.m.Y H:i',
-        mask: '39.19.9999 29:59'
-    });
-    $('input[data-date]').datetimepicker({
-        format:'d.m.Y',
-        timepicker:false,
-        mask: '39.19.9999',
-        startDate: ($('input[data-date]').data('startDate') || false),
-        minDate: ($('input[data-date]').data('minDate') || 'yesterday'), //yesterday is minimum date(for today use 0 or -1970/01/01)
-        maxDate: ($('input[data-date]').data('maxDate') || 'tomorrow')  //tomorrow is maximum date calendar
-    });
+    // INSERT CUSTOM SCRIPTS HERE
 });
