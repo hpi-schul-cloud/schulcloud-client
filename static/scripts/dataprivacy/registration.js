5441e795782424b7fa624134ee3ff34b5d52cc36
schurigh <hannes.schurig@hpi.de>
2018-07-06 13:18:33 +0200

    welcome screen final

--- static/scripts/dataprivacy/registration.js
+++ static/scripts/dataprivacy/registration.js
@@ -2,18 +2,26 @@ window.addEventListener('DOMContentLoaded', ()=>{
     // show steppers depending on age of student
     let birthdateInput = document.querySelector('input[name="student-age"]');
     let showFormButton = document.querySelector('#showRegistrationForm');
+    let radiou18 = document.getElementById("reg-u18");
+    let radio18 = document.getElementById("reg-18");
 
     if(birthdateInput && showFormButton) {
-        alert("a");
         document.querySelector('#showRegistrationForm').addEventListener("click", ()=>{
-            alert("b");
             const baseUrl = `/administration/dataprivacy/registration`;
             
-            let radiou18 = document.getElementById("reg-u18");
             if(radiou18.checked){
                 window.location.href = `${baseUrl}/byparent`;
             }else{
                 window.location.href = `${baseUrl}/bystudent`;
+            }
+        });
+        $("input[type='radio']").on("change", () => {
+            if(radio18.checked) {
+                document.getElementById("infotext-18").style.display = "block";
+                document.getElementById("infotext-u18").style.display = "none";
+            } else {
+                document.getElementById("infotext-18").style.display = "none";
+                document.getElementById("infotext-u18").style.display = "block";
             }
         });
     }
