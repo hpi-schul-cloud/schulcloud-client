2a7d46412933dd313919cf31ec9aed1621529416
Jonas Keutel <jonas.keutel@student.hpi.de>
2018-07-12 11:42:12 +0200

    [WIP] use vue in my-material view

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { VueLoaderPlugin } = require('vue-loader');
 const webpack = require("webpack");
 
 module.exports = {
@@ -14,7 +15,11 @@ module.exports = {
                 },
             },
             // moment needs to be globally exposed in order to work with fullcalendar
-            { test: require.resolve('moment'), loader: 'expose-loader?moment' }
+            { test: require.resolve('moment'), loader: 'expose-loader?moment' },
+            {
+              test: /\.vue$/,
+              loader: 'vue-loader',
+            },
         ]
     },
     optimization: {
@@ -24,6 +29,11 @@ module.exports = {
                 react: {
                   test: /[\\/]node_modules[\\/](react\-dom|react)[\\/]/,
                   name: 'vendor-react',
+                  chunks: 'all',
+                },
+                vue: {
+                  test: /[\\/]node_modules[\\/](vue)[\\/]/,
+                  name: 'vendor-vue',
                   chunks: 'all',
                 },
             }
@@ -41,5 +51,6 @@ module.exports = {
         // By default, moment loads aaaall the locale files, which bloats the bundle size
         // This plugin forces moment to only load the German locale
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /de/),
+        new VueLoaderPlugin()
     ]
 };
