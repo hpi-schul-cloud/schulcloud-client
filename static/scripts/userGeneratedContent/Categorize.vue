6dce568a81ba38fb39b7eec24581091dcacdc4da
Jonas Keutel <jonas.keutel@student.hpi.de>
2018-07-17 20:57:23 +0200

    [WIP] use vue in my-material view

---
+++ static/scripts/userGeneratedContent/Categorize.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+
+    sdkfjkljsdf
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="css" >
+</style>
