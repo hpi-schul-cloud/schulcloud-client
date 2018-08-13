48e2c242097336c27099176cb6f2e147b3aebfe6
Thomas <thomas.feldtkeller@student.hpi.de>
2018-08-13 14:58:05 +0200

    serverside registration submit

--- controllers/registration.js
+++ controllers/registration.js
@@ -39,98 +39,13 @@ router.post('/registration/pincreation', function (req, res, next) {
 });
 
 router.post('/registration/submit', function (req, res, next) {
-
-    let pininput = req.body["email-pin"]; 
-    let usermail = req.body["parent-email"] ? req.body["parent-email"] : req.body["student-email"];
-    let parent = null;
-    let user;
-
-    let passwort = req.body["initial-password"];
-    return api(req).get('/registrationPins/', {
-        qs: { "pin": pininput, "email": usermail, verified:false }
-    }).then(check => {
-        //check pin
-        if (!(check.data && check.data.length>0 && check.data[0].pin === pininput)) {
-            return Promise.reject("Ungültige Pin, bitte überprüfe die Eingabe.");
-        }
-        if (req.body["parent-email"] && req.body["parent-email"] === req.body["student-email"]) {
-            return Promise.reject("Bitte gib eine eigene E-Mail-Adresse für dein Kind an.");
-        }
-        return Promise.resolve;
-    }).then(function() {
-        //create user
-        user = {
-            firstName: req.body["student-firstname"],
-            lastName: req.body["student-secondname"],
-            email: req.body["student-email"],
-            gender: req.body["gender"],
-            roles: ["student"],
-            classId: req.body.classId,
-            birthday: new Date(req.body["student-birthdate"])
-        };
-        return api(req).post('/users/', {
-            json: user,
-			qs:{email:req.body["parent-email"]}
-        }).catch(err =>{
-            return Promise.reject("Fehler beim Erstellen des Schülers. Eventuell ist die E-Mail-Adresse bereits im System registriert.");
-        });
-    }).then(newUser => {
-        user = newUser;
-        // create account
-        return createAccount(req, {username: user.email, password: passwort, userId: user._id, activated: true});
-    }).then(res => {
-        //add parent if necessary    
-        if(req.body["parent-email"]) {
-            parent = {
-                firstName: req.body["parent-firstname"],
-                lastName: req.body["parent-secondname"],
-                email: req.body["parent-email"],
-                children: [user._id],
-                schoolId: user.schoolId,
-                roles: ["parent"]
-            };
-            return api(req).post('/users/', {
-                json: parent
-            }).then(newParent => {
-                parent = newParent;
-            }).catch(err => {
-                if (err.error.message==="parentCreatePatch") {
-                    return Promise.resolve();
-                } else {
-                    return Promise.reject(new Error("Fehler beim Erstellen des Elternaccounts."));
-                }
-            });
-        } else {
-            return Promise.resolve;
-        }
-    }).then(function(){
-        console.log("CONSENT");
-        //store consent
-        let consent = {
-            form: 'digital',
-            privacyConsent: req.body.Erhebung,
-            thirdPartyConsent: req.body.Pseudonymisierung,
-            termsOfUseConsent: Boolean(req.body.Nutzungsbedingungen),
-            researchConsent: req.body.Forschung
-        };
-        if (parent) {
-            consent.parentId = parent._id;
-            return api(req).post('/consents/', {
-                json: {
-                    userId: user._id,
-                    parentConsents: [consent]
-                }
-            });
-        } else {
-            return api(req).post('/consents/', {
-                json: {userId: user._id, userConsent: consent}
-            });
-        }
-    }).then(function() {
+    return api(req).post('/registration/', {
+        json: req.body  
+    }).then(response => {   
         //send Mails
-        let eMailAdresses = [user.email];
-        if(parent){
-            eMailAdresses.push(parent.email);
+        let eMailAdresses = [response.user.email];
+        if(response.parent){
+            eMailAdresses.push(response.parent.email);
         }
         eMailAdresses.forEach(eMailAdress => {
             return api(req).post('/mails/', {
@@ -138,15 +53,12 @@ router.post('/registration/submit', function (req, res, next) {
                         subject: `Willkommen in der ${res.locals.theme.title}!`,
                         headers: {},
                         content: {
-                            "text": `Hallo ${user.firstName}
-
+                            "text": `Hallo ${response.user.firstName}
 mit folgenden Anmeldedaten kannst du dich in der ${res.locals.theme.title} einloggen:
 Adresse: ${req.headers.origin || process.env.HOST}
-E-Mail: ${user.email}
-Startpasswort: ${passwort}
-
+E-Mail: ${response.user.email}
+Startpasswort: ${req.body["initial-password"]}
 Nach dem ersten Login musst du ein persönliches Passwort festlegen. Wenn du zwischen 14 und 18 Jahre alt bist, bestätige bitte zusätzlich die Einverständniserklärung, damit du die ${res.locals.theme.short_title} nutzen kannst.
-
 Viel Spaß und einen guten Start wünscht dir dein
 ${res.locals.theme.short_title}-Team`
                         }
@@ -156,27 +68,7 @@ ${res.locals.theme.short_title}-Team`
     }).then(function () {
         res.sendStatus(200);
     }).catch(err => {
-
-        /*
-        //If user for student created, remove that user, cannot do this bc no rights to do this
-        let userpromise = api(req).get('/users/', {
-            qs: {email: req.body["student-email"]}
-        }).then(students => {
-            return api(req).delete('/users/' + students._id);
-        });
-
-        //If account for student created, remove that account, cannot do this bc no rights to do this
-        let accpromise = api(req).get('/accounts/', {
-            qs: {email: req.body["student-email"]}
-        }).then(students => {
-            return api(req).delete('/accounts/' + students._id);
-        });
-
-        Promise.all([userpromise, accpromise]).then(()=>{*/
-            res.status(500).send((err.error||{}).message || err.message || "Fehler bei der Registrierung."); // TODO: Errorhandling /account/ is used even when error occurs
-        /*}).catch(err => {
-            res.status(500).send((err.error||{}).message || err.message || "Fehler bei der Registrierung.");
-        });*/
+        res.status(500).send((err.error||{}).message || err.message || "Fehler bei der Registrierung.");
     });
 });
 
