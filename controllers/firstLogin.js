9ace6e3e00d835e107b43378ab23ddcc976850e9
schurigh <hannes.schurig@hpi.de>
2018-08-20 18:39:56 +0200

    firstlogin fixed final

--- controllers/firstLogin.js
+++ controllers/firstLogin.js
@@ -51,10 +51,10 @@ router.post('/submit', function (req, res, next) {
     let accountUpdate = {};
     let accountPromise = Promise.resolve();
     let userUpdate = {};
-    let userPromise = Promise.resolve();
+    let userPromise;
     let consentUpdate = {};
     let consentPromise = Promise.resolve();
-
+    
     if (req.body["password-1"]) {
         accountUpdate.password_verification = req.body.password_verification;
         accountUpdate.password = req.body["password-1"];
@@ -68,18 +68,21 @@ router.post('/submit', function (req, res, next) {
         let dateArr = req.body.studentBirthdate.split(".");
         let userBirthday = new Date(`${dateArr[1]}.${dateArr[0]}.${dateArr[2]}`);
         if(userBirthday instanceof Date && isNaN(userBirthday)) {
-            return Promise.reject("Fehler bei der Erkennung des ausgewählten Geburtstages. Bitte lade die Seite neu und starte erneut.");
+            res.status(500).send("Bitte einen validen Geburtstag auswählen.");
         }
         userUpdate.birthday = userBirthday;
     }
+    // malformed email?
     if (req.body["student-email"]) {
         var regex = RegExp("^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
-        if (!regex.test(hook.data.email)) {
-            return Promise.reject(new errors.BadRequest('Bitte gib eine valide E-Mail Adresse an!'));
+        if (!regex.test(req.body["student-email"])) {
+            res.status(500).send("Bitte eine valide E-Mail-Adresse eingeben.");
+        } else {
+            userUpdate.email = req.body["student-email"];
         }
-        userUpdate.email = req.body["student-email"];
     }
-    var preferences = res.locals.currentUser.preferences || {};
+    
+    let preferences = res.locals.currentUser.preferences || {};
     preferences.firstLogin = true;
     userUpdate.preferences = preferences;
 
@@ -104,51 +107,13 @@ router.post('/submit', function (req, res, next) {
     }
 
     return Promise.all([accountPromise, userPromise, consentPromise])
-            // Nutzerdatenmails vorerst nicht zuschicken. Passwort muss erst verschlüsselt werden.
-        /*
-        .then(async () => {
-        if (req.body["sendCredentials"]){
-            let mailcontent = {
-                json: { email: res.locals.currentUser.email,
-                        subject: `Deine Zugangsdaten für die ${res.locals.theme.title}!`,
-                        headers: {},
-                        content: {
-                            "text": `Hallo ${res.locals.currentUser.displayName}
-
-mit folgenden Anmeldedaten kannst du dich in der ${res.locals.theme.title} einloggen:
-
-Adresse: ${req.headers.origin || process.env.HOST}
-E-Mail: ${res.locals.currentUser.email}
-Passwort: ${req.body["password-1"]}
-
-Bitte bewahre die Zugangsdaten auf.
-
-Viel Spaß wünscht dir dein 
-${res.locals.theme.short_title}-Team`,
-                            "html": ""
-                        }
-                }
-            }
-
-            try {
-                await api(req).post('/mails/', mailcontent);
-                res.status(200).json({type: 'success', message: `Die Zugangsdaten wurden erfolgreich an ${res.locals.currentUser.email} verschickt.`});    
-            } catch (err) {
-                // console.warn("Mailing fehlgeschlagen, zweiter Versuch");
-                // console.error("ERR: " + err);
-                try {
-                    await api(req).post('/mails/', mailcontent);
-                    res.status(200).json({type: 'success', message: `Die Zugangsdaten wurden erfolgreich an ${res.locals.currentUser.email} verschickt.`});
-                } catch (err) {
-                    res.status(200).json({type: 'danger', message: `Die Zugangsdaten konnten nicht an ${res.locals.currentUser.email}  verschickt werden. Bitte notiere dir dein Passwort selbst.`});
-                }
-            }
-        }
-
-    })*/
-    .catch(err => {
-        res.status(500).send((err.error || err).message || "Ein Fehler ist bei der Verarbeitung der FirstLogin Daten aufgetreten.");
-    });
+        .then(([resAccount,resUser,resConsent]) => {
+            let a = 1;
+            res.sendStatus(200);
+        })
+        .catch(err => {
+            res.status(500).send((err.error || err).message || "Ein Fehler ist bei der Verarbeitung der FirstLogin Daten aufgetreten.");
+        });
 });
 router.get('/existingU14', function (req, res, next) {
     res.render('firstLogin/firstLoginExistingUserU14', {
