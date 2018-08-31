1d4c341668e5998ef0e6929c4a0ac9e21615346e
Alexander Kremer <alex@kremer.io>
2018-08-31 17:46:21 +0200

    Fix merge conflicts

--- test/nightwatch/login/login_demo_schueler.js
+++ test/nightwatch/login/login_demo_schueler.js
@@ -68,11 +68,7 @@ module.exports = {
 
         //Content
         browser.url(base_url + 'content/');
-<<<<<<< HEAD
-        browser.expect.element('#titlebar h4#page-title').text.to.contain('Materialien').before(10000);
-=======
-        browser.expect.element('#titlebar > div.col-sm-9 > div:nth-child(1) > h4').text.to.contain('LernStore').before(10000);
->>>>>>> master
+        browser.expect.element('#titlebar h4#page-title').text.to.contain('LernStore').before(10000);
 
         //Settings
         browser.url(base_url + 'account/');
