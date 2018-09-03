7514d14aa3b4a4ba6a468d0eb961b825da3e7b99
Tobias Wollowski <tobias@jbbr.net>
2018-09-03 15:00:58 +0200

    recvert calendar

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -56,15 +56,6 @@ $(document).ready(function () {
         timezone: 'UTC',
         events: function (start, end, timezone, callback) {
             $.getJSON('/calendar/events/',
-            const updatesChannel = new BroadcastChannel('event-updates');
-            updatesChannel.addEventListener('message', async (event) => {
-                const {cacheName, updatedUrl} = event.data.payload;
-                console.log('cache updated', cacheName, updatedUrl); 
-                const cache = await caches.open(cacheName);
-                const updatedResponse = await cache.match(updatedUrl);
-                const updatedText = await updatedResponse.text();
-                callback(updatedText);
-            });
                 function (events) {
                     callback(events);
                 });
