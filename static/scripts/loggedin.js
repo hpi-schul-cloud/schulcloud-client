3a6878e0c3668d6c78422a536c6601752e24c904
Tobias Wollowski <tobias@jbbr.net>
2018-09-17 13:00:35 +0200

    do not install service worker by default

--- static/scripts/loggedin.js
+++ static/scripts/loggedin.js
@@ -289,8 +289,8 @@ function startIntro() {
     .start();
 }
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js');
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker.register('/sw.js');
+//     });
+// }
