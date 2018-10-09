4d087525a249a91977defaa447143cfe97d6da73
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-10-10 01:08:15 +0200

    Remove old schic stuff

--- controllers/schics.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router({ mergeParams: true });
-
-router.get("/", (req, res, next) => {
-  res.render("schics/schic", {title: 'Schics'});
-});
-
-module.exports = router;
