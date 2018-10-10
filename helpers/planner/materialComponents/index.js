ef33fe527fad6f2ba6cac4c29967eb473a878f3d
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-10-10 17:11:35 +0200

    Remove Headline component

--- helpers/planner/materialComponents/index.js
+++ helpers/planner/materialComponents/index.js
@@ -1,23 +1,21 @@
-import ExpansionPanel from './ExpansionPanel';
-import Select from './Select';
-import Tabs from './Tabs';
-import Headline from './Headline';
-import Label from './Label';
-import Button from './Button';
-import IconButton from './IconButton';
-import Text from './Text';
-import TextField from './TextField';
-import TextArea from './TextArea';
-import Chip from './Chip';
-import SelectorInput from './SelectorInput';
-import TextFieldTable from './TextFieldTable';
-import TopicElement from './TopicElement';
+import ExpansionPanel from "./ExpansionPanel";
+import Select from "./Select";
+import Tabs from "./Tabs";
+import Label from "./Label";
+import Button from "./Button";
+import IconButton from "./IconButton";
+import Text from "./Text";
+import TextField from "./TextField";
+import TextArea from "./TextArea";
+import Chip from "./Chip";
+import SelectorInput from "./SelectorInput";
+import TextFieldTable from "./TextFieldTable";
+import TopicElement from "./TopicElement";
 
 export {
   ExpansionPanel,
   Select,
   Tabs,
-  Headline,
   Label,
   Button,
   IconButton,
