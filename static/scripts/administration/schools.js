da24629c2047526b4ecc57359c43d7166f6cd3c9
Kathi <katharina.blass@student.hpi.de>
2018-10-10 11:51:42 +0200

    renaming

--- static/scripts/administration/schools.js
+++ static/scripts/administration/schools.js
@@ -17,7 +17,7 @@ function transformToBase64(image_src){
         ctx.clearRect(0, 0, canvas.width, canvas.height);                  
         (img.width < img.height) ? scalingFactor = img.height / canvas.height : scalingFactor = img.width / canvas.width;                                                
         ctx.drawImage(img, canvas.width / 2 - img.width / scalingFactor / 2, canvas.height / 2 - img.height / scalingFactor / 2, img.width / scalingFactor, img.height / scalingFactor);
-        document.getElementsByName('logo_url')[0].value = canvas.toDataURL("image/png");
+        document.getElementsByName('logo_dataUrl')[0].value = canvas.toDataURL("image/png");
         document.querySelector('#preview-logo').src = canvas.toDataURL("image/png");
         document.querySelector('#logo-filename').innerHTML = 'Datei ausgewählt';
     }
