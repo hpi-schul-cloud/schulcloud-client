95be50ca81ff5ebb7f38536ac8451788cd670482
Adrian Jost <adrianjost@users.noreply.github.com>
2018-10-11 12:49:32 +0200

    Update gulpfile.js (#827)

--- gulpfile.js
+++ gulpfile.js
@@ -209,7 +209,7 @@ gulp.task('build-all', ['images', 'other', 'styles', 'fonts', 'scripts', 'base-s
   'vendor-styles', 'vendor-scripts', 'vendor-assets'
 ]);
 
-gulp.task('build-theme-files', ['styles']);
+gulp.task('build-theme-files', ['styles', 'images']);
 
 //watch and run corresponding task on change, process changed files only
 gulp.task('watch', ['build-all'], () => {
