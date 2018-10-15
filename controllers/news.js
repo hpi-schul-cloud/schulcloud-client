013cea180a5e1ed8b07931c9d66f02d3cfb72446
smith64fx <michael@schmidigital.de>
2018-10-15 14:06:12 +0200

    update dynamic routing

--- controllers/news.js
+++ controllers/news.js
@@ -11,203 +11,203 @@ const handlebars = require("handlebars");
 const moment = require("moment");
 moment.locale('de');
 
-module.exports = function(context = '') {
-    router.use(authHelper.authChecker);
+router.use(authHelper.authChecker);
 
-    const getActions = (item, path) => {
-        return [
-            {
-                link: path + item._id + "/edit",
-                class: 'btn-edit',
-                icon: 'edit',
-                alt: 'bearbeiten'
-            },
-            {
-                link: path + item._id,
-                class: 'btn-delete',
-                icon: 'trash-o',
-                method: 'delete',
-                alt: 'löschen'
-            }
-        ];
-    };
-
-    const getDeleteHandler = (service) => {
-        return function (req, res, next) {
-            api(req).delete('/' + service + '/' + req.params.id).then(_ => {
-                res.sendStatus(200);
-            }).catch(err => {
-                next(err);
-            });
-        };
-    };
-
-    router.post('/', function (req, res, next) {
-        let { body } = req;
-        if (body.displayAt && body.displayAt != "__.__.____ __:__") { // rewrite german format to ISO
-            body.displayAt = moment(body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
-        } else {
-            body.displayAt = undefined;
+const getActions = (item, path) => {
+    return [
+        {
+            link: path + item._id + "/edit",
+            class: 'btn-edit',
+            icon: 'edit',
+            alt: 'bearbeiten'
+        },
+        {
+            link: path + item._id,
+            class: 'btn-delete',
+            icon: 'trash-o',
+            method: 'delete',
+            alt: 'löschen'
         }
-        body.creatorId = res.locals.currentUser._id;
-        body.createdAt = moment().toISOString();
-
-        if(body.context) {
-            body.target = body.targetId;
-            body.targetModel = body.context;
-        }
-
-        api(req).post('/news/', {
-            // TODO: sanitize
-            json: body
-        }).then(data => {
-            if (body.context) {
-                res.redirect(`/${body.context}/` + body.targetId);
-            }
-            res.redirect('/news');
-        }).catch(err => {
-            next(err);
-        });
-    });
-    router.patch('/:newsId', function (req, res, next) {
-        api(req).get('/news/' + req.params.newsId, {}).then(orgNews => {
-            req.body.displayAt = moment(req.body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
-
-            const historyEntry = {
-                "title": orgNews.title,
-                "content": orgNews.content,
-                "displayAt": orgNews.displayAt,
-
-                "creatorId": (orgNews.updaterId) ? (orgNews.updaterId) : (orgNews.creatorId),
-                "parentId": req.params.newsId
-            };
-
-            api(req).post('/newshistory/', {
-                // TODO: sanitize
-                json: historyEntry
-            }).then(data => {
-                req.body.updaterId = res.locals.currentUser._id;
-                req.body.updatedAt = moment().toISOString();
-                orgNews.history.push(data._id);
-                req.body.history = orgNews.history;
-
-                api(req).patch('/news/' + req.params.newsId, {
-                    // TODO: sanitize
-                    json: req.body
-                }).then(data => {
-                    res.redirect('/news');
-                }).catch(err => {
-                    next(err);
-                });
-
-
-            }).catch(err => {
-                next(err);
-            });
-        }).catch(err => {
-            next(err);
-        });
-    });
-    router.delete('/:id', getDeleteHandler('news'));
-
-    router.all('/', function (req, res, next) {
-        const query = req.query.q;
-        const itemsPerPage = 9;
-        const currentPage = parseInt(req.query.p) || 1;
-
-        let queryObject = {
-            $limit: itemsPerPage,
-            displayAt: (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) ? {} : {$lte: new Date().getTime()},
-            $skip: (itemsPerPage * (currentPage -1)),
-            $sort: '-displayAt',
-            title: { $regex: query, $options: 'i' },
-            target: { $exists: false }
-        };
-
-        if (req.params.targetId) {
-            queryObject.target = req.params.targetId;
-        }
-
-        if (!query)
-            delete queryObject.title;
-
-        return api(req).get('/news/', { qs: queryObject })
-            .then(news => {
-            const totalNews = news.total;
-            const colors = ["F44336","E91E63","3F51B5","2196F3","03A9F4","00BCD4","009688","4CAF50","CDDC39","FFC107","FF9800","FF5722"];
-            news = news.data.map(news => {
-                news.url = '/news/' + news._id;
-                news.secondaryTitle = moment(news.displayAt).fromNow();
-                news.background = '#'+colors[(news.title||"").length % colors.length];
-                if (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) {
-                    news.actions = getActions(news, '/news/');
-                }
-                return news;
-            });
-            const pagination = {
-                currentPage,
-                numPages: Math.ceil(totalNews / itemsPerPage),
-                baseUrl: '/news/?p={{page}}'
-            };
-            let title = 'Neuigkeiten aus meiner Schule';
-            switch (context) {
-                case 'teams': {
-                    title = 'Neuigkeiten aus meinem Team';
-                }
-            }
-            res.render('news/overview', {
-                title,
-                news,
-                pagination,
-                searchLabel: 'Suche nach Neuigkeiten',
-                searchAction: '/news/',
-                showSearch: true
-            });
-        }).catch(err => {
-            next(err);
-        });
-    });
-
-    router.get('/new', function (req, res, next) {
-        res.render('news/edit', {
-            title: "News erstellen",
-            submitLabel: 'Hinzufügen',
-            closeLabel: 'Abbrechen',
-            method: 'post',
-            action: '/news/',
-            context,
-            targetId: req.params.targetId
-        });
-    });
-
-    router.get('/:newsId', function (req, res, next) {
-        api(req).get('/news/' + req.params.newsId, {
-            qs: {
-                $populate: ['creatorId', 'updaterId']
-            }
-        }).then(news => {
-            news.url = '/news/' + news._id;
-            res.render('news/article', {title: news.title, news });
-        }).catch(err => {
-            next(err);
-        });
-    });
-
-    router.get('/:newsId/edit', function (req, res, next) {
-        api(req).get('/news/' + req.params.newsId, {}).then(news => {
-            news.displayAt = moment(news.displayAt).format('DD.MM.YYYY HH:mm');
-            res.render('news/edit', {
-                title: "News bearbeiten",
-                submitLabel: 'Speichern',
-                closeLabel: 'Abbrechen',
-                method: 'patch',
-                action: '/news/' + req.params.newsId,
-                news
-            });
-        }).catch(err => {
-            next(err);
-        });
-    });
-
-    return router;
+    ];
 };
+
+const getDeleteHandler = (service) => {
+    return function (req, res, next) {
+        api(req).delete('/' + service + '/' + req.params.id).then(_ => {
+            res.sendStatus(200);
+        }).catch(err => {
+            next(err);
+        });
+    };
+};
+
+router.post('/', function (req, res, next) {
+    let { body } = req;
+    if (body.displayAt && body.displayAt != "__.__.____ __:__") { // rewrite german format to ISO
+        body.displayAt = moment(body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
+    } else {
+        body.displayAt = undefined;
+    }
+    body.creatorId = res.locals.currentUser._id;
+    body.createdAt = moment().toISOString();
+
+    if(body.context) {
+        body.target = body.targetId;
+        body.targetModel = body.context;
+    }
+
+    api(req).post('/news/', {
+        // TODO: sanitize
+        json: body
+    }).then(data => {
+        if (body.context) {
+            res.redirect(`/${body.context}/` + body.targetId);
+        }
+        res.redirect('/news');
+    }).catch(err => {
+        next(err);
+    });
+});
+router.patch('/:newsId', function (req, res, next) {
+    api(req).get('/news/' + req.params.newsId, {}).then(orgNews => {
+        req.body.displayAt = moment(req.body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
+
+        const historyEntry = {
+            "title": orgNews.title,
+            "content": orgNews.content,
+            "displayAt": orgNews.displayAt,
+
+            "creatorId": (orgNews.updaterId) ? (orgNews.updaterId) : (orgNews.creatorId),
+            "parentId": req.params.newsId
+        };
+
+        api(req).post('/newshistory/', {
+            // TODO: sanitize
+            json: historyEntry
+        }).then(data => {
+            req.body.updaterId = res.locals.currentUser._id;
+            req.body.updatedAt = moment().toISOString();
+            orgNews.history.push(data._id);
+            req.body.history = orgNews.history;
+
+            api(req).patch('/news/' + req.params.newsId, {
+                // TODO: sanitize
+                json: req.body
+            }).then(data => {
+                res.redirect('/news');
+            }).catch(err => {
+                next(err);
+            });
+
+
+        }).catch(err => {
+            next(err);
+        });
+    }).catch(err => {
+        next(err);
+    });
+});
+router.delete('/:id', getDeleteHandler('news'));
+
+router.all('/', function (req, res, next) {
+    const query = req.query.q;
+    const itemsPerPage = 9;
+    const currentPage = parseInt(req.query.p) || 1;
+    const context = req.originalUrl.split('/')[1];
+    let queryObject = {
+        $limit: itemsPerPage,
+        displayAt: (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) ? {} : {$lte: new Date().getTime()},
+        $skip: (itemsPerPage * (currentPage -1)),
+        $sort: '-displayAt',
+        title: { $regex: query, $options: 'i' },
+        target: { $exists: false }
+    };
+
+    if (req.params.targetId) {
+        queryObject.target = req.params.targetId;
+    }
+
+    if (!query)
+        delete queryObject.title;
+
+    return api(req).get('/news/', { qs: queryObject })
+        .then(news => {
+        const totalNews = news.total;
+        const colors = ["F44336","E91E63","3F51B5","2196F3","03A9F4","00BCD4","009688","4CAF50","CDDC39","FFC107","FF9800","FF5722"];
+        news = news.data.map(news => {
+            news.url = '/news/' + news._id;
+            news.secondaryTitle = moment(news.displayAt).fromNow();
+            news.background = '#'+colors[(news.title||"").length % colors.length];
+            if (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) {
+                news.actions = getActions(news, '/news/');
+            }
+            return news;
+        });
+        const pagination = {
+            currentPage,
+            numPages: Math.ceil(totalNews / itemsPerPage),
+            baseUrl: '/news/?p={{page}}'
+        };
+        let title = 'Neuigkeiten aus meiner Schule';
+        // ToDo: Hier kommen noch News für Kurse und Klassen rein.
+        switch (context) {
+            case 'teams': {
+                title = 'Neuigkeiten aus meinem Team';
+            }
+        }
+        res.render('news/overview', {
+            title,
+            news,
+            pagination,
+            searchLabel: 'Suche nach Neuigkeiten',
+            searchAction: '/news/',
+            showSearch: true
+        });
+    }).catch(err => {
+        next(err);
+    });
+});
+
+router.get('/new', function (req, res, next) {
+    const context = req.originalUrl.split('/')[1];
+    res.render('news/edit', {
+        title: "News erstellen",
+        submitLabel: 'Hinzufügen',
+        closeLabel: 'Abbrechen',
+        method: 'post',
+        action: '/news/',
+        context,
+        targetId: req.params.targetId
+    });
+});
+
+router.get('/:newsId', function (req, res, next) {
+    api(req).get('/news/' + req.params.newsId, {
+        qs: {
+            $populate: ['creatorId', 'updaterId']
+        }
+    }).then(news => {
+        news.url = '/news/' + news._id;
+        res.render('news/article', {title: news.title, news });
+    }).catch(err => {
+        next(err);
+    });
+});
+
+router.get('/:newsId/edit', function (req, res, next) {
+    api(req).get('/news/' + req.params.newsId, {}).then(news => {
+        news.displayAt = moment(news.displayAt).format('DD.MM.YYYY HH:mm');
+        res.render('news/edit', {
+            title: "News bearbeiten",
+            submitLabel: 'Speichern',
+            closeLabel: 'Abbrechen',
+            method: 'patch',
+            action: '/news/' + req.params.newsId,
+            news
+        });
+    }).catch(err => {
+        next(err);
+    });
+});
+
+module.exports = router;
--- controllers/tools.js
+++ controllers/tools.js
@@ -8,157 +8,160 @@ const authHelper = require('../helpers/authentication');
 const ltiCustomer = require('../helpers/ltiCustomer');
 const request = require('request');
 
-module.exports = function(context = 'courses') {
-
-    const createToolHandler = (req, res, next) => {
-        api(req).post('/ltiTools/', {
-            json: req.body
-        }).then(tool => {
-            if (tool._id) {
-                api(req).patch(`/${context}/` + req.body.courseId, {
-                    json: {
-                        $push: {
-                            ltiToolIds: tool._id
-                        }
+const createToolHandler = (req, res, next) => {
+    const context = req.originalUrl.split('/')[1];
+    api(req).post('/ltiTools/', {
+        json: req.body
+    }).then(tool => {
+        if (tool._id) {
+            api(req).patch(`/${context}/` + req.body.courseId, {
+                json: {
+                    $push: {
+                        ltiToolIds: tool._id
                     }
-                }).then(course => {
-                res.redirect(`/${context}/` + course._id);
-                });
-            }
-        });
-    };
-
-    const addToolHandler = (req, res, next) => {
-        let action = `/${context}/` + req.params.courseId + '/tools/add';
-
-        api(req).get('/ltiTools', { qs: {isTemplate: true}})
-        .then(tools => {
-            api(req).get(`/${context}/` + req.params.courseId)
-                .then(course => {
-                    res.render('courses/add-tool', {
-                        action,
-                        title: 'Tool anlegen für ' + course.name,
-                        submitLabel: 'Tool anlegen',
-                        ltiTools: tools.data,
-                        courseId: req.params.courseId
-                    });
-                });
-        });
-    };
-
-    const runToolHandler = (req, res, next) => {
-        let currentUser = res.locals.currentUser;
-        Promise.all([
-        api(req).get('/ltiTools/' + req.params.ltiToolId),
-        api(req).get('/roles/' + currentUser.roles[0]._id),
-        api(req).get('/pseudonym?userId=' + currentUser._id + '&toolId=' + req.params.ltiToolId)
-        ]).then(([tool, role, pseudonym]) => {
-        let customer = new ltiCustomer.LTICustomer();
-        let consumer = customer.createConsumer(tool.key, tool.secret);
-        let user_id = '';
-        if (tool.privacy_permission === 'pseudonymous') {
-            user_id = pseudonym.data[0].pseudonym;
-        } else if (tool.privacy_permission === 'name' || tool.privacy_permission === 'e-mail') {
-            user_id = currentUser._id;
+                }
+            }).then(course => {
+            res.redirect(`/${context}/` + course._id);
+            });
         }
-        let payload = {
-            lti_version: tool.lti_version,
-            lti_message_type: tool.lti_message_type,
-            resource_link_id: tool.resource_link_id || req.params.courseId,
-            roles: customer.mapSchulcloudRoleToLTIRole(role.name),
-            launch_presentation_document_target: 'window',
-            launch_presentation_locale: 'en',
-            lis_person_name_full: (tool.privacy_permission === 'name'
-                ? currentUser.displayName || `${currentUser.firstName} ${currentUser.lastName}`
-                : null),
-            lis_person_contact_email_primary: (tool.privacy_permission === 'e-mail'
-                ? currentUser.email
-                : null),
-            user_id
-        };
-        tool.customs.forEach((custom) => {
-            payload[customer.customFieldToString(custom)] = custom.value;
-        });
+    });
+};
 
-        let request_data = {
+const addToolHandler = (req, res, next) => {
+    const context = req.originalUrl.split('/')[1];
+    let action = `/${context}/` + req.params.courseId + '/tools/add';
+
+    api(req).get('/ltiTools', { qs: {isTemplate: true}})
+    .then(tools => {
+        api(req).get(`/${context}/` + req.params.courseId)
+            .then(course => {
+                res.render('courses/add-tool', {
+                    action,
+                    title: 'Tool anlegen für ' + course.name,
+                    submitLabel: 'Tool anlegen',
+                    ltiTools: tools.data,
+                    courseId: req.params.courseId
+                });
+            });
+    });
+};
+
+const runToolHandler = (req, res, next) => {
+    let currentUser = res.locals.currentUser;
+    Promise.all([
+    api(req).get('/ltiTools/' + req.params.ltiToolId),
+    api(req).get('/roles/' + currentUser.roles[0]._id),
+    api(req).get('/pseudonym?userId=' + currentUser._id + '&toolId=' + req.params.ltiToolId)
+    ]).then(([tool, role, pseudonym]) => {
+    let customer = new ltiCustomer.LTICustomer();
+    let consumer = customer.createConsumer(tool.key, tool.secret);
+    let user_id = '';
+    if (tool.privacy_permission === 'pseudonymous') {
+        user_id = pseudonym.data[0].pseudonym;
+    } else if (tool.privacy_permission === 'name' || tool.privacy_permission === 'e-mail') {
+        user_id = currentUser._id;
+    }
+    let payload = {
+        lti_version: tool.lti_version,
+        lti_message_type: tool.lti_message_type,
+        resource_link_id: tool.resource_link_id || req.params.courseId,
+        roles: customer.mapSchulcloudRoleToLTIRole(role.name),
+        launch_presentation_document_target: 'window',
+        launch_presentation_locale: 'en',
+        lis_person_name_full: (tool.privacy_permission === 'name'
+            ? currentUser.displayName || `${currentUser.firstName} ${currentUser.lastName}`
+            : null),
+        lis_person_contact_email_primary: (tool.privacy_permission === 'e-mail'
+            ? currentUser.email
+            : null),
+        user_id
+    };
+    tool.customs.forEach((custom) => {
+        payload[customer.customFieldToString(custom)] = custom.value;
+    });
+
+    let request_data = {
+        url: tool.url,
+        method: 'POST',
+        data: payload
+    };
+
+        var formData = consumer.authorize(request_data);
+
+        res.render('courses/components/run-lti-frame', {
             url: tool.url,
             method: 'POST',
-            data: payload
-        };
-
-            var formData = consumer.authorize(request_data);
-
-            res.render('courses/components/run-lti-frame', {
-                url: tool.url,
-                method: 'POST',
-                formData: Object.keys(formData).map(key => {
-                    return {name: key, value: formData[key]};
-                })
-            });
-        });
-    };
-
-    const getDetailHandler = (req, res, next) => {
-        Promise.all([
-            api(req).get(`/${context}/`, {
-            qs: {
-                teacherIds: res.locals.currentUser._id}
-            }),
-            api(req).get('/ltiTools/' + req.params.id)]).
-        then(([courses, tool]) => {
-            res.json({
-                tool: tool
-            });
-        }).catch(err => {
-            next(err);
-        });
-    };
-
-    const showToolHandler = (req, res, next) => {
-
-        Promise.all([
-            api(req).get('/ltiTools/' + req.params.ltiToolId),
-            api(req).get(`/${context}/` + req.params.courseId)
-        ])
-        .then(([tool, course]) => {
-            let renderPath = tool.isLocal ? 'courses/run-tool-local' : 'courses/run-lti';
-            res.render(renderPath, {
-                course: course,
-                title: `${tool.name}, Kurs/Fach: ${course.name}`,
-                tool: tool
-            });
-        });
-    };
-
-
-    // secure routes
-    router.use(authHelper.authChecker);
-
-    router.get('/', (req, res, next) => {
-        res.redirect(`/${context}/` + req.params.courseId);
-    });
-
-    router.get('/add', addToolHandler);
-    router.post('/add', createToolHandler);
-
-    router.get('/run/:ltiToolId', runToolHandler);
-    router.get('/show/:ltiToolId', showToolHandler);
-
-    router.get('/:id', getDetailHandler);
-
-    router.delete('/delete/:ltiToolId', function (req, res, next) {
-        api(req).patch(`/${context}/` + req.params.courseId, {
-            json: {
-                $pull: {
-                    ltiToolIds: req.params.ltiToolId
-                }
-            }
-        }).then(_ => {
-            api(req).delete('/ltiTools/' + req.params.ltiToolId).then(_ => {
-                res.sendStatus(200);
-            });
+            formData: Object.keys(formData).map(key => {
+                return {name: key, value: formData[key]};
+            })
         });
     });
-
-    return router;
 };
+
+const getDetailHandler = (req, res, next) => {
+    const context = req.originalUrl.split('/')[1];
+    Promise.all([
+        api(req).get(`/${context}/`, {
+        qs: {
+            teacherIds: res.locals.currentUser._id}
+        }),
+        api(req).get('/ltiTools/' + req.params.id)]).
+    then(([courses, tool]) => {
+        res.json({
+            tool: tool
+        });
+    }).catch(err => {
+        next(err);
+    });
+};
+
+const showToolHandler = (req, res, next) => {
+    const context = req.originalUrl.split('/')[1];
+
+    Promise.all([
+        api(req).get('/ltiTools/' + req.params.ltiToolId),
+        api(req).get(`/${context}/` + req.params.courseId)
+    ])
+    .then(([tool, course]) => {
+        let renderPath = tool.isLocal ? 'courses/run-tool-local' : 'courses/run-lti';
+        res.render(renderPath, {
+            course: course,
+            title: `${tool.name}, Kurs/Fach: ${course.name}`,
+            tool: tool
+        });
+    });
+};
+
+
+// secure routes
+router.use(authHelper.authChecker);
+
+router.get('/', (req, res, next) => {
+    const context = req.originalUrl.split('/')[1];
+    res.redirect(`/${context}/` + req.params.courseId);
+});
+
+router.get('/add', addToolHandler);
+router.post('/add', createToolHandler);
+
+router.get('/run/:ltiToolId', runToolHandler);
+router.get('/show/:ltiToolId', showToolHandler);
+
+router.get('/:id', getDetailHandler);
+
+router.delete('/delete/:ltiToolId', function (req, res, next) {
+    const context = req.originalUrl.split('/')[1];
+    api(req).patch(`/${context}/` + req.params.courseId, {
+        json: {
+            $pull: {
+                ltiToolIds: req.params.ltiToolId
+            }
+        }
+    }).then(_ => {
+        api(req).delete('/ltiTools/' + req.params.ltiToolId).then(_ => {
+            res.sendStatus(200);
+        });
+    });
+});
+
+module.exports = router;
