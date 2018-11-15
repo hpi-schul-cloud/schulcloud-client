b6a2e557348c4caaa0cf2be196739affda48a0f5
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-11-15 12:17:43 +0100

    Add minWidth to Select component

--- helpers/planner/materialComponents/Select.jsx
+++ helpers/planner/materialComponents/Select.jsx
@@ -3,12 +3,21 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import createStyles from "@material-ui/core/styles/createStyles";
+import withStyles from "@material-ui/core/styles/withStyles";
 import SCTheme from "./SchulCloudThemeProvider";
 
-const CustomSelect = ({ caption, initialValue, values, onChange }) => {
+const styles = () =>
+  createStyles({
+    formControl: {
+      minWidth: 120
+    }
+  });
+
+const CustomSelect = ({ caption, initialValue, values, classes, onChange }) => {
   return (
     <SCTheme>
-      <FormControl>
+      <FormControl className={classes.formControl}>
         <InputLabel htmlFor="age-simple">{caption}</InputLabel>
         <Select
           value={initialValue}
@@ -29,4 +38,4 @@ const CustomSelect = ({ caption, initialValue, values, onChange }) => {
   );
 };
 
-export default CustomSelect;
+export default withStyles(styles)(CustomSelect);
