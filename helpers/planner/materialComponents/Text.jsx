b0574af56a3bcc8d8e1e76cd5a94bf612dd3e98b
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-11-23 01:52:39 +0100

    Fix some styling issues for material Text component

--- helpers/planner/materialComponents/Text.jsx
+++ helpers/planner/materialComponents/Text.jsx
@@ -3,9 +3,15 @@ import Typography from "@material-ui/core/Typography";
 import Label from "./Label";
 
 const TextField = ({ label = "", text }) => {
-  const labelComponent = label ? <Label caption={label} /> : null;
+  const labelComponent = label ? <Label caption={label} type="small" /> : null;
   return (
-    <div style={{ display: "inline-flex", flexDirection: "column" }}>
+    <div
+      style={{
+        display: "inline-flex",
+        flexDirection: "column",
+        marginTop: label ? "16px" : "0px"
+      }}
+    >
       {labelComponent}
       <Typography variant="subheading">{text}</Typography>
     </div>
