0fb235ba78d21e1d277d9ebe9cbc59f91a5e91c9
Adrian Jost <adrianjost@users.noreply.github.com>
2018-12-05 14:15:35 +0100

    cleanup of old,deprecated, unused files & packages related to this ticket

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -35,8 +34,6 @@ app.set("view engine", "hbs");
 
 app.set('view cache', true);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
