096d8fc5a5f103bc990aae1bbd15bad9aa218b9d
Tobias Wollowski <tobias@jbbr.net>
2018-12-05 15:30:23 +0100

    add network protocol to logs

--- static/scripts/sw/metrix.js
+++ static/scripts/sw/metrix.js
@@ -35,6 +35,27 @@ function readConnectionType(result) {
   }
 }
 
+function setProtocol(result, perfEntry) {
+  var value = "nextHopProtocol" in perfEntry;
+  if (value) {
+    result['networkProtocol'] = perfEntry.nextHopProtocol;
+  } else {
+    result['networkProtocol'] = 'unknown';
+  }
+  return;
+}
+
+function readNetworkProtocol(result) {
+  if(performance && performance.getEntriesByType){
+    var p = performance.getEntriesByType("resource");
+    for (var i = 0; i < p.length; i++) {
+      setProtocol(result, p[i]);
+      break;
+    }
+  }
+  return;
+}
+
 import ttiPolyfill from './tti-polyfill.js';
 
 function sendResults(result) {
@@ -59,6 +80,7 @@ function calculateMetrics() {
     measureCRP(result);
     calculatePaintingTimes(result);
     readConnectionType(result);
+    readNetworkProtocol(result);
     sendResults(result);
   });
 }
