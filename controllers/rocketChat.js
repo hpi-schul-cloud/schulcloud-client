6f0be27413cc565940fb7f072c0279a288192bae
Thomas <thomas.feldtkeller@hpi.de>
2018-12-11 15:30:51 +0100

    rocketchat iframe auth first prototype

---
+++ controllers/rocketChat.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const router = express.Router();
+const api = require('../api');
+const authHelper = require('../helpers/authentication');
+
+router.use(authHelper.authChecker);
+
+router.get('/Iframe', function(req, res, next) {
+    return api(req).get('/rocketChat/' + res.locals.currentUser._id).then(result => {
+        let rocketChatURL = 'http://dev-rocketchat.schul-cloud.org:3000/home';
+        return res.send(`<script>
+            window.parent.postMessage({
+                event: 'login-with-token',
+                loginToken: '${result.authToken}'
+                }, '${rocketChatURL}');
+            </script>`
+        )
+    }) 
+})
+
+router.get('/authGet', function(req, res, next) {
+    return api(req).get('/rocketChat/' + res.locals.currentUser._id , {}).then(result => {
+        return res.send({
+            loginToken: result.authToken
+        })
+    })
+})
+
+module.exports = router;
