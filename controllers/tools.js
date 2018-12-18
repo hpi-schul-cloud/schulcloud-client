ad9cf120de730f57c0eb36ed1966de9381494be7
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2018-12-18 12:18:37 +0100

    Fix error handling.

--- controllers/tools.js
+++ controllers/tools.js
@@ -1,12 +1,13 @@
 const _ = require('lodash');
-const moment = require('moment');
+const logger = require('winston');
+//const moment = require('moment');
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const marked = require('marked');
+//const marked = require('marked');
 const api = require('../api');
 const authHelper = require('../helpers/authentication');
 const ltiCustomer = require('../helpers/ltiCustomer');
-const request = require('request');
+//const request = require('request');
 
 const createToolHandler = (req, res, next) => {
     api(req).post('/ltiTools/', {
@@ -20,7 +21,7 @@ const createToolHandler = (req, res, next) => {
                     }
                 }
             }).then(course => {
-               res.redirect('/courses/' + course._id);
+                res.redirect('/courses/' + course._id);
             });
         }
     });
@@ -29,60 +30,76 @@ const createToolHandler = (req, res, next) => {
 const addToolHandler = (req, res, next) => {
     let action = '/courses/' + req.params.courseId + '/tools/add';
 
-    api(req).get('/ltiTools', { qs: {isTemplate: true}})
-    .then(tools => {
-        api(req).get('/courses/' + req.params.courseId)
-            .then(course => {
-                res.render('courses/add-tool', {
-                    action,
-                    title: 'Tool anlegen für ' + course.name,
-                    submitLabel: 'Tool anlegen',
-                    ltiTools: tools.data,
-                    courseId: req.params.courseId
-                });
-            });
+    const courses = api(req).get('/courses/' + req.params.courseId).then(course => {
+        return course.name;
+    }).catch(err => {
+        logger.warn('Course not found');
+        return '';
+    });
+
+    const ltiTools = api(req).get('/ltiTools', { qs: { isTemplate: true } }).then(tools => {
+        return tools.data;
+    }).catch(err => {
+        logger.warn('ltiTools not found');
+        return [];
+    });
+
+    const getData = ([courseName, toolsData]) => {
+        return {
+            action,
+            title: 'Tool anlegen für ' + courseName || '',
+            submitLabel: 'Tool anlegen',
+            ltiTools: toolsData || [],
+            courseId: req.params.courseId
+        };
+    };
+
+    Promise.all([courses, ltiTools]).then(data => {
+        res.render('courses/add-tool',getData(data));
+    }).catch(err => {
+        res.render('courses/add-tool',getData([]));
     });
 };
 
 const runToolHandler = (req, res, next) => {
     let currentUser = res.locals.currentUser;
     Promise.all([
-      api(req).get('/ltiTools/' + req.params.ltiToolId),
-      api(req).get('/roles/' + currentUser.roles[0]._id),
-      api(req).get('/pseudonym?userId=' + currentUser._id + '&toolId=' + req.params.ltiToolId)
+        api(req).get('/ltiTools/' + req.params.ltiToolId),
+        api(req).get('/roles/' + currentUser.roles[0]._id),
+        api(req).get('/pseudonym?userId=' + currentUser._id + '&toolId=' + req.params.ltiToolId)
     ]).then(([tool, role, pseudonym]) => {
-       let customer = new ltiCustomer.LTICustomer();
-       let consumer = customer.createConsumer(tool.key, tool.secret);
-       let user_id = '';
-       if (tool.privacy_permission === 'pseudonymous') {
-         user_id = pseudonym.data[0].pseudonym;
-       } else if (tool.privacy_permission === 'name' || tool.privacy_permission === 'e-mail') {
-         user_id = currentUser._id;
-       }
-       let payload = {
-           lti_version: tool.lti_version,
-           lti_message_type: tool.lti_message_type,
-           resource_link_id: tool.resource_link_id || req.params.courseId,
-           roles: customer.mapSchulcloudRoleToLTIRole(role.name),
-           launch_presentation_document_target: 'window',
-           launch_presentation_locale: 'en',
-           lis_person_name_full: (tool.privacy_permission === 'name'
-             ? currentUser.displayName || `${currentUser.firstName} ${currentUser.lastName}`
-             : ''),
-           lis_person_contact_email_primary: (tool.privacy_permission === 'e-mail'
-             ? currentUser.email
-             : ''),
-           user_id
-       };
-       tool.customs.forEach((custom) => {
-           payload[customer.customFieldToString(custom)] = custom.value;
-       });
+        let customer = new ltiCustomer.LTICustomer();
+        let consumer = customer.createConsumer(tool.key, tool.secret);
+        let user_id = '';
+        if (tool.privacy_permission === 'pseudonymous') {
+            user_id = pseudonym.data[0].pseudonym;
+        } else if (tool.privacy_permission === 'name' || tool.privacy_permission === 'e-mail') {
+            user_id = currentUser._id;
+        }
+        let payload = {
+            lti_version: tool.lti_version,
+            lti_message_type: tool.lti_message_type,
+            resource_link_id: tool.resource_link_id || req.params.courseId,
+            roles: customer.mapSchulcloudRoleToLTIRole(role.name),
+            launch_presentation_document_target: 'window',
+            launch_presentation_locale: 'en',
+            lis_person_name_full: (tool.privacy_permission === 'name'
+                ? currentUser.displayName || `${currentUser.firstName} ${currentUser.lastName}`
+                : ''),
+            lis_person_contact_email_primary: (tool.privacy_permission === 'e-mail'
+                ? currentUser.email
+                : ''),
+            user_id
+        };
+        tool.customs.forEach((custom) => {
+            payload[customer.customFieldToString(custom)] = custom.value;
+        });
 
-       let request_data = {
-           url: tool.url,
-           method: 'POST',
-           data: payload
-       };
+        let request_data = {
+            url: tool.url,
+            method: 'POST',
+            data: payload
+        };
 
         var formData = consumer.authorize(request_data);
 
@@ -90,7 +107,7 @@ const runToolHandler = (req, res, next) => {
             url: tool.url,
             method: 'POST',
             formData: Object.keys(formData).map(key => {
-                return {name: key, value: formData[key]};
+                return { name: key, value: formData[key] };
             })
         });
     });
@@ -99,17 +116,18 @@ const runToolHandler = (req, res, next) => {
 const getDetailHandler = (req, res, next) => {
     Promise.all([
         api(req).get('/courses/', {
-        qs: {
-            teacherIds: res.locals.currentUser._id}
+            qs: {
+                teacherIds: res.locals.currentUser._id
+            }
         }),
         api(req).get('/ltiTools/' + req.params.id)]).
-    then(([courses, tool]) => {
-        res.json({
-            tool: tool
+        then(([courses, tool]) => {
+            res.json({
+                tool: tool
+            });
+        }).catch(err => {
+            next(err);
         });
-    }).catch(err => {
-        next(err);
-    });
 };
 
 const showToolHandler = (req, res, next) => {
@@ -118,14 +136,14 @@ const showToolHandler = (req, res, next) => {
         api(req).get('/ltiTools/' + req.params.ltiToolId),
         api(req).get('/courses/' + req.params.courseId)
     ])
-    .then(([tool, course]) => {
-        let renderPath = tool.isLocal ? 'courses/run-tool-local' : 'courses/run-lti';
-        res.render(renderPath, {
-            course: course,
-            title: `${tool.name}, Kurs/Fach: ${course.name}`,
-            tool: tool
+        .then(([tool, course]) => {
+            let renderPath = tool.isLocal ? 'courses/run-tool-local' : 'courses/run-lti';
+            res.render(renderPath, {
+                course: course,
+                title: `${tool.name}, Kurs/Fach: ${course.name}`,
+                tool: tool
+            });
         });
-    });
 };
 
 
