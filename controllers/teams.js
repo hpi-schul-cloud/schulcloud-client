2ca70988a142c3f8cad1275f51f7e27fba7bd206
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2019-01-28 11:07:03 +0100

    N21/rocket chat (#1009)
    * copy over from other PR
    
    * hardcoded dedicated id for demo
    
    * update stylesheet
    
    * I dont like closing tags
    
    * update environment variable

--- controllers/teams.js
+++ controllers/teams.js
@@ -389,7 +389,7 @@ router.get('/:teamId', async function(req, res, next) {
         if (process.env.ROCKETCHAT_SERVICE_ENABLED || req.params.teamId == "ffffd213816abba584714c00") { //for demo
             try{
                 const rocketChatChannel = await api(req).get('/rocketChat/channel/' + req.params.teamId);
-                const rocketChatURL = process.env.ROCKET_CHAT
+                const rocketChatURL = process.env.ROCKET_CHAT_URI
                 rocketChatCompleteURL = rocketChatURL + "/group/" + rocketChatChannel.channelName;
             }
             catch(e) {
