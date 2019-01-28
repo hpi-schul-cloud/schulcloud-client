fab0470f3489caca577c3c8616007be4f871155d
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-01-28 17:31:27 +0100

    cleanup

--- storybook/stories/index.stories.js
+++ storybook/stories/index.stories.js
@@ -14,24 +14,5 @@ storiesOf('Welcome', module).add('to Storybook', () => ({
   methods: { action: linkTo('Button') },
 }));
 
-storiesOf('Buttons', module)
-  .add('with text', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action('clicked') },
-  }))
-  .add('with JSX', () => ({
-    components: { MyButton },
-    // eslint-disable-next-line no-unused-vars
-    render(h) {
-      return <my-button onClick={this.action}>With JSX</my-button>;
-    },
-    methods: { action: linkTo('clicked') },
-  }))
-  .add('with some emoji', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action('clicked') },
-  }));
 
 /* eslint-enable react/react-in-jsx-scope */
