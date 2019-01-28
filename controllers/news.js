58b451fc436f67d981fb5fcd7be2426c8e250166
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-01-28 14:14:22 +0100

    news controller eslint fix

--- controllers/news.js
+++ controllers/news.js
@@ -14,176 +14,176 @@ moment.locale('de');
 router.use(authHelper.authChecker);
 
 const getActions = (item, path) => {
-    return [
-        {
-            link: path + item._id + "/edit",
-            class: 'btn-edit',
-            icon: 'edit',
-            alt: 'bearbeiten'
-        },
-        {
-            link: path + item._id,
-            class: 'btn-delete',
-            icon: 'trash-o',
-            method: 'delete',
-            alt: 'löschen'
-        }
-    ];
+	return [
+		{
+			link: path + item._id + "/edit",
+			class: 'btn-edit',
+			icon: 'edit',
+			alt: 'bearbeiten'
+		},
+		{
+			link: path + item._id,
+			class: 'btn-delete',
+			icon: 'trash-o',
+			method: 'delete',
+			alt: 'löschen'
+		}
+	];
 };
 
 const getDeleteHandler = (service) => {
-    return function (req, res, next) {
-        api(req).delete('/' + service + '/' + req.params.id).then(_ => {
-            res.sendStatus(200);
-        }).catch(err => {
-            next(err);
-        });
-    };
+	return (req, res, next) => {
+		api(req).delete('/' + service + '/' + req.params.id).then(_ => {
+			res.sendStatus(200);
+		}).catch(err => {
+			next(err);
+		});
+	};
 };
 
-router.post('/', function (req, res, next) {
-    if (req.body.displayAt && req.body.displayAt != "__.__.____ __:__") { // rewrite german format to ISO
-        req.body.displayAt = moment(req.body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
-    } else {
-        req.body.displayAt = undefined;
-    }
-    req.body.creatorId = res.locals.currentUser._id;
-    req.body.createdAt = moment().toISOString();
+router.post('/', (req, res, next) => {
+	if (req.body.displayAt && req.body.displayAt != "__.__.____ __:__") { // rewrite german format to ISO
+		req.body.displayAt = moment(req.body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
+	} else {
+		req.body.displayAt = undefined;
+	}
+	req.body.creatorId = res.locals.currentUser._id;
+	req.body.createdAt = moment().toISOString();
 
-    api(req).post('/news/', {
-        // TODO: sanitize
-        json: req.body
-    }).then(data => {
-        res.redirect('/news');
-    }).catch(err => {
-        next(err);
-    });
+	api(req).post('/news/', {
+		// TODO: sanitize
+		json: req.body
+	}).then(data => {
+		res.redirect('/news');
+	}).catch(err => {
+		next(err);
+	});
 });
-router.patch('/:newsId', function (req, res, next) {
-    api(req).get('/news/' + req.params.newsId, {}).then(orgNews => {
-        req.body.displayAt = moment(req.body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
+router.patch('/:newsId', (req, res, next) => {
+	api(req).get('/news/' + req.params.newsId, {}).then(orgNews => {
+		req.body.displayAt = moment(req.body.displayAt, 'DD.MM.YYYY HH:mm').toISOString();
 
-        const historyEntry = {
-            "title": orgNews.title,
-            "content": orgNews.content,
-            "displayAt": orgNews.displayAt,
+		const historyEntry = {
+			"title": orgNews.title,
+			"content": orgNews.content,
+			"displayAt": orgNews.displayAt,
 
-            "creatorId": (orgNews.updaterId) ? (orgNews.updaterId) : (orgNews.creatorId),
-            "parentId": req.params.newsId
-        };
+			"creatorId": (orgNews.updaterId) ? (orgNews.updaterId) : (orgNews.creatorId),
+			"parentId": req.params.newsId
+		};
 
-        api(req).post('/newshistory/', {
-            // TODO: sanitize
-            json: historyEntry
-        }).then(data => {
-            req.body.updaterId = res.locals.currentUser._id;
-            req.body.updatedAt = moment().toISOString();
-            orgNews.history.push(data._id);
-            req.body.history = orgNews.history;
+		api(req).post('/newshistory/', {
+			// TODO: sanitize
+			json: historyEntry
+		}).then(data => {
+			req.body.updaterId = res.locals.currentUser._id;
+			req.body.updatedAt = moment().toISOString();
+			orgNews.history.push(data._id);
+			req.body.history = orgNews.history;
 
-            api(req).patch('/news/' + req.params.newsId, {
-                // TODO: sanitize
-                json: req.body
-            }).then(data => {
-                res.redirect('/news');
-            }).catch(err => {
-                next(err);
-            });
+			api(req).patch('/news/' + req.params.newsId, {
+				// TODO: sanitize
+				json: req.body
+			}).then(data => {
+				res.redirect('/news');
+			}).catch(err => {
+				next(err);
+			});
 
 
-        }).catch(err => {
-            next(err);
-        });
-    }).catch(err => {
-        next(err);
-    });
+		}).catch(err => {
+			next(err);
+		});
+	}).catch(err => {
+		next(err);
+	});
 });
 router.delete('/:id', getDeleteHandler('news'));
 
 router.all('/', function (req, res, next) {
-    const query = req.query.q;
-    const itemsPerPage = 9;
-    const currentPage = parseInt(req.query.p) || 1;
+	const query = req.query.q;
+	const itemsPerPage = 9;
+	const currentPage = parseInt(req.query.p) || 1;
 
-    let queryObject = {
-        $limit: itemsPerPage,
-        displayAt: (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) ? {} : {$lte: new Date().getTime()},
-        $skip: (itemsPerPage * (currentPage -1)),
-        $sort: '-displayAt',
-        title: { $regex: query, $options: 'i' }
-    };
+	let queryObject = {
+		$limit: itemsPerPage,
+		displayAt: (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) ? {} : { $lte: new Date().getTime() },
+		$skip: (itemsPerPage * (currentPage - 1)),
+		$sort: '-displayAt',
+		title: { $regex: query, $options: 'i' }
+	};
 
-    if (!query)
-        delete queryObject.title;
+	if (!query)
+		delete queryObject.title;
 
-    return api(req).get('/news/', { qs: queryObject })
-        .then(news => {
-        const totalNews = news.total;
-        const colors = ["F44336","E91E63","3F51B5","2196F3","03A9F4","00BCD4","009688","4CAF50","CDDC39","FFC107","FF9800","FF5722"];
-        news = news.data.map(news => {
-            news.url = '/news/' + news._id;
-            news.secondaryTitle = moment(news.displayAt).fromNow();
-            news.background = '#'+colors[(news.title||"").length % colors.length];
-            if (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) {
-                news.actions = getActions(news, '/news/');
-            }
-            return news;
-        });
-        const pagination = {
-            currentPage,
-            numPages: Math.ceil(totalNews / itemsPerPage),
-            baseUrl: '/news/?p={{page}}'
-        };
-        res.render('news/overview', {
-            title: 'Neuigkeiten aus meiner Schule',
-            news,
-            pagination,
-            searchLabel: 'Suche nach Neuigkeiten',
-            searchAction: '/news/',
-            showSearch: true
-        });
-    }).catch(err => {
-        next(err);
-    });
+	return api(req).get('/news/', { qs: queryObject })
+		.then(news => {
+			const totalNews = news.total;
+			const colors = ["F44336", "E91E63", "3F51B5", "2196F3", "03A9F4", "00BCD4", "009688", "4CAF50", "CDDC39", "FFC107", "FF9800", "FF5722"];
+			news = news.data.map(news => {
+				news.url = '/news/' + news._id;
+				news.secondaryTitle = moment(news.displayAt).fromNow();
+				news.background = '#' + colors[(news.title || "").length % colors.length];
+				if (res.locals.currentUser.permissions.includes('SCHOOL_NEWS_EDIT')) {
+					news.actions = getActions(news, '/news/');
+				}
+				return news;
+			});
+			const pagination = {
+				currentPage,
+				numPages: Math.ceil(totalNews / itemsPerPage),
+				baseUrl: '/news/?p={{page}}'
+			};
+			res.render('news/overview', {
+				title: 'Neuigkeiten aus meiner Schule',
+				news,
+				pagination,
+				searchLabel: 'Suche nach Neuigkeiten',
+				searchAction: '/news/',
+				showSearch: true
+			});
+		}).catch(err => {
+			next(err);
+		});
 });
 
 router.get('/new', function (req, res, next) {
-    res.render('news/edit', {
-        title: "News erstellen",
-        submitLabel: 'Hinzufügen',
-        closeLabel: 'Abbrechen',
-        method: 'post',
-        action: '/news/'
-    });
+	res.render('news/edit', {
+		title: "News erstellen",
+		submitLabel: 'Hinzufügen',
+		closeLabel: 'Abbrechen',
+		method: 'post',
+		action: '/news/'
+	});
 });
 
 router.get('/:newsId', function (req, res, next) {
-    api(req).get('/news/' + req.params.newsId, {
-        qs: {
-            $populate: ['creatorId', 'updaterId']
-        }
-    }).then(news => {
-        news.url = '/news/' + news._id;
-        res.render('news/article', {title: news.title, news });
-    }).catch(err => {
-        next(err);
-    });
+	api(req).get('/news/' + req.params.newsId, {
+		qs: {
+			$populate: ['creatorId', 'updaterId']
+		}
+	}).then(news => {
+		news.url = '/news/' + news._id;
+		res.render('news/article', { title: news.title, news });
+	}).catch(err => {
+		next(err);
+	});
 });
 
 router.get('/:newsId/edit', function (req, res, next) {
-    api(req).get('/news/' + req.params.newsId, {}).then(news => {
-        news.displayAt = moment(news.displayAt).format('DD.MM.YYYY HH:mm');
-        res.render('news/edit', {
-            title: "News bearbeiten",
-            submitLabel: 'Speichern',
-            closeLabel: 'Abbrechen',
-            method: 'patch',
-            action: '/news/' + req.params.newsId,
-            news
-        });
-    }).catch(err => {
-        next(err);
-    });
+	api(req).get('/news/' + req.params.newsId, {}).then(news => {
+		news.displayAt = moment(news.displayAt).format('DD.MM.YYYY HH:mm');
+		res.render('news/edit', {
+			title: "News bearbeiten",
+			submitLabel: 'Speichern',
+			closeLabel: 'Abbrechen',
+			method: 'patch',
+			action: '/news/' + req.params.newsId,
+			news
+		});
+	}).catch(err => {
+		next(err);
+	});
 });
 
 module.exports = router;
