70e248e7d2a02efa2cee8acf0293692049864b01
Falko Benthin <Falko.Benthin@hpi.de>
2019-02-04 13:54:36 +0100

    widerspenstige Sonderzeichen in Tagnamen ersetzt

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
+# replace special characters in branch name for docker tag
+export DOCKERTAG=$( echo $TRAVIS_BRANCH | tr -s "[:punct:]" "-" )
+
 # build containers
-docker build -t schul-cloud/schulcloud-client:$TRAVIS_BRANCH -t schul-cloud/schulcloud-client:$GIT_SHA .
+docker build -t schulcloud/schulcloud-client:$DOCKERTAG -t schulcloud/schulcloud-client:$GIT_SHA .
 
 # Log in to the docker CLI
 echo "$MY_DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
 # take those images and push them up to docker hub
-docker push schul-cloud/schulcloud-client:$TRAVIS_BRANCH
-docker push schul-cloud/schulcloud-client:$GIT_SHA
+docker push schulcloud/schulcloud-client:$DOCKERTAG
+docker push schulcloud/schulcloud-client:$GIT_SHA
 
 # screw together config file for docker swarm 
 eval "echo \"$( cat compose-client-test.dummy )\"" > docker-compose-client.yml
