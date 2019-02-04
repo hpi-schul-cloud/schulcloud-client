37a13c4de1d1a4e0a38aaf5f353ad755b4cf73d9
Falko Benthin <Falko.Benthin@hpi.de>
2019-02-04 09:30:08 +0100

    deploy-script soll Client zusammenschrauben, nicht Server

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 # build containers
-docker build -t schul-cloud/schulcloud-server:$TRAVIS_BRANCH -t schul-cloud/schulcloud-server:$GIT_SHA .
+docker build -t schul-cloud/schulcloud-client:$TRAVIS_BRANCH -t schul-cloud/schulcloud-client:$GIT_SHA .
 
 # Log in to the docker CLI
 echo "$MY_DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
 # take those images and push them up to docker hub
-docker push schul-cloud/schulcloud-server:$TRAVIS_BRANCH
-docker push schul-cloud/schulcloud-server:$GIT_SHA
+docker push schul-cloud/schulcloud-client:$TRAVIS_BRANCH
+docker push schul-cloud/schulcloud-client:$GIT_SHA
 
 # screw together config file for docker swarm 
-eval "echo \"$( cat compose-client-test.dummy )\"" > compose-client.yml
+eval "echo \"$( cat compose-client-test.dummy )\"" > docker-compose-client.yml
 
 # copy config-file to server and execute mit travis_rsa
-cp -i travis_rsa compose-client.yml linux@test.schul-cloud.org:~
+scp -i travis_rsa docker-compose-client.yml linux@test.schul-cloud.org:~
 
 
 exit 0
