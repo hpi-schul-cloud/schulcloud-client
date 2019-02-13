01b669734144791f0e19c994c283237cd90f34ba
Martin Malchow <martin.malchow@hpi.de>
2019-02-13 16:50:26 +0100

    UI general done Next steps: save in Database Verify saved data for ldap

--- static/scripts/administration/systems/ldap-edit.js
+++ static/scripts/administration/systems/ldap-edit.js
@@ -28,4 +28,22 @@ window.addEventListener('DOMContentLoaded', () => {
 	document.querySelector('#user-section-toggle').addEventListener('click', (clicked) => { toggleFieldClass(clicked.target, '.user-attribute'); });
 	document.querySelector('#role-section-toggle').addEventListener('click', (clicked) => { toggleFieldClass(clicked.target, '.role-attribute'); });
 	document.querySelector('#class-section-toggle').addEventListener('click', (clicked) => { toggleFieldClass(clicked.target, '.class-attribute'); });
+
+	document.querySelector('#select-roletype').onchange = function (e) {
+		if (e.target.options[e.target.selectedIndex].value === 'group') {
+			document.querySelector('#role-name').style.display = 'none';
+			document.querySelector('#headline-role-type').innerHTML = 'Rollen-LDAP-Pfade';
+		} else {
+			document.querySelector('#role-name').style.display = 'block';
+			document.querySelector('#headline-role-type').innerHTML = 'Rollen-Attribute';
+		}
+	};
+
+	document.querySelector('#select-classes').onchange = function (e) {
+		if(e.target.checked) {
+			document.querySelector('.class-settings').style.display = 'block';
+		} else {
+			document.querySelector('.class-settings').style.display = 'none';
+		}
+	}
 });
