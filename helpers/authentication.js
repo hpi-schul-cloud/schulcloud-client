65eef0c81457bbf5c161d41212136044b8658d3d
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-02-25 14:51:10 +0100

    Fix import button visibility for users with multiple roles

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -94,6 +94,7 @@ const populateCurrentUser = (req, res) => {
             res.locals.currentUser = data;
             setTestGroup(res.locals.currentUser);
             res.locals.currentRole = rolesDisplayName[data.roles[0].name];
+            res.locals.roleNames = data.roles.map(r => rolesDisplayName[r.name]);
             return api(req).get('/schools/' + res.locals.currentUser.schoolId, {
                 qs: {
                     $populate: ['federalState']
