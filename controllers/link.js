aa2c9e183a6598b15b4a3f5b0052b9feec3b5faa
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-02-26 12:12:11 +0100

    SC-696 enable students to join courses through links (#1051)
    backend pull: https://github.com/schul-cloud/schulcloud-server/pull/485

--- controllers/link.js
+++ controllers/link.js
@@ -5,10 +5,10 @@ const api = require('../api');
 router.post('/', function (req, res, next) {
     // check first if target already exists (preventing db to be wasted)
     let target = `${(req.headers.origin || process.env.HOST)}/${req.body.target}`;
-    api(req).get("/link/", {qs: {target: target}}).then(result => {
+    api(req).get("/link/", { qs: { target: target } }).then(result => {
         let existingLink = result.data[0];
         if (!existingLink) {
-            api(req).post("/link/", {json: {target:  target}}).then(data => {
+            api(req).post("/link/", { json: { target: target } }).then(data => {
                 data.newUrl = `${(req.headers.origin || process.env.HOST)}/link/${data._id}`;
                 res.json(data);
             });
@@ -22,7 +22,7 @@ router.post('/', function (req, res, next) {
 // handles redirecting from client
 router.get('/:id', function (req, res, next) {
     let baseUrl = process.env.PUBLIC_BACKEND_URL || 'http://localhost:3030';
-    res.redirect(`${baseUrl}/link/${req.params.id}`);
+    res.redirect(`${baseUrl}/link/${req.params.id}?includeShortId=true`);
 });
 
 module.exports = router;
