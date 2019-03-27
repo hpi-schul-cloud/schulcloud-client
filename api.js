bdeb951178d92f9653074e8bd3160b6b080717c2
Tobias Wollowski <tobias.wollowski@student.hpi.de>
2019-03-27 13:53:45 +0100

    extract backend url to separate file

--- api.js
+++ api.js
@@ -1,15 +1,16 @@
 const request = require('request');
 var rp = require('request-promise');
+const defaults = require('./defaults');
 
 const api = (req, { useCallback = false, json = true } = {}) => {
     const headers = {};
-    if(req && req.cookies && req.cookies.jwt) {
-        headers['Authorization'] = (req.cookies.jwt.startsWith("Bearer ")?"":"Bearer ") + req.cookies.jwt;
+    if (req && req.cookies && req.cookies.jwt) {
+        headers['Authorization'] = (req.cookies.jwt.startsWith("Bearer ") ? "" : "Bearer ") + req.cookies.jwt;
     }
 
     const handler = useCallback ? request : rp;
     return handler.defaults({
-        baseUrl: process.env.BACKEND_URL || 'http://localhost:3030/',
+        baseUrl: defaults.BACKEND_URL,
         json,
         headers
     });
---
+++ defaults.js
@@ -0,0 +1,3 @@
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3030/';
+
+module.exports = {BACKEND_URL};
