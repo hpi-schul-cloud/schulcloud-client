1bccfba842acd0d65fd72cea45b233cfd6603ae6
Tobias Wollowski <tobias.wollowski@student.hpi.de>
2019-03-27 13:55:35 +0100

    added space between date and time

--- helpers/handlebars/helpers/index.js
+++ helpers/handlebars/helpers/index.js
@@ -178,7 +178,7 @@ module.exports = {
         let now = moment();
         let d = moment(date);
         if (d.diff(now) < 0 || d.diff(now, 'days') > 5) {
-            return moment(date).format('DD.MM.YYYY') + "("+moment(date).format('HH:mm')+")";
+            return moment(date).format('DD.MM.YYYY') + " ("+moment(date).format('HH:mm')+")";
         } else {
             return moment(date).fromNow();
         }
