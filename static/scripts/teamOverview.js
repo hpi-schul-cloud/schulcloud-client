d55b8a04a1618e570c0bc3f3f266a2c1a96f860a
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-04-08 11:23:10 +0200

    Implement click handler for team card

--- static/scripts/teamOverview.js
+++ static/scripts/teamOverview.js
@@ -1,11 +1,9 @@
 $(document).ready(() => {
-	console.log('event bla');
 	$('.section-teamInvitations a').click(function handler(e) {
 		e.stopPropagation();
 		e.preventDefault();
 
 		const id = $(this).parents('.sc-card-wrapper').data('id');
-		console.log(id, $(this).parents('.sc-card-wrapper'));
 
 		$.ajax({
 			url: `/teams/invitation/accept/${id}`,
@@ -17,4 +15,35 @@ $(document).ready(() => {
 			$.showNotification('Problem beim Akzeptieren der Einladung', 'danger', true);
 		});
 	});
+
+	$('.btn-member').on('click', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        let teamId = $(this).attr('data-id');
+
+        $.ajax({
+            url: "/teams/" + teamId + "/usersJson"
+        }).done(function (res) {
+            let $memberModal = $('.member-modal');
+            let teamMembers = 'Keine Teilnehmer';
+            let teamName = res.course.name;
+            if(res.course.userIds.length != 0) {
+                teamMembers = '<ol>';
+                res.course.userIds.forEach(member => {
+                    if (member.displayName) {
+                        teamMembers = teamMembers + '<li>' + member.displayName + '</li>';
+                    } else {
+                        teamMembers = teamMembers + '<li>' + member.firstName + ' ' + member.lastName + '</li>';
+                    }
+                });
+                teamMembers = teamMembers + '</ol>';
+            }
+
+            populateModal($memberModal, '.modal-title', 'Mitglieder des Teams: '.concat(teamName));
+            populateModal($memberModal, '#member-modal-body', teamMembers);
+
+            $memberModal.appendTo('body').modal('show');
+        });
+    });
 });
