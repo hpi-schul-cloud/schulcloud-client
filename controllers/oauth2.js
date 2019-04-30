6c3464c259ca3064c5455435219b8daa5e2e643d
dominikglandorf <do.gl@gmx.de>
2019-04-30 18:06:31 +0200

    linting2

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -1,96 +1,100 @@
-const express = require('express')
+const express = require('express');
+
 const router = express.Router();
+const csrf = require('csurf');
 const auth = require('../helpers/authentication');
 const api = require('../api');
-const csrf = require('csurf');
 
 const csrfProtection = csrf({ cookie: true });
 
-router.get('/login', csrfProtection, (req, res, next) => {
-  return api(req).get('/oauth2/loginRequest/' + req.query.login_challenge).then(loginRequest => {
-    req.session.login_challenge = req.query.login_challenge;
-    if (loginRequest.skip) {
-      res.redirect('/oauth2/login/success');
-    } else {
-      res.redirect('/login');
-    }
-  })
-})
+router.get('/login', csrfProtection, (req, res, next) => api(req)
+	.get(`/oauth2/loginRequest/${req.query.login_challenge}`).then((loginRequest) => {
+		req.session.login_challenge = req.query.login_challenge;
+		if (loginRequest.skip) {
+			res.redirect('/oauth2/login/success');
+		} else {
+			res.redirect('/login');
+		}
+	}));
 
 router.get('/login/success', csrfProtection, auth.authChecker, (req, res, next) => {
-  if(!req.session.login_challenge) res.redirect('/dashboard/');
+	if (!req.session.login_challenge) res.redirect('/dashboard/');
 
-  const body = {
-    remember: false,
-    remember_for: 0
-  }
+	const body = {
+		remember: false,
+		remember_for: 0,
+	};
 
-  api(req).patch('/oauth2/loginRequest/' + req.session.login_challenge + '/?accept=1',
-    {body}).then(loginRequest => {
-      delete(req.session.login_challenge);
-	res.redirect(loginRequest.redirect_to);
-    });
+	api(req).patch(`/oauth2/loginRequest/${req.session.login_challenge}/?accept=1`,
+		{ body }).then((loginRequest) => {
+		delete (req.session.login_challenge);
+		res.redirect(loginRequest.redirect_to);
+	});
 });
 
-const acceptConsent = (r, w, challenge, grantScopes, remember=false) => {
+const acceptConsent = (r, w, challenge, grantScopes, remember = false) => {
+	const body = {
+		grant_scope: (Array.isArray(grantScopes) ? grantScopes : [grantScopes]),
+		remember,
+		remember_for: 60 * 60 * 24 * 30,
+	};
 
-  const body = {
-    grant_scope: (Array.isArray(grantScopes) ? grantScopes : [grantScopes]),
-    remember,
-    remember_for: 60*60*24*30
-  }
-
-  return api(r).patch('/oauth2/consentRequest/' +  challenge + '/?accept=1', {body}
-  ).then(consentRequest => w.redirect(consentRequest.redirect_to));
-}
+	return api(r).patch(`/oauth2/consentRequest/${challenge}/?accept=1`, { body })
+		.then(consentRequest => w.redirect(consentRequest.redirect_to));
+};
 
 router.get('/consent', csrfProtection, auth.authChecker, (r, w) => {
-  // This endpoint is hit when hydra initiates the consent flow
-  if (r.query.error) {
-    // An error occurred (at hydra)
-    return w.send(`${r.query.error}<br />${r.query.error_description}`);
-  }
-  return api(r).get('/oauth2/consentRequest/' + r.query.consent_challenge).then(consentRequest => {
-    if (consentRequest.skip) {
-      return acceptConsent(r, w, r.query.consent_challenge, consentRequest.requested_scope);
-    }
-    return w.render('oauth2/consent', {
-      inline: true,
-      title: 'Login mit Schul-Cloud',
-      subtitle: '',
-      client: consentRequest.client.client_name,
-      action: `/oauth2/consent?challenge=${r.query.consent_challenge}`,
-      buttonLabel: 'Akzeptieren',
-      scopes: consentRequest.requested_scope.map(scope =>
-	    ({ display: (scope === 'openid' ? "eine eindeutige Zeichenfolge, die keinen Rückschluss auf deine wahre Identität zulässt" : scope),
-		value: scope}))
-    })
-  })
+	// This endpoint is hit when hydra initiates the consent flow
+	if (r.query.error) {
+		// An error occurred (at hydra)
+		return w.send(`${r.query.error}<br />${r.query.error_description}`);
+	}
+	return api(r).get(`/oauth2/consentRequest/${r.query.consent_challenge}`).then((consentRequest) => {
+		if (consentRequest.skip) {
+			return acceptConsent(r, w, r.query.consent_challenge, consentRequest.requested_scope);
+		}
+		return w.render('oauth2/consent', {
+			inline: true,
+			title: 'Login mit Schul-Cloud',
+			subtitle: '',
+			client: consentRequest.client.client_name,
+			action: `/oauth2/consent?challenge=${r.query.consent_challenge}`,
+			buttonLabel: 'Akzeptieren',
+			scopes: consentRequest.requested_scope.map(scope => ({
+				display: (scope === 'openid'
+					? 'eine eindeutige Zeichenfolge, die keinen Rückschluss auf deine wahre Identität zulässt'
+					: scope),
+				value: scope,
+			})),
+		});
+	});
 });
 
-router.post('/consent', auth.authChecker, (r, w) => {
-  return acceptConsent(r, w, r.query.challenge, r.body.grantScopes, true);
-});
+router.post('/consent', auth.authChecker, (r, w) => acceptConsent(r, w, r.query.challenge, r.body.grantScopes, true));
 
 router.get('/username/:pseudonym', (req, res, next) => {
-  api(req).get('/pseudonym', {
-    qs: {
-      pseudonym: req.params.pseudonym,
-    }
-  }).then(pseudonym => {
-    let shortName, completeName, anonymousName = '???';
-    shortName = completeName = anonymousName;
-    if (pseudonym.data.length) {
-      completeName = `${pseudonym.data[0].user.firstName} ${pseudonym.data[0].user.lastName}`
-      shortName = `${pseudonym.data[0].user.firstName} ${pseudonym.data[0].user.lastName.charAt(0)}.`
-    }
-    res.render('oauth2/username', {
-      completeName,
-      shortName,
-      infoText: 'Der Anbieter dieses Bildungsinhaltes ist nicht im Wissen des echten Namens, da dieser hier direkt aus ' +
-      'der Schul-Cloud abgerufen wird. Es handelt sich um ein sogenanntes Iframe, das Seiten anderer Webserver ' +
-      'anzeigen kann.'});
-  });
+	api(req).get('/pseudonym', {
+		qs: {
+			pseudonym: req.params.pseudonym,
+		},
+	}).then((pseudonym) => {
+		let shortName;
+		let completeName;
+		const anonymousName = '???';
+		completeName = anonymousName;
+		shortName = completeName;
+		if (pseudonym.data.length) {
+			completeName = `${pseudonym.data[0].user.firstName} ${pseudonym.data[0].user.lastName}`;
+			shortName = `${pseudonym.data[0].user.firstName} ${pseudonym.data[0].user.lastName.charAt(0)}.`;
+		}
+		res.render('oauth2/username', {
+			completeName,
+			shortName,
+			infoText: 'Der Anbieter dieses Bildungsinhaltes ist nicht im Wissen des echten Namens, da dieser direkt aus'
+      + ' der Schul-Cloud abgerufen wird. Es handelt sich um ein sogenanntes Iframe, das Seiten anderer Webserver'
+      + ' anzeigen kann.',
+		});
+	});
 });
 
-module.exports = router
+module.exports = router;
--- static/scripts/tools.js
+++ static/scripts/tools.js
@@ -1,85 +1,83 @@
- $(document).ready(function () {
+$(document).ready(() => {
+	const $modals = $('.modal');
+	const $editModal = $('.edit-modal');
+	let customFieldCount = 0;
 
-    var $modals = $('.modal');
-    var $editModal = $('.edit-modal');
-    var customFieldCount = 0;
+	function guidGenerator() {
+		const S4 = function () {
+			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+		};
+		return (`${S4() + S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`);
+	}
 
-    function guidGenerator() {
-        var S4 = function () {
-            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        };
-        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-    }
+	const deleteCustomField = function (customFieldId) {
+		$(`#${customFieldId}`).remove();
+	};
 
-    var deleteCustomField = function (customFieldId) {
-        $('#' + customFieldId).remove();
-    };
+	const addNewCustomField = function (modal) {
+		const $customFields = modal.find('.custom-fields');
+		const $newCustomFieldKey = modal.find('.new-custom-field-key');
+		const $newCustomFieldValue = modal.find('.new-custom-field-value');
+		populateCustomField($customFields, {
+			key: $newCustomFieldKey.val(),
+			value: $newCustomFieldValue.val(),
+		});
+		$newCustomFieldKey.val('');
+		$newCustomFieldValue.val('');
+	};
 
-    var addNewCustomField = function (modal) {
-        var $customFields = modal.find('.custom-fields');
-        var $newCustomFieldKey = modal.find('.new-custom-field-key');
-        var $newCustomFieldValue = modal.find('.new-custom-field-value');
-        populateCustomField($customFields, {
-            key: $newCustomFieldKey.val(),
-            value: $newCustomFieldValue.val()
-        });
-        $newCustomFieldKey.val("");
-        $newCustomFieldValue.val("");
-    };
+	var populateCustomField = function ($customFields, field) {
+		if (!field.key || field.key == '') return;
 
-    var populateCustomField = function ($customFields, field) {
-        if (!field.key || field.key == '') return;
+		const _id = guidGenerator();
+		const $field = $(`<div id='${_id}'>Key: ${field.key}, Value: ${field.value}</div>`)
+			.append($(`<input name='customs[${customFieldCount}][key]' value='${field.key}' type='hidden'></input>`))
+			.append($(`<input name='customs[${customFieldCount}][value]' value='${field.value}' type='hidden'></input>`))
+			.append($("<i class='fa fa-trash-o custom-field-delete' />")
+				.click(deleteCustomField.bind(this, _id)));
+		$customFields.append($field);
+		customFieldCount++;
+	};
 
-        var _id = guidGenerator();
-        var $field = $("<div id='" + _id + "'>Key: " + field.key + ", Value: " + field.value + "</div>")
-            .append($("<input name='customs[" + customFieldCount + "][key]' value='" + field.key + "' type='hidden'></input>"))
-            .append($("<input name='customs[" + customFieldCount + "][value]' value='" + field.value + "' type='hidden'></input>"))
-            .append($("<i class='fa fa-trash-o custom-field-delete' />")
-                .click(deleteCustomField.bind(this, _id))
-            );
-        $customFields.append($field);
-        customFieldCount++;
-    };
+	const populateCustomFields = function (modal, customFields) {
+		const $customFields = modal.find('.custom-fields');
 
-    var populateCustomFields = function (modal, customFields) {
-        var $customFields = modal.find('.custom-fields');
+		// cleanup
+		$customFields.empty();
 
-        // cleanup
-        $customFields.empty();
+		customFields.forEach((field) => {
+			populateCustomField($customFields, field);
+		});
+		modal.find('.new-custom-field-add').click(addNewCustomField.bind(this, modal));
+	};
 
-        customFields.forEach(function (field) {
-            populateCustomField($customFields, field);
-        });
-        modal.find('.new-custom-field-add').click(addNewCustomField.bind(this, modal));
-    };
-
-    /**
+	/**
      * posts a (non-lti) local tool to the server
      * @param modal {Modal} - the modal which has the post-action and the courseId
      * @param tool {object} - the tool which will be created
      */
-    var createLocalTool = function (modal, tool) {
-        var $modalForm = modal.find('.modal-form');
-        var href = $modalForm.attr('action');
-        var courseId = $modalForm.find("input[name='courseId']").val();
-        // cleaning
-        tool.isTemplate = false;
-        tool.courseId = courseId;
-        tool.originTool = tool._id;
-        delete tool.oAuthClientId;
-        delete tool.useIframePseudonym;
-        delete tool._id;
-        $.ajax({
-            action: href,
-            data: tool,
-            method: 'POST',
-            success: function(result) {
-                window.location.href = "/courses/" + courseId;
-            }
-        });
-    };
+	const createLocalTool = function (modal, tool) {
+		const $modalForm = modal.find('.modal-form');
+		const href = $modalForm.attr('action');
+		const courseId = $modalForm.find("input[name='courseId']").val();
+		// cleaning
+		tool.isTemplate = false;
+		tool.courseId = courseId;
+		tool.originTool = tool._id;
+		delete tool.oAuthClientId;
+		delete tool.useIframePseudonym;
+		delete tool._id;
+		$.ajax({
+			action: href,
+			data: tool,
+			method: 'POST',
+			success(result) {
+				window.location.href = `/courses/${courseId}`;
+			},
+		});
+	};
 
-    /**var populateCourseSelection = function (modal, courses) {
+	/** var populateCourseSelection = function (modal, courses) {
         var $selection = modal.find('.course-selection');
         courses.forEach(function (course) {
             var option = document.createElement("option");
@@ -88,28 +86,27 @@
             $selection.append(option);
         });
         $selection.chosen().trigger("chosen:updated");
-    };**/
+    };* */
 
-    $('.template_tool').on('click', function (e) {
-        e.preventDefault();
-        var entry = $(this).attr('href');
-        $.getJSON(entry, function (result) {
-            if (result.tool.isLocal) {
-                createLocalTool($editModal, result.tool);
-            } else {
-                populateModalForm($editModal, {
-                    closeLabel: 'Abbrechen',
-                    submitLabel: 'Speichern',
-                    fields: result.tool
-                });
-                populateCustomFields($editModal, result.tool.customs);
-                $editModal.appendTo('body').modal('show');
-            }
-        });
-    });
+	$('.template_tool').on('click', function (e) {
+		e.preventDefault();
+		const entry = $(this).attr('href');
+		$.getJSON(entry, (result) => {
+			if (result.tool.isLocal) {
+				createLocalTool($editModal, result.tool);
+			} else {
+				populateModalForm($editModal, {
+					closeLabel: 'Abbrechen',
+					submitLabel: 'Speichern',
+					fields: result.tool,
+				});
+				populateCustomFields($editModal, result.tool.customs);
+				$editModal.appendTo('body').modal('show');
+			}
+		});
+	});
 
-    $modals.find('.close, .btn-close').on('click', function () {
-        $modals.modal('hide');
-    });
-
+	$modals.find('.close, .btn-close').on('click', () => {
+		$modals.modal('hide');
+	});
 });
