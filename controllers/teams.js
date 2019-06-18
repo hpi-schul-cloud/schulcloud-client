74f6c97d2b9525a934042671377dfa8d4b322b66
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-06-18 14:20:22 +0200

    Use correct permissions

--- controllers/teams.js
+++ controllers/teams.js
@@ -625,7 +625,7 @@ router.get('/:teamId', async (req, res, next) => {
 				canCreateDir: true,
 				canCreateFile: true,
 				canEditPermissions: permissions.includes('EDIT_ALL_FILES'),
-				canEditEvents: permissions.includes('NEWS_EDIT'),
+				canEditEvents: permissions.includes('CALENDAR_EDIT'),
 				createEventAction: `/teams/${req.params.teamId}/events/`,
 				leaveTeamAction,
 				couldLeave,
