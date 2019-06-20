7e15ef49cde6b24476c6f18e0c9cc13bd8d9f424
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-06-20 15:03:29 +0200

    Fix role name translations

--- controllers/teams.js
+++ controllers/teams.js
@@ -810,7 +810,7 @@ router.get('/:teamId/members', async (req, res, next) => {
 
 	const roleTranslations = {
 		teammember: 'Teilnehmer',
-		teamexpert: 'Externer&nbsp;Experte',
+		teamexpert: 'Externer Experte',
 		teamleader: 'Leiter',
 		teamadministrator: 'Administrator',
 		teamowner: 'Eigentümer',
