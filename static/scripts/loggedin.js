77dd24dcf0ae0d9f0f5db6e5afece0831326fb75
dasGoogle <aaronschlitt@gmx.de>
2019-06-28 15:38:35 +0200

    fix helpdesk submissions

--- static/scripts/loggedin.js
+++ static/scripts/loggedin.js
@@ -65,6 +65,9 @@ function sendFeedback(modal, e) {
             currentState: fmodal.find('#hasHappened').val(),
             targetState: fmodal.find('#supposedToHappen').val()
         },
+        beforeSend(xhr) {
+			xhr.setRequestHeader('Csrf-Token', csrftoken);
+		},
         success: function (result) {
             showAJAXSuccess("Feedback erfolgreich versendet!", fmodal);
         },
