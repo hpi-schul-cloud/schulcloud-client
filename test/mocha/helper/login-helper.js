3f5dd76267554f1a3f0b8bb0884df20d99c63612
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-06-28 15:39:08 +0200

    lint

--- test/mocha/helper/login-helper.js
+++ test/mocha/helper/login-helper.js
@@ -1,32 +1,33 @@
-'use strict';
 
-const student_name = process.env.STUDENT_NAME || 'schueler@schul-cloud.org';
-const password = process.env.PASSWORD || "Schulcloud1!";
+
+const studentName = process.env.STUDENT_NAME || 'schueler@schul-cloud.org';
+const password = process.env.PASSWORD || 'Schulcloud1!';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+
 chai.use(chaiHttp);
 
 const login = (app) => {
-    let agent = chai.request.agent(app); // create agent for storing cookies
-    return new Promise((resolve, reject) => {
-        agent
-            .post('/login/')
-            .send({'username': student_name, 'password': password})
-            .end((err, res) => {
-                if (err) {
-                    reject(err);
-                }
+	const agent = chai.request.agent(app); // create agent for storing cookies
+	return new Promise((resolve, reject) => {
+		agent
+			.post('/login/')
+			.send({ username: studentName, password })
+			.end((err, res) => {
+				if (err) {
+					reject(err);
+				}
 
-                // return agent for making further request in loggedIn state
-                resolve({
-                    agent,
-                    res
-                });
-            });
-    });
+				// return agent for making further request in loggedIn state
+				resolve({
+					agent,
+					res,
+				});
+			});
+	});
 };
 
 module.exports = {
-    login
+	login,
 };
