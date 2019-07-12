93b758dc08c1197e613a30b5c7e4312a33d251da
Adrian Jost <adrianjost@users.noreply.github.com>
2019-07-12 09:57:47 +0200

    linting

--- controllers/pwrecovery.js
+++ controllers/pwrecovery.js
@@ -1,26 +1,27 @@
 
 const express = require('express');
+
 const router = express.Router();
 const api = require('../api');
 
-const obscure_email = (email) => {
-    let parts = email.split("@");
-    let name = parts[0];
-    let result = name.charAt(0);
-    for(var i=1; i<name.length; i++) {
-        result += "*";
-    }
-    result += name.charAt(name.length - 1);
-    result += "@";
-    let domain = parts[1];
-    result += domain.charAt(0);
-    let dot = domain.indexOf(".");
-    for(var j=1; j<dot; j++) {
-        result += "*";
-    }
-    result += domain.substring(dot);
+const obscureEmail = (email) => {
+	const parts = email.split('@');
+	const name = parts[0];
+	let result = name.charAt(0);
+	for (let i = 1; i < name.length; i += 1) {
+		result += '*';
+	}
+	result += name.charAt(name.length - 1);
+	result += '@';
+	const domain = parts[1];
+	result += domain.charAt(0);
+	const dot = domain.indexOf('.');
+	for (let j = 1; j < dot; j += 1) {
+		result += '*';
+	}
+	result += domain.substring(dot);
 
-    return result;
+	return result;
 };
 
 router.get('/response', (req, res, next) => {
@@ -35,7 +36,7 @@ router.get('/:pwId', (req, res, next) => {
 		.then((result) => {
 			if (result.changed) {
 				const error = new Error(
-					'Ihr Passwort wurde bereits über diese URL geändert.'
+					'Ihr Passwort wurde bereits über diese URL geändert.',
 				);
 				error.status = 400;
 				throw error;
@@ -47,7 +48,7 @@ router.get('/:pwId', (req, res, next) => {
 			}
 			return res.render('pwRecovery/pwrecovery', {
 				title: 'Passwort Recovery',
-				subtitle: obscure_email(result.account.username),
+				subtitle: obscureEmail(result.account.username),
 				accountId: result.account._id,
 				resetId: req.params.pwId,
 				action: '/pwrecovery/reset/',
@@ -70,14 +71,13 @@ router.post('/', (req, res, next) => {
 	});
 });
 
-router.post('/reset', function (req, res, next) {
-    api(req).post('/passwordRecovery/reset', {json: req.body}).then(_ => {
-        res.redirect('/login/');
-    }).catch(err => {
-        next(err);
-    });
+router.post('/reset', (req, res, next) => {
+	api(req).post('/passwordRecovery/reset', { json: req.body }).then((_) => {
+		res.redirect('/login/');
+	}).catch((err) => {
+		next(err);
+	});
 });
-
 
 
 module.exports = router;
