60aa50350d6a4445c485e5f65ff8280d56225f9a
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-07-23 15:35:20 +0200

    update version info to server

--- controllers/version.js
+++ controllers/version.js
@@ -1,32 +1,34 @@
-const getRepoInfo = require('git-repo-info');
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const winston = require('winston');
 
-const packages = require('../package.json');
+const { version } = require('../package.json');
 
 const router = express.Router();
 
-const fields = [
-	'abbreviatedSha',
-	'sha',
-	'branch',
-	'tag',
-	'lastTag',
-	'committerDate',
-	'commitsSinceLastTag',
-	'commitMessage',
-];
+const logger = winston.createLogger({
+	transports: [
+		new winston.transports.Console({
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple(),
+			),
+		}),
+	],
+});
 
 router.get('/', (req, res, next) => {
 	if (!process.env.SHOW_VERSION) {
-		res.send(403);
+		return res.sendStatus(403);
 	}
-	const info = getRepoInfo();
-	const response = { };
-	response.version = packages.version;
-	fields.forEach((field) => {
-		response[field] = info[field];
-	});
-	res.json(response);
+	let sha = false;
+	try {
+		sha = fs.readFileSync(path.join(__dirname, '..', 'version'), 'utf8').trim();
+	} catch (error) {
+		logger.error('version file missing', error);
+	}
+	return res.json({ sha, version });
 });
 
 module.exports = router;
