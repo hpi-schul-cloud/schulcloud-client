4efd29d0efe9bee5596493acb2229657faa63bac
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-07-30 12:21:28 +0200

    Added skip consent to student admin view

--- controllers/administration.js
+++ controllers/administration.js
@@ -1566,6 +1566,7 @@ router.get(
 		const accountPromise = api(req).get('/accounts/', {
 			qs: { userId: req.params.id },
 		});
+		const canSkip = permissionsHelper.userHasPermission(res.locals.currentUser, 'STUDENT_SKIP_REGISTRATION');
 
 		Promise.all([userPromise, consentPromise, accountPromise])
 			.then(([user, _consent, [account]]) => {
@@ -1584,6 +1585,8 @@ router.get(
 					user,
 					consentStatusIcon: getConsentStatusIcon(consent.consentStatus),
 					consent,
+					canSkipConsent: canSkip,
+					hasImportHash: user.importHash,
 					hidePwChangeButton,
 					schoolUsesLdap: res.locals.currentSchoolData.ldapSchoolIdentifier,
 					referrer: req.header('Referer'),
