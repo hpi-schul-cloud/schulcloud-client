f427b77efb81baf216beea454dbe55744ef8d025
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-20 16:06:18 +0200

    make partials themable

--- controllers/help.js
+++ controllers/help.js
@@ -9,62 +9,6 @@ const router = express.Router();
 // read here for updateding the tutorials.json: https://docs.schul-cloud.org/display/Intern/Hilfe-Artikel+aktualisieren
 const tutorials = require('../helpers/content/tutorials.json');
 
-const firstStepsItems = [{
-	title: 'Schüler',
-	'img-src': '/images/help/schueler-icon.png',
-	src: '/help/confluence/40304731',
-}, {
-	title: 'Lehrer',
-	'img-src': '/images/help/lehrer-icon.png',
-	src: '/help/confluence/40304726',
-}, {
-	title: 'Admin',
-	'img-src': '/images/help/admin-icon.png',
-	src: '/help/confluence/40304667',
-}, {
-	title: 'Schulleitung',
-	'img-src': '/images/help/schulleitung-icon.png',
-	src: '/help/confluence/40304728',
-},
-];
-const quickHelpItems = [{
-	title: 'Online-Videokurse',
-	icon: 'fa-video-camera',
-	src: 'https://mooc.house/courses/schulcloud2019',
-}, {
-	title: 'Webinare',
-	icon: 'fa-desktop',
-	src: 'https://docs.schul-cloud.org/display/SCDOK/Webinare',
-}, {
-	title: 'Schnellstart-PDF',
-	icon: 'fa-file-pdf-o',
-	// eslint-disable-next-line max-len
-	src: 'https://docs.schul-cloud.org/download/attachments/13828239/HPI%20Schul-Cloud%20-%20Schnellstart%20f%C3%BCr%20Lehrkr%C3%A4fte.pdf',
-},
-];
-const knowledgeItems = [{
-	title: 'Überblick',
-	icon: 'fa-info',
-	src: '/about',
-}, {
-	title: 'FAQ',
-	icon: 'fa-comments',
-	src: '/help/confluence/23232533',
-}, {
-	title: 'Ansprechpartner',
-	icon: 'fa-user',
-	src: '/help/faq/people',
-}, {
-	title: 'Release Notes',
-	icon: 'fa-clipboard',
-	src: '/help/releases',
-}, {
-	title: 'Website (Logout)',
-	icon: 'fa-globe',
-	src: '/logout',
-},
-];
-
 // secure routes
 router.use(authHelper.authChecker);
 
@@ -72,20 +16,11 @@ router.get('/', (req, res, next) => {
 	const isDemo = res.locals.currentUser.roles.every(role => role.name.includes('demo'));
 	const isStudent = res.locals.currentUser.roles.every(role => role.name === 'student');
 
-	const quickhelp = quickHelpItems.slice(0);
-	if (!isDemo && !isStudent) {
-		quickhelp.push({
-			title: 'Willkommens-Dokumente',
-			icon: 'fa-folder-open',
-			src: '/help/faq/documents ',
-		});
-	}
 	res.render('help/help', {
 		title: 'Hilfebereich',
+		isDemo,
+		isStudent,
 		tutorials,
-		knowledgeItems,
-		quickHelpItems: quickhelp,
-		firstStepsItems,
 		demo: isDemo,
 		adminFormIsActive: req.query.activeForm === 'admin',
 		teamFormIsActive: req.query.activeForm === 'team',
