6be14d3cf0815596a876dbd6fc972134b7d50b5e
dasGoogle <aaronschlitt@gmx.de>
2019-08-21 10:30:46 +0200

    sort students by last name in course and team edit

--- controllers/courses.js
+++ controllers/courses.js
@@ -142,6 +142,7 @@ const editCourseHandler = (req, res, next) => {
 	const studentsPromise = getSelectOptions(req, 'users', {
 		roles: ['student', 'demoStudent'],
 		$limit: false,
+		$sort: 'lastName'
 	});
 
 	Promise.all([
