45659f005f294de9656b6722ad6860836fb36e0c
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-21 09:19:10 +0200

    lint

--- config/documents.js
+++ config/documents.js
@@ -8,12 +8,10 @@ const baseFiles = {
 module.exports = {
 	defaultDocuments: () => ({
 		documentBaseDir: process.env.DOCUMENT_BASE_DIR || 'https://schul-cloud-hpi.s3.hidrive.strato.com/',
-		baseFiles: (baseDir) => {
-			return Object.entries(baseFiles).reduce((obj, [key, value]) => {
-				obj[key] = String(new URL(value, baseDir));
-				return obj;
-			}, {});
-		},
+		baseFiles: baseDir => Object.entries(baseFiles).reduce((obj, [key, value]) => {
+			obj[key] = String(new URL(value, baseDir));
+			return obj;
+		}, {}),
 		otherFiles: {},
-	})
+	}),
 };
