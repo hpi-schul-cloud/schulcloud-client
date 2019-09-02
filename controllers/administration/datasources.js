84b446e8217bb2cd01f368e96fff020bcf7d7755
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-09-02 15:39:48 +0200

    webuntis page mocks

--- controllers/administration/datasources.js
+++ controllers/administration/datasources.js
@@ -10,30 +10,37 @@ const router = express.Router();
 
 router.use(authHelper.authChecker);
 
+
+router.get('/webuntis/edit', (req, res, next) => {
+	res.render('administration/datasources/webuntis-edit', {
+		title: 'WebUntis bearbeiten',
+		action: '/administration/datasources/webuntis/edit',
+	});
+});
+router.post('/webuntis/edit', (req, res, next) => {
+	if (!req.body.endpoint) {
+		req.body.endpoint = 'erato.webuntis.com';
+	}
+	console.log('BODY:', req.body);
+	res.redirect('/administration/datasources/webuntis');
+});
+
+
+router.post('/webuntis', (req, res, next) => {
+	console.log('BODY:', req.body);
+	res.redirect('/administration/datasources/webuntis');
+});
+router.get('/webuntis', (req, res, next) => {
+	res.render('administration/datasources/webuntis', {
+		title: 'WebUntis',
+		action: '/administration/datasources/webuntis',
+		hasLoginData: !!(new Date().getSeconds() % 2),
+	});
+});
 router.get('/', (req, res, next) => {
-	const context = req.query.context || '';
-	const contextId = req.query.contextId || '';
 	res.render('administration/datasources/overview', {
 		title: 'Datenquellen',
 	});
-});
-
-router.get('/:newsId', (req, res, next) => {
-	api(req)
-		.get(`/news/${req.params.newsId}`, {
-			qs: {},
-		})
-		.then((news) => {
-			news.url = `/news/${news._id}`;
-			res.render('news/article', {
-				title: news.title,
-				news,
-				isRSS: news.source === 'rss',
-			});
-		})
-		.catch((err) => {
-			next(err);
-		});
 });
 
 
