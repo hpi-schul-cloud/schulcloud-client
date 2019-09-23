548d5b82ad4563dc0db92528de2da6c7c2c0c826
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-09-23 16:54:19 +0200

    lint

--- static/scripts/sentry.js
+++ static/scripts/sentry.js
@@ -1,5 +1,5 @@
 import * as Browser from '@sentry/browser';
 import * as Integrations from '@sentry/integrations';
 
-window.Sentry = Browser,
-window.SentryIntegrations = Integrations
+window.Sentry = Browser;
+window.SentryIntegrations = Integrations;
