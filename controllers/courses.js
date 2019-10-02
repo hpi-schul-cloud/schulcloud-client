bc42cd32a46cfa5878099e0cf786f36e719c9321
BlackGhostHD <36112357+BlackGhostHD@users.noreply.github.com>
2019-10-02 11:37:58 +0200

    SC-1976 | removed limit of classes displayed

--- controllers/courses.js
+++ controllers/courses.js
@@ -136,10 +136,10 @@ const editCourseHandler = (req, res, next) => {
 			qs: {
 				schoolId: res.locals.currentSchool,
 				$populate: ['year'],
-				$limit: 1000,
+				$limit: -1,
 			},
 		})
-		.then(data => data.data);
+		//.then(data => data.data); needed when pagination is not disabled
 	const teachersPromise = getSelectOptions(req, 'users', {
 		roles: ['teacher', 'demoTeacher'],
 		$limit: false,
