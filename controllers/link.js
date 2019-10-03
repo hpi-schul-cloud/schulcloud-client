91b6b23b799327e2c66472bfb0268fee16daed5d
dasGoogle <aaronschlitt@gmx.de>
2019-10-03 15:56:55 +0200

    code style

--- controllers/link.js
+++ controllers/link.js
@@ -1,33 +1,34 @@
 const express = require('express');
+
 const router = express.Router();
 const api = require('../api');
 
-router.post('/', function (req, res, next) {
-    // check first if target already exists (preventing db to be wasted)
-    let target = `${(req.headers.origin || process.env.HOST)}/${req.body.target}`;
-    api(req).get("/link/", { qs: { target: target } }).then(result => {
-        let existingLink = result.data[0];
-        if (!existingLink) {
-            api(req).post("/link/", { json: { target: target } }).then(data => {
-                data.newUrl = `${(req.headers.origin || process.env.HOST)}/link/${data._id}`;
-                res.json(data);
-            });
-        } else {
-            existingLink.newUrl = `${(req.headers.origin || process.env.HOST)}/link/${existingLink._id}`;
-            res.json(existingLink);
-        }
-    }).catch(err => next(err));
+router.post('/', (req, res, next) => {
+	// check first if target already exists (preventing db to be wasted)
+	const target = `${(req.headers.origin || process.env.HOST)}/${req.body.target}`;
+	api(req).get('/link/', { qs: { target } }).then((result) => {
+		const existingLink = result.data[0];
+		if (!existingLink) {
+			api(req).post('/link/', { json: { target } }).then((data) => {
+				data.newUrl = `${(req.headers.origin || process.env.HOST)}/link/${data._id}`;
+				res.json(data);
+			});
+		} else {
+			existingLink.newUrl = `${(req.headers.origin || process.env.HOST)}/link/${existingLink._id}`;
+			res.json(existingLink);
+		}
+	}).catch(err => next(err));
 });
 
-//handles expired registration links
-router.get('/expired', function (req, res, next) {
-    res.render('link/expired');
+// handles expired registration links
+router.get('/expired', (req, res, next) => {
+	res.render('link/expired');
 });
 
 // handles redirecting from client
-router.get('/:id', function (req, res, next) {
-    let baseUrl = process.env.PUBLIC_BACKEND_URL || 'http://localhost:3030';
-    res.redirect(`${baseUrl}/link/${req.params.id}?includeShortId=true`);
+router.get('/:id', (req, res, next) => {
+	const baseUrl = process.env.PUBLIC_BACKEND_URL || 'http://localhost:3030';
+	res.redirect(`${baseUrl}/link/${req.params.id}?includeShortId=true`);
 });
 
 module.exports = router;
