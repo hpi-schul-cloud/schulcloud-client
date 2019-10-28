8bc4440a92ed227e9c5fe8a4d7b0c834b0d2313e
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2019-10-28 18:06:27 +0100

    made script reusable and added to settings

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,3 +1,5 @@
+import './pwd.js';
+
 /* global introJs */
 $(document).ready(function() {
 
@@ -129,19 +131,6 @@ $(document).ready(function() {
         $school.trigger('chosen:updated');
         $school.trigger('change');
     }
-
-    $("#show_hide_password a").on('click', function(event) {
-        event.preventDefault();
-        if($('#show_hide_password input').attr("type") == "text"){
-            $('#show_hide_password input').attr('type', 'password');
-            $('#show_hide_password i').addClass( "fa-eye-slash" );
-            $('#show_hide_password i').removeClass( "fa-eye" );
-        }else if($('#show_hide_password input').attr("type") == "password"){
-            $('#show_hide_password input').attr('type', 'text');
-            $('#show_hide_password i').removeClass( "fa-eye-slash" );
-            $('#show_hide_password i').addClass( "fa-eye" );
-        }
-    });
 
 });
 
