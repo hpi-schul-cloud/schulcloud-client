764803e93939b38f73594ded42182719e0fc932b
dasGoogle <aaronschlitt@gmx.de>
2019-11-04 10:55:54 +0100

    remove need for redirect from tests

--- test/mocha/link/link.test.js
+++ test/mocha/link/link.test.js
@@ -1,39 +1,33 @@
-'use strict';
-
-const assert = require('assert');
-const app = require('../../../app');
 const chai = require('chai');
-const expect = chai.expect;
+
+const { expect } = chai;
 const chaiHttp = require('chai-http');
+const app = require('../../../app');
+
 chai.use(chaiHttp);
 
-describe('Link tests', function () {
-    before(function (done) {
-        this.server = app.listen(3031);
-        this.server.once('listening', () => {
-            done();
-        });
-    });
+describe.only('Link tests', () => {
+	before((done) => {
+		this.server = app.listen(3031);
+		this.server.once('listening', () => {
+			done();
+		});
+	});
 
-    after(function (done) {
-        this.server.close(done);
-    });
+	after((done) => {
+		this.server.close(done);
+	});
 
-    it('GET /link/:id', function () {
-        let testId = "sc42";
-        return new Promise((resolve, reject) => {
-            chai.request(app)
-                .get('/link/' + testId)
-                .end((err, res) => {
-
-                    // NOTE It is unclear why res.redirects is an array. However, to make the test agnostic, we loop through all
-                    let baseUrl = process.env.BACKEND_URL || 'http://localhost:3030';
-
-                    expect(res.redirects.some(link => {
-                        return link.includes(`${baseUrl}/link/${testId}`)
-                    })).to.be.true;
-                    resolve();
-                });
-        });
-    });
+	it('GET /link/:id', () => {
+		const testId = 'sc42';
+		return new Promise((resolve) => {
+			chai.request(app)
+				.get(`/link/${testId}`)
+				.end((err, res) => {
+					// check that this link is redirected to error page
+					expect(res.text).to.contain('Dieser Link existiert nicht.');
+					resolve();
+				});
+		});
+	});
 });
