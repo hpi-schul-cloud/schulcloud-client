a446c0da25dc1549e95b53f8aa95829286068d89
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-11-06 11:14:17 +0100

    remove console and lint

--- static/scripts/courses.js
+++ static/scripts/courses.js
@@ -1,21 +1,20 @@
 $(document).ready(() => {
-    console.log('load courses js')
-    $('.js-course-name-input').change(function () {
-        $(this).val($(this).val().trim());
-    });
+	$('.js-course-name-input').change(function () {
+		$(this).val($(this).val().trim());
+	});
 
-    $('.btn-hidden-toggle').click(function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        var $hiddenToggleBtn = $(this);
-        var $hiddenToggleIcon = $(this).find('.fa');
-        var $card = $(this).closest('.card');
-        const href = $(this).attr('href');
-        $.ajax({
-            method: 'PATCH',
-            url: `${href}?json=true`,
-            data: {hidden: !$hiddenToggleIcon.hasClass('fa-eye-slash')},
-            success: function(result) {
+	$('.btn-hidden-toggle').click(function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+		let $hiddenToggleBtn = $(this);
+		let $hiddenToggleIcon = $(this).find('.fa');
+		let $card = $(this).closest('.card');
+		const href = $(this).attr('href');
+		$.ajax({
+			method: 'PATCH',
+			url: `${href}?json=true`,
+			data: { hidden: !$hiddenToggleIcon.hasClass('fa-eye-slash') },
+			success(result) {
                 if (result.hidden) {
                     $hiddenToggleIcon.addClass('fa-eye-slash');
                     $hiddenToggleIcon.removeClass('fa-eye');
@@ -27,25 +26,25 @@ $(document).ready(() => {
                     $hiddenToggleBtn.attr('data-original-title', "Thema verstecken");
                     $card.removeClass('card-transparent');
                 }
-            }
-        });
-    });
+            },
+		});
+	});
 
-    $('.btn-create-invitation').click(function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        let target = $(this).attr("data-href") + 'addStudent';
-        let $invitationModal = $('.invitation-modal');
-        $.ajax({
-            type: "POST",
-            url: "/link/",
-            beforeSend(xhr) {
-                xhr.setRequestHeader('Csrf-Token', csrftoken);
-            },
-            data: {
-                target: target
-            },
-            success: function(data) {
+	$('.btn-create-invitation').click(function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+		const target = `${$(this).attr("data-href")  }addStudent`;
+		const $invitationModal = $('.invitation-modal');
+		$.ajax({
+			type: 'POST',
+			url: '/link/',
+			beforeSend(xhr) {
+				xhr.setRequestHeader('Csrf-Token', csrftoken);
+			},
+			data: {
+				target,
+			},
+			success(data) {
                 populateModalForm($invitationModal, {
                     title: 'Einladungslink generiert!',
                     closeLabel: 'Abbrechen',
@@ -59,37 +58,37 @@ $(document).ready(() => {
 
                 $invitationModal.appendTo('body').modal('show');
 
-            }
-        });
-    });
+            },
+		});
+	});
 
 
-    $('.btn-import-topic').click(function (e) {
+	$('.btn-import-topic').click(function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+		const courseId = $(this).attr('data-courseId');
+		const $importModal = $('.import-modal');
+		populateModalForm($importModal, {
+			title: 'Thema importieren',
+			closeLabel: 'Abbrechen',
+			submitLabel: 'Speichern',
+			fields: { courseId },
+		});
+
+		const $modalForm = $importModal.find('.modal-form');
+		$modalForm.attr('action', `/courses/${courseId}/importTopic`);
+		$importModal.appendTo('body').modal('show');
+	});
+
+	$('.move-handle').click((e) => {
         e.stopPropagation();
-        e.preventDefault();
-        let courseId = $(this).attr("data-courseId");
-        let $importModal = $('.import-modal');
-        populateModalForm($importModal, {
-            title: 'Thema importieren',
-            closeLabel: 'Abbrechen',
-            submitLabel: 'Speichern',
-            fields: {courseId: courseId}
-        });
-
-        let $modalForm = $importModal.find(".modal-form");
-        $modalForm.attr('action', `/courses/${courseId}/importTopic`);
-        $importModal.appendTo('body').modal('show');
     });
 
-    $(".move-handle").click(function(e) {
-        e.stopPropagation();
-    });
-
-    if ($('#topic-list').length) {
-        $("#topic-list").sortable({
-            placeholder: "ui-state-highlight",
-            handle: '.move-handle',
-            update: function(event, ui) {
+	if ($('#topic-list').length) {
+		$('#topic-list').sortable({
+			placeholder: 'ui-state-highlight',
+			handle: '.move-handle',
+			update(event, ui) {
                 let positions = {};
                 $( "#topic-list .card-topic" ).each(function(i) {
                     positions[($( this ).attr("data-topicId"))] = i;
@@ -101,20 +100,20 @@ $(document).ready(() => {
                     data: positions
                 });
             },
-        });
+		});
 
-        $( "#topic-list" ).disableSelection();
-    }
+		$('#topic-list').disableSelection();
+	}
 
-    $('.btn-create-share-course').click(function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        let courseId = $(this).attr("data-courseId");
-        let $shareModal = $('.share-modal');
-        $.ajax({
-            type: "GET",
-            url: `/courses/${courseId}/share/`,
-            success: function(data) {
+	$('.btn-create-share-course').click(function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+		const courseId = $(this).attr('data-courseId');
+		const $shareModal = $('.share-modal');
+		$.ajax({
+			type: 'GET',
+			url: `/courses/${courseId}/share/`,
+			success(data) {
                 populateModalForm($shareModal, {
                     title: 'Kopiercode generiert!',
                     closeLabel: 'Schließen',
@@ -133,7 +132,7 @@ $(document).ready(() => {
                 $shareqrbox.empty();
                 $shareqrbox.append('<p>Alternativ kannst du deinen Lehrer-Kollegen auch folgenden QR-Code zeigen. </p>')
                 $shareqrbox.append(image);
-            }
-        });
-    });
+            },
+		});
+	});
 });
