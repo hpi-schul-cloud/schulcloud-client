a01619e8727fa981d9bb60e3e5657a78b37e17cc
BlackGhostHD <36112357+BlackGhostHD@users.noreply.github.com>
2019-11-07 16:12:08 +0100

    Update logs.js

--- controllers/logs.js
+++ controllers/logs.js
@@ -9,31 +9,33 @@ const helper = require('../helpers/googleAnalytics');
 
 const logger = require('../helpers/logger');
 
-logger.info('Google Analytics Tracking ID: ' + process.env.SW_GOOGLE_ANALYTICS_TRACKING_ID);
+logger.info(`Google Analytics Tracking ID: ${  process.env.SW_GOOGLE_ANALYTICS_TRACKING_ID}`);
 
 const router = express.Router();
+
+let request = require('request');
 
 router.use(require('../helpers/authentication').authChecker);
 
 /**
  * replaces id occurences of given url.
  * may result in false positives if url slugs have a length of 24 characters
- * @param {string} url 
+ * @param {string} url
  */
-function idCleanup(url){
-    const match = /\/[0-9a-f]{24}/g;
-    if(url.match(match)){
-        return url.replace(match,'/ID');
-    }
-    return url;
+function idCleanup(url) {
+	const match = /\/[0-9a-f]{24}/g;
+	if (url.match(match)) {
+		return url.replace(match, '/ID');
+	}
+	return url;
 }
 
 // removes query string and anchor from url
 function getPathFromUrl(url) {
-    return url.split(/[?#]/)[0];
-  }
+	return url.split(/[?#]/)[0];
+}
 
-router.post('/', function (req, res, next) {
+router.post('/', (req, res, next) => {
     let data = req.body;
     let context = data.attributes.context;
     data.attributes.url = getPathFromUrl(idCleanup(data.attributes.url));
@@ -67,7 +69,58 @@ router.post('/', function (req, res, next) {
         cd5: res.locals.currentSchool,
         cd6: context['networkProtocol'], // http1.1 / http2 / unknown
     };
+    let xApi = {
+        actor: {
+            account: {
+                id: res.locals.currentUser._id,
+                school_id: res.locals.currentSchool,
+                roles: res.locals.currentUser.roles.map(r => r.name)
+            },
+            objectType: 'Agent'
+        },
+        verb: {
+            id: 'http://id.tincanapi.com/verb/viewed',
+            display: {
+                "en-US": 'viewed'
+            }
+        },
+        object: {
+            id: data.attributes.url,
+            objectType: 'Activity'
+        },
+        context: {
+            contextActivities: {
+                'queue-time': data.attributes['qt'], // queue time (integer)
+
+                'first-paint': context['first-paint'], // cm1 is first paint ms
+                'time-to-interactive': context['time-to-interactive'], // cm2 is time to interactive ms
+                'page-loaded': context['page-loaded'], // page load time ms
+                'dom-interactive-time': context['dom-interactive-time'], // dom interactive time ms
+                'dom-content-loaded': context['dom-content-loaded'], // content load time ms
+                'downlink': context['downlink'], // download speed in mbit/s
+                'request-start': context['request-start'],
+                'response-start': context['response-start'],
+                'response-end': context['response-end'],
+
+                'connection': context['connection'], // connection type http://wicg.github.io/netinfo/ 
+                'localhost': data.attributes.url.includes('localhost') ? 'local' : 'default',
+                'sw-offline': req.headers['sw-offline'] === 'true' ? true : false,
+                'sw-enabled': req.headers['sw-enabled'] === 'true' ? true : false,
+                'networkProtocol': context['networkProtocol'] // http1.1 / http2 / unknown
+            }
+        }
+    };
     api(req).post('/analytics', { json: hit }).then(result => {
+
+        request.post('http://localhost:3000/insights', {
+            json: xApi
+          }, (error, res, body) => {
+            if (error) {
+              logger.error(error)
+              return
+            }
+            logger.info(`statusCode: ${res.statusCode}`)
+          })
         res.send(result);
     }).catch(_ => {
         res.send('error');
