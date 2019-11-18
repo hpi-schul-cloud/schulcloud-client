0cdfabbaf25b91a5dde5e023e6efb0eba0931bff
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-11-18 14:01:45 +0100

    scroll hotfix

--- static/scripts/help.js
+++ static/scripts/help.js
@@ -1,21 +1,24 @@
-import livesearch from "./helpers/livesearch.js";
-import { resizeIframes } from "./helpers/iFrameResize.js";
-import "./help/contactForm.js";
+import livesearch from './helpers/livesearch.js';
+import { resizeIframes } from './helpers/iFrameResize.js';
+import './help/contactForm.js';
 
 
-$(document).ready(function () {
-    $('.btn-poll').on('click', function (e) {
-        e.preventDefault();
+$(document).ready(() => {
+	$('.btn-poll').on('click', (e) => {
+		e.preventDefault();
 
-        document.cookie = "pollClicked=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-    });
+		document.cookie = 'pollClicked=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+	});
 });
 
 // iFrame full height
 resizeIframes(undefined, () => {
-    document.querySelectorAll(".mobile-warning").forEach((warning) => {
-        warning.remove();
-    });
+	document.querySelectorAll('.mobile-warning').forEach((warning) => {
+		warning.remove();
+	});
+	document.querySelectorAll('iframe').forEach((frame) => {
+		frame.setAttribute('scrolling', 'no');
+	});
 });
 
 
@@ -40,41 +43,40 @@ window.addEventListener("message", (event) => {
 */
 
 // confluence live-search
-function truncate(text, length){
-    if (text.length <= length) {
-        return text;
-    }
-    const subString = text.substr(0, length-1);
-    return subString.substr(0, subString.lastIndexOf(' ')) + "...";
+function truncate(text, length) {
+	if (text.length <= length) {
+		return text;
+	}
+	const subString = text.substr(0, length - 1);
+	return `${subString.substr(0, subString.lastIndexOf(' '))}...`;
 }
 
-function extractResults(result){
-    return result.results;
+function extractResults(result) {
+	return result.results;
 }
 
-function parseData(result){
-    if(result){
-        return {
-            class: "",
-            link: `/help/confluence/${result.id}`,
-            title: result.title,
-            short_description: truncate(result.bodyTextHighlights, 100)
-        };
-    } else{
-        return {
-            class: "disabled",
-            link: "#",
-            title: "Keine Ergebnisse gefunden 😪",
-            short_description: "Probiere es mit anderen Suchbegriffen erneut"
-        };
-    }
+function parseData(result) {
+	if (result) {
+		return {
+			class: '',
+			link: `/help/confluence/${result.id}`,
+			title: result.title,
+			short_description: truncate(result.bodyTextHighlights, 100),
+		};
+	}
+	return {
+		class: 'disabled',
+		link: '#',
+		title: 'Keine Ergebnisse gefunden 😪',
+		short_description: 'Probiere es mit anderen Suchbegriffen erneut',
+	};
 }
 const config = {
-    url: "https://docs.schul-cloud.org/rest/searchv3/1.0/search?queryString=${inputValue}&where=SCDOK&type=page&pageSize=10&highlight=false",
-    extractResultArray: extractResults,
-    dataParser: parseData,
-    livesearchRootSelector: ".live-search",
-    inputSelector: "input.search",
-    resultContainerSelector: ".live-search-results"
+	url: 'https://docs.schul-cloud.org/rest/searchv3/1.0/search?queryString=${inputValue}&where=SCDOK&type=page&pageSize=10&highlight=false',
+	extractResultArray: extractResults,
+	dataParser: parseData,
+	livesearchRootSelector: '.live-search',
+	inputSelector: 'input.search',
+	resultContainerSelector: '.live-search-results',
 };
 livesearch(config);
