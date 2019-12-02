dce64e70a3d7f6bb14ecbe5d0eb92068524fd21e
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-12-02 15:50:15 +0100

    restore ifCondBool

--- helpers/handlebars/helpers/index.js
+++ helpers/handlebars/helpers/index.js
@@ -6,38 +6,39 @@ const permissionsHelper = require('../../permissions');
 
 moment.locale('de');
 
+const ifCondBool = (v1, operator, v2) => {
+	switch (operator) {
+		case '==':
+			return (v1 == v2);
+		case '===':
+			return (v1 === v2);
+		case '!=':
+			return (v1 != v2);
+		case '!==':
+			return (v1 !== v2);
+		case '<':
+			return (v1 < v2);
+		case '<=':
+			return (v1 <= v2);
+		case '>':
+			return (v1 > v2);
+		case '>=':
+			return (v1 >= v2);
+		case '&&':
+			return (v1 && v2);
+		case '&& !':
+			return (v1 && !v2);
+		case '||':
+			return (v1 || v2);
+		case '|| !':
+			return (v1 || !v2);
+		default:
+			return false;
+	}
+};
+
 const helpers = app => ({
 	pagination: require('./pagination'),
-	ifCondBool: (v1, operator, v2) => {
-		switch (operator) {
-			case '==':
-				return (v1 == v2);
-			case '===':
-				return (v1 === v2);
-			case '!=':
-				return (v1 != v2);
-			case '!==':
-				return (v1 !== v2);
-			case '<':
-				return (v1 < v2);
-			case '<=':
-				return (v1 <= v2);
-			case '>':
-				return (v1 > v2);
-			case '>=':
-				return (v1 >= v2);
-			case '&&':
-				return (v1 && v2);
-			case '&& !':
-				return (v1 && !v2);
-			case '||':
-				return (v1 || v2);
-			case '|| !':
-				return (v1 || !v2);
-			default:
-				return false;
-		}
-	},
 	ifArray: (item, options) => {
 		if (Array.isArray(item)) {
 			return options.fn(item);
