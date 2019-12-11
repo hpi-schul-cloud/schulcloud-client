c88798e4404cb9613dea6a576f7752ed7ed448a7
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-12-11 14:26:09 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,28 +1,18 @@
-const request = require('request');
 const rp = require('request-promise');
 
-const api = (req, { useCallback = false, json = true, backend = 'server' } = {}) => {
+const baseUrl = process.env.BACKEND_URL || 'http://localhost:3030/';
+const { KEEP_ALIVE } = process.env;
+
+const api = (req, { json = true } = {}) => {
 	const headers = {};
 	if (req && req.cookies && req.cookies.jwt) {
 		headers.Authorization = (req.cookies.jwt.startsWith('Bearer ') ? '' : 'Bearer ') + req.cookies.jwt;
 	}
-	if (process.env.KEEP_ALIVE) {
+	if (KEEP_ALIVE) {
 		headers.Connection = 'Keep-Alive';
 	}
 
-	const handler = useCallback ? request : rp;
-
-	let baseUrl;
-	switch (backend) {
-		case 'editor':
-			baseUrl = process.env.EDITOR_URL || 'http://localhost:4001';
-			break;
-		case 'server':
-		default:
-			baseUrl = process.env.BACKEND_URL || 'http://localhost:3030/';
-	}
-
-	return handler.defaults({
+	return rp.defaults({
 		baseUrl,
 		json,
 		headers,
