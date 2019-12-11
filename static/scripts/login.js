ead6cc12cd28e8b97300d2c34b35ece37c4b05d9
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-12-11 16:36:40 +0100

    undo linterstuff

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -2,12 +2,13 @@ import './pwd.js';
 import './cleanup'; // see loggedin.js for loggedin users
 
 /* global introJs */
-$(document).ready(() => {
+$(document).ready(function() {
+
 	// reset localStorage when new version is Published
 	const newVersion = 1;
 	const currentVersion = parseInt(localStorage.getItem('homepageVersion') || '0', 10);
 
-	if (currentVersion < newVersion) {
+	if(currentVersion < newVersion){
 		localStorage.clear();
 		localStorage.setItem('homepageVersion', newVersion.toString());
 	}
@@ -22,132 +23,135 @@ $(document).ready(() => {
 	  \\ \\_\\ \\_\\ \\_\\    /\\_____\\   \\ \`\\____\\ \\____\\\\ \\_\\ \\_\\ \\____/ /\\____\\/_____/ \\ \\____//\\____\\ \\____/\\ \\____/\\ \\___,_\\
 	   \\/_/\\/_/\\/_/    \\/_____/    \\/_____/\\/____/ \\/_/\\/_/\\/___/  \\/____/         \\/___/ \\/____/\\/___/  \\/___/  \\/__,_ /
 	`);
-		console.log('Mit Node, React und Feathers verknüpfst du eher die Sprache Javascript als Englisch? Du suchst ein junges Team, lockere Atmosphäre und flache Hierarchien? Dann schau dir unsere Stellen an: https://schul-cloud.org/community#jobs');
-	} catch (e) {
+		console.log("Mit Node, React und Feathers verknüpfst du eher die Sprache Javascript als Englisch? Du suchst ein junges Team, lockere Atmosphäre und flache Hierarchien? Dann schau dir unsere Stellen an: https://schul-cloud.org/community#jobs");
+	} catch(e) {
 		// no log
 	}
 
-	const $btnToggleProviers = $('.btn-toggle-providers');
-	const $btnHideProviers = $('.btn-hide-providers');
-	const $btnLogin = $('.btn-login');
-	const $loginProviders = $('.login-providers');
-	const $school = $('.school');
-	const $systems = $('.system');
-	const $modals = $('.modal');
-	const $pwRecoveryModal = $('.pwrecovery-modal');
-	const $submitButton = $('#submit-login');
+    var $btnToggleProviers = $('.btn-toggle-providers');
+    var $btnHideProviers = $('.btn-hide-providers');
+    var $btnLogin = $('.btn-login');
+    var $loginProviders = $('.login-providers');
+    var $school = $('.school');
+    var $systems = $('.system');
+    var $modals = $('.modal');
+    var $pwRecoveryModal = $('.pwrecovery-modal');
+    var $submitButton = $('#submit-login');
 
-	var incTimer = function () {
-		setTimeout(() => {
-			if (countdownNum != 1) {
-				countdownNum--;
-				$submitButton.val(`Bitte ${countdownNum} Sekunden warten`);
-				incTimer();
-			} else {
-				$submitButton.val('Anmelden');
-			}
-		}, 1000);
-	};
+    var incTimer = function(){
+        setTimeout (function(){
+            if(countdownNum != 1){
+                countdownNum--;
+                $submitButton.val('Bitte ' + countdownNum + ' Sekunden warten');
+                incTimer();
+            } else {
+                $submitButton.val('Anmelden');
+            }
+        },1000);
+    };
 
-	if ($submitButton.data('timeout')) {
-		setTimeout(() => {
-			$submitButton.prop('disabled', false);
-		}, $submitButton.data('timeout') * 1000);
+    if($submitButton.data('timeout')){
+        setTimeout (function(){
+            $submitButton.prop('disabled', false);
+        },$submitButton.data('timeout')*1000);
 
-		var countdownNum = $submitButton.data('timeout');
-		incTimer();
-	}
+        var countdownNum = $submitButton.data('timeout');
+        incTimer();
+    }
 
-	const loadSystems = function (schoolId) {
-		$systems.empty();
-		$.getJSON(`/login/systems/${schoolId}`, (systems) => {
-			systems.forEach((system) => {
-				const systemAlias = system.alias ? ` (${system.alias})` : '';
-				let selected;
-				if (localStorage.getItem('loginSystem') == system._id) {
-					selected = true;
-				}
-				$systems.append(`<option ${selected ? 'selected' : ''} value="${system._id}">${system.type}${systemAlias}</option>`);
-			});
-			$systems.trigger('chosen:updated');
-			if (systems.length < 2) {
-				$systems.parent().hide();
-			}
-		});
-	};
+    var loadSystems = function(schoolId) {
+        $systems.empty();
+        $.getJSON('/login/systems/' + schoolId, function(systems) {
+            systems.forEach(function(system) {
+                var systemAlias = system.alias ? ' (' + system.alias + ')' : '';
+                let selected;
+                if(localStorage.getItem('loginSystem') == system._id) {
+                    selected = true;
+                }
+                $systems.append('<option ' + (selected ? 'selected': '') + ' value="' + system._id + '">' + system.type + systemAlias + '</option>');
+            });
+            $systems.trigger('chosen:updated');
+            systems.length == 1 ? $systems.parent().hide() : $systems.parent().show();
+        });
+        if (systems.length < 2){
+            $systems.parent().hide()
+            return;
+        }
+    };
 
-	$btnToggleProviers.on('click', (e) => {
-		e.preventDefault();
-		$btnToggleProviers.hide();
-		$loginProviders.show();
-	});
+    $btnToggleProviers.on('click', function(e) {
+        e.preventDefault();
+        $btnToggleProviers.hide();
+        $loginProviders.show();
+    });
 
-	$btnHideProviers.on('click', (e) => {
-		e.preventDefault();
-		$btnToggleProviers.show();
-		$loginProviders.hide();
-		$school.val('');
-		$school.trigger('chosen:updated');
-		$systems.val('');
-		$systems.trigger('chosen:updated');
-	});
+    $btnHideProviers.on('click', function(e) {
+        e.preventDefault();
+        $btnToggleProviers.show();
+        $loginProviders.hide();
+        $school.val('');
+        $school.trigger('chosen:updated');
+        $systems.val('');
+        $systems.trigger('chosen:updated');
+    });
 
-	$btnLogin.on('click', (e) => {
+    $btnLogin.on('click', function(e) {
 		const school = $school.val();
 		const system = $systems.val();
-		if (school) {
+		if(school){
 			localStorage.setItem('loginSchool', school);
 		}
-		if (system) {
+		if(system){
 			localStorage.setItem('loginSystem', system);
 		}
-	});
+    });
 
-	$school.on('change', function () {
-		const id = $(this).val();
-		loadSystems(id);
-		$school.find('option').not(`[value='${id}']`).removeAttr('selected');
-		$school.find(`option[value='${id}']`).attr('selected', true);
-		$school.trigger('chosen:updated');
-	});
+    $school.on('change', function() {
+        const id = $(this).val();
+        loadSystems( id );
+        $school.find("option").not("[value='"+id+"']").removeAttr('selected');
+        $school.find("option[value='"+id+"']").attr('selected',true);
+        $school.trigger('chosen:updated');
+    });
 
-	$('.submit-pwrecovery').on('click', (e) => {
-		e.preventDefault();
-		populateModalForm($pwRecoveryModal, {
-			title: 'Passwort Zurücksetzen',
-			closeLabel: 'Abbrechen',
-			submitLabel: 'Abschicken',
-		});
-		$pwRecoveryModal.appendTo('body').modal('show');
-	});
+    $('.submit-pwrecovery').on('click', function(e) {
+        e.preventDefault();
+        populateModalForm($pwRecoveryModal, {
+            title: 'Passwort Zurücksetzen',
+            closeLabel: 'Abbrechen',
+            submitLabel: 'Abschicken'
+        });
+        $pwRecoveryModal.appendTo('body').modal('show');
+    });
 
-	$modals.find('.close, .btn-close').on('click', () => {
-		$modals.modal('hide');
-	});
+    $modals.find('.close, .btn-close').on('click', function() {
+        $modals.modal('hide');
+    });
 
-	// if stored login system - use that
-	if (localStorage.getItem('loginSchool')) {
-		$btnToggleProviers.hide();
-		$loginProviders.show();
-		$school.val(localStorage.getItem('loginSchool'));
-		$school.trigger('chosen:updated');
-		$school.trigger('change');
-	}
+    // if stored login system - use that
+    if(localStorage.getItem('loginSchool')) {
+        $btnToggleProviers.hide();
+        $loginProviders.show();
+        $school.val(localStorage.getItem('loginSchool'));
+        $school.trigger('chosen:updated');
+        $school.trigger('change');
+    }
+
 });
 
 window.startIntro = function startIntro() {
-	introJs()
-		.setOptions({
-			nextLabel: 'Weiter',
-			prevLabel: 'Zurück',
-			doneLabel: 'Nächste Seite',
-			skipLabel: 'Überspringen',
-			hidePrev: true, // hide previous button in the first step
-			hideNext: true, // hide next button in the last step
-		})
-		.start()
-		.oncomplete(() => {
-			localStorage.setItem('Tutorial', true);
-			document.querySelector('#demologin').click();
-		});
+    introJs()
+    .setOptions({
+        nextLabel: "Weiter",
+        prevLabel: "Zurück",
+        doneLabel: "Nächste Seite",
+        skipLabel: "Überspringen",
+        hidePrev: true, //hide previous button in the first step
+        hideNext: true  //hide next button in the last step
+    })
+    .start()
+    .oncomplete(function() {
+        localStorage.setItem('Tutorial', true);
+        document.querySelector("#demologin").click();
+    });
 };
