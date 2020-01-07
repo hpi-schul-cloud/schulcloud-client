6db1aaf5875b248ee0c111ce9af26e7d99786d2a
Alexander Kremer <alex@kremer.io>
2020-01-07 09:54:52 +0100

    bootstrap.min.js 	- 4.0.0-alpha.5 to 4.3.1 ckeditor.js		- 4.6.2 to 4.13.1 jquery.min.js		- 3.1.1 to 3.4.1 npm audit fix replace hbs dependency with fork

--- static/vendor/ckeditor/config.js
+++ static/vendor/ckeditor/config.js
@@ -1,40 +1,34 @@
 /**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function (config) {
+CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
-		{ name: 'clipboard', groups: ['undo', 'clipboard'] },
-		{ name: 'forms' },
-		{ name: 'insert' },
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
 		{ name: 'links' },
-		{ name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
-		{ name: 'others' },
-		{ name: 'about' },
-		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'insert' },
+		{ name: 'forms' },
 		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
 		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
-		{ name: 'colors', groups: ['colors'] },
-		{ name: 'basicstyles', groups: ['basicstyles'] },
-		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
-		{ name: 'colors', groups: ['colors'] },
-		{ name: 'basicstyles', groups: ['cleanup'] },
+		{ name: 'colors' },
+		{ name: 'about' }
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Subscript,Superscript,Source,PasteFromWord,Maximize';
-
-	// add color picker
-	config.extraPlugins = 'mathjax,colorbutton,colordialog,print';
-	config.mathJaxLib = '/vendor-optimized/mathjax/MathJax.js?config=TeX-AMS_HTML';
+	config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
