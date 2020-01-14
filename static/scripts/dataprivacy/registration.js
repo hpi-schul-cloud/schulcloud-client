606dd10b5e6e88c68be86c57479e5a65bf75620f
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-14 15:08:33 +0100

    run lint

--- static/scripts/dataprivacy/registration.js
+++ static/scripts/dataprivacy/registration.js
@@ -1,28 +1,28 @@
-import "./registration-link-validation.js";
+import './registration-link-validation';
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
 	// show steppers depending on age of student
-	const radiou16 = document.getElementById("reg-u16");
-	const radio16 = document.getElementById("reg-16");
+	const radiou16 = document.getElementById('reg-u16');
+	const radio16 = document.getElementById('reg-16');
 
-	if (document.querySelector("#showRegistrationForm")) {
+	if (document.querySelector('#showRegistrationForm')) {
 		document
-			.querySelector("#showRegistrationForm")
-			.addEventListener("click", () => {
-				const baseUrl = "/registration";
+			.querySelector('#showRegistrationForm')
+			.addEventListener('click', () => {
+				const baseUrl = '/registration';
 
-				const classOrSchoolId = $("input[name=classOrSchoolId]").val();
-				let additional = "";
-				additional +=
-					$("input[name=sso]").val() === "true"
-						? `sso/${$("input[name=account]").val()}`
-						: "";
-				additional +=
-					$("input[name=importHash]").val() !== undefined
+				const classOrSchoolId = $('input[name=classOrSchoolId]').val();
+				let additional = '';
+				additional
+					+= $('input[name=sso]').val() === 'true'
+						? `sso/${$('input[name=account]').val()}`
+						: '';
+				additional
+					+= $('input[name=importHash]').val() !== undefined
 						? `?importHash=${encodeURIComponent(
-								$("input[name=importHash]").val()
-						  )}`
-						: "";
+							$('input[name=importHash]').val(),
+						)}`
+						: '';
 
 				if (radiou16.checked) {
 					window.location.href = `${baseUrl}/${classOrSchoolId}/byparent/${additional}`;
@@ -30,18 +30,18 @@ window.addEventListener("DOMContentLoaded", () => {
 					window.location.href = `${baseUrl}/${classOrSchoolId}/bystudent/${additional}`;
 				}
 			});
-		$("input[type='radio']").on("change", () => {
+		$("input[type='radio']").on('change', () => {
 			if (radio16.checked) {
-				document.getElementById("infotext-16").style.display = "block";
-				document.getElementById("infotext-u16").style.display = "none";
+				document.getElementById('infotext-16').style.display = 'block';
+				document.getElementById('infotext-u16').style.display = 'none';
 				document.getElementById(
-					"showRegistrationForm"
+					'showRegistrationForm',
 				).disabled = false;
 			} else {
-				document.getElementById("infotext-16").style.display = "none";
-				document.getElementById("infotext-u16").style.display = "block";
+				document.getElementById('infotext-16').style.display = 'none';
+				document.getElementById('infotext-u16').style.display = 'block';
 				document.getElementById(
-					"showRegistrationForm"
+					'showRegistrationForm',
 				).disabled = false;
 			}
 		});
--- static/scripts/dataprivacy/user-registration.js
+++ static/scripts/dataprivacy/user-registration.js
@@ -1,26 +1,26 @@
-import "./dataprivacy";
-import "./registration-link-validation.js";
+import './dataprivacy';
+import './registration-link-validation';
 
 function validateDifferent() {
 	const parentMailInput = document.querySelector(
-		'input[name="parent-email"]'
+		'input[name="parent-email"]',
 	);
 	const studentMailInput = document.querySelector(
-		'input[name="student-email"]'
+		'input[name="student-email"]',
 	);
 	if (
-		parentMailInput.value &&
-		studentMailInput.value &&
-		parentMailInput.value === studentMailInput.value
+		parentMailInput.value
+		&& studentMailInput.value
+		&& parentMailInput.value === studentMailInput.value
 	) {
 		parentMailInput.setCustomValidity(
-			"Für den Schüler muss eine andere Mailadresse als für die Eltern angegeben werden."
+			'Für den Schüler muss eine andere Mailadresse als für die Eltern angegeben werden.',
 		);
 		$(parentMailInput)
-			.closest("section")
-			.addClass("show-invalid");
+			.closest('section')
+			.addClass('show-invalid');
 	} else {
-		parentMailInput.setCustomValidity("");
+		parentMailInput.setCustomValidity('');
 	}
 }
 function goBack(event) {
@@ -29,85 +29,86 @@ function goBack(event) {
 	window.history.back();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
 	const parentMailInput = document.querySelector(
-		'input[name="parent-email"]'
+		'input[name="parent-email"]',
 	);
 	const studentMailInput = document.querySelector(
-		'input[name="student-email"]'
+		'input[name="student-email"]',
 	);
 	if (parentMailInput && studentMailInput) {
-		"change input keyup paste".split(" ").forEach(event => {
+		'change input keyup paste'.split(' ').forEach((event) => {
 			parentMailInput.addEventListener(event, validateDifferent, false);
 			studentMailInput.addEventListener(event, validateDifferent, false);
 		});
 	}
 
 	const firstSection = document.querySelector(
-		'.form section[data-panel="section-1"]:not(.noback)'
+		'.form section[data-panel="section-1"]:not(.noback)',
 	);
 	if (firstSection) {
-		firstSection.addEventListener("showSection", () => {
-			const backButton = document.getElementById("prevSection");
-			backButton.addEventListener("click", goBack);
-			backButton.removeAttribute("disabled");
+		firstSection.addEventListener('showSection', () => {
+			const backButton = document.getElementById('prevSection');
+			backButton.addEventListener('click', goBack);
+			backButton.removeAttribute('disabled');
 		});
-		firstSection.addEventListener("hideSection", () => {
-			const backButton = document.getElementById("prevSection");
-			backButton.removeEventListener("click", goBack);
+		firstSection.addEventListener('hideSection', () => {
+			const backButton = document.getElementById('prevSection');
+			backButton.removeEventListener('click', goBack);
 		});
 	}
 
-	$("input[readonly]").click(() => {
+	$('input[readonly]').click(() => {
 		/* eslint-disable-next-line max-len */
 		$.showNotification(
-			"Diese Daten hat deine Lehrkraft oder dein Administrator für dich eingetragen. Falls Anpassungen notwendig sind wende dich bitte an ihn/sie. Du kannst deine Daten auch nach abgeschlossenem Registrierungsprozess selbst ändern.",
-			"danger",
-			false
+			`Diese Daten hat deine Lehrkraft oder dein Administrator für dich eingetragen.
+			Falls Anpassungen notwendig sind wende dich bitte an ihn/sie.
+			Du kannst deine Daten auch nach abgeschlossenem Registrierungsprozess selbst ändern.`,
+			'danger',
+			false,
 		);
 	});
 });
 
 // GENERATE START PASSWORD
-window.addEventListener("load", () => {
-	if (document.querySelector(".form .student-password")) {
+window.addEventListener('load', () => {
+	if (document.querySelector('.form .student-password')) {
 		// generate password if password field present
 		const words = [
-			"auto",
-			"baum",
-			"bein",
-			"blumen",
-			"flocke",
-			"frosch",
-			"halsband",
-			"hand",
-			"haus",
-			"herr",
-			"horn",
-			"kind",
-			"kleid",
-			"kobra",
-			"komet",
-			"konzert",
-			"kopf",
-			"kugel",
-			"puppe",
-			"rauch",
-			"raupe",
-			"schuh",
-			"seele",
-			"spatz",
-			"taktisch",
-			"traum",
-			"trommel",
-			"wolke"
+			'auto',
+			'baum',
+			'bein',
+			'blumen',
+			'flocke',
+			'frosch',
+			'halsband',
+			'hand',
+			'haus',
+			'herr',
+			'horn',
+			'kind',
+			'kleid',
+			'kobra',
+			'komet',
+			'konzert',
+			'kopf',
+			'kugel',
+			'puppe',
+			'rauch',
+			'raupe',
+			'schuh',
+			'seele',
+			'spatz',
+			'taktisch',
+			'traum',
+			'trommel',
+			'wolke',
 		];
-		const pw =
-			words[Math.floor(Math.random() * words.length)] +
-			Math.floor(Math.random() * 98 + 1).toString();
-		$(".form .student-password").text(pw);
-		$(".form .student-password-input")
+		const pw =			words[Math.floor(Math.random() * words.length)]
+			+ Math.floor(Math.random() * 98 + 1).toString();
+		$('.form .student-password').text(pw);
+		$('.form .student-password-input')
 			.val(pw)
-			.trigger("input");
+			.trigger('input');
 	}
 });
