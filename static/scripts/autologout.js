e061a153a3d55ea6c4b90407789b09236875a1ad
BlackGhostHD <36112357+BlackGhostHD@users.noreply.github.com>
2020-01-17 15:33:52 +0100

    removed cli-server proxying

--- static/scripts/autologout.js
+++ static/scripts/autologout.js
@@ -1,6 +1,11 @@
 $(document).ready(() => {
-	const timeOnScriptLoad = Date.now(); // timestamp on script load
+	const BACKEND_URL = document.querySelector('script[data-backendurl]').dataset.backendurl;
+	const getJwt = (() => {
+		const rawJwt = document.cookie.split(';').filter(item => item.includes('jwt'));
+		return rawJwt[0].replace('jwt=', '');
+	});
 
+	let timeOnStart = Date.now(); // timestamp on script load
 	const $autoLoggoutAlertModal = $('.auto-logout-alert-modal');
 	const showModalOnRemainingSeconds = $autoLoggoutAlertModal.find('.form-group').data('showOnRemainingSec') || 3600;
 	const rstDefault = $autoLoggoutAlertModal.find('.form-group').data('rstDefault') || showModalOnRemainingSeconds * 2;
@@ -27,7 +32,7 @@ $(document).ready(() => {
 	const decRst = (() => {
 		setTimeout(() => {
 			if (rst >= 60) {
-				rst = (rstDefault - Math.floor((Date.now() - timeOnScriptLoad) / 1000));
+				rst = Math.max(0, rstDefault - Math.floor((Date.now() - timeOnStart) / 1000));
 				$('.js-time').text(Math.floor(rst / 60));
 				// show auto loggout alert modal
 				// don't show modal while processing
@@ -36,38 +41,57 @@ $(document).ready(() => {
 				}
 				decRst();
 			}
-		}, 1000 * 60);
+		}, 1000 * 20);
 	});
 
 	let retry = 0;
 	let totalRetry = 0;
 	// extend session
 	const IStillLoveYou = (async () => {
-		$.post('/account/ttl', { resetTimer: true }, () => {
-			// on success
-			processing = false;
-			totalRetry = 0;
-			retry = 0;
-			rst = rstDefault;
-			$.showNotification('Sitzung erfolgreich verlängert.', 'success', true);
-		}).fail(() => {
-			// retry 4 times before showing error
-			if (retry < 4) {
-				retry += 1;
-				setTimeout(() => {
-					IStillLoveYou();
-				}, (2 ** retry) * 1000);
-			} else {
+		$.ajax({
+			url: `${BACKEND_URL}accounts/jwtTimer`,
+			type: 'POST',
+			dataType: 'json',
+			// Fetch the stored token from localStorage and set in the header
+			headers: { Authorization: `Bearer ${getJwt()}` },
+		})
+			.done(() => {
+				processing = false;
+				totalRetry = 0;
 				retry = 0;
-				if (totalRetry === maxTotalRetrys) {
-					/* eslint-disable-next-line max-len */
-					$.showNotification('Deine Sitzung konnte nicht verlängert werden! Bitte speichere deine Arbeit und lade die Seite neu.', 'danger', false);
-				} else {
-					showAutoLogoutModal('error');
+				$.showNotification('Sitzung erfolgreich verlängert.', 'success', true);
+				timeOnStart = Date.now();
+				// check if decRst needs to be restarted
+				if (rst < 60) {
+					decRst();
 				}
-				totalRetry += 1;
-			}
-		});
+				rst = rstDefault;
+			})
+			.fail((err) => {
+				if (err && err.status !== 401) {
+					// retry 4 times before showing error
+					if (retry < 4) {
+						retry += 1;
+						setTimeout(() => {
+							IStillLoveYou();
+						}, (2 ** retry) * 1000);
+					} else {
+						retry = 0;
+						if (totalRetry === maxTotalRetrys) {
+							/* eslint-disable-next-line max-len */
+							$.showNotification('Deine Sitzung konnte nicht verlängert werden! Bitte speichere deine Arbeit und lade die Seite neu.', 'danger', false);
+						} else {
+							showAutoLogoutModal('error');
+						}
+						totalRetry += 1;
+					}
+				} else {
+					// Session was expired due to inactivity - autologout
+					window.location.href = '/login';
+					/* eslint-disable-next-line max-len */
+					$.showNotification('Deine Sitzung ist bereits abgelaufen. Bitte melde dich erneut an.', 'danger', false);
+				}
+			});
 	});
 
 	decRst(); // dec. rst
