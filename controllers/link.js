2c9d1573048e0880cedd3dfbb87d332a45a3d4fb
dasGoogle <aaronschlitt@gmx.de>
2020-01-19 13:01:15 +0100

    simplify link creation

--- controllers/link.js
+++ controllers/link.js
@@ -6,19 +6,12 @@ const api = require('../api');
 const { HOST } = process.env;
 
 router.post('/', async (req, res, next) => {
-	// check first if target already exists (preventing db to be wasted)
+	// create link without duplicate prevention, moved to server
 	const target = `${(req.headers.origin || HOST)}/${req.body.target}`;
 	try {
-		const result = await api(req).get('/link/', { qs: { target } });
-		const existingLink = result.data[0];
-		if (!existingLink) {
-			const data = await api(req).post('/link/', { json: { target } })
-			data.newUrl = `${(req.headers.origin || HOST)}/link/${data._id}`;
-			res.json(data);
-		} else {
-			existingLink.newUrl = `${(req.headers.origin || HOST)}/link/${existingLink._id}`;
-			res.json(existingLink);
-		}
+		const data = await api(req).post('/link/', { json: { target } });
+		data.newUrl = `${(req.headers.origin || HOST)}/link/${data._id}`;
+		res.json(data);
 	} catch (error) {
 		next(error);
 	}
