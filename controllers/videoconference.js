535be28a078759ba1692f3977a69b4e62c690d9a
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-01-27 17:10:55 +0100

    update response handling

--- controllers/videoconference.js
+++ controllers/videoconference.js
@@ -6,25 +6,21 @@ const api = require('../api');
 
 router.get('/:scopeName/:scopeId', (req, res, next) => {
 	const { scopeName, scopeId } = req.params;
-
 	return authHelper.isAuthenticated(req).then(() => api(req)
 		.get(`/videoconference/${scopeName}/${scopeId}?demo=wait`))
-		.then((response)=>{
-			console.log(response); 
-			return response;
-		})
+		.then(response => res.send(response))
 		.catch(err => next(err));
 });
 
 router.post('/', (req, res, next) => {
 	const { scopeName, scopeId, options } = req.params;
-
 	return authHelper.isAuthenticated(req).then(() => api(req)
 		.post('/videoconference', {
 			body: {
 				scopeName, scopeId, options,
 			},
 		}))
+		.then(response => res.send(response))
 		.catch(err => next(err));
 });
 
