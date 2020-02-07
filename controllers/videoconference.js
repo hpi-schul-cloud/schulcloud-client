cd17c0e814f3319a206dd6ea648018b0658dd914
j-jmnz <jmnz.jesus@gmail.com>
2020-02-07 11:23:38 +0100

    remove demo=wait from controller

--- controllers/videoconference.js
+++ controllers/videoconference.js
@@ -9,7 +9,7 @@ const { HOST } = process.env;
 router.get('/:scopeName/:scopeId', (req, res, next) => {
 	const { scopeName, scopeId } = req.params;
 	return authHelper.isAuthenticated(req).then(() => api(req)
-		.get(`/videoconference/${scopeName}/${scopeId}?demo=wait`))
+		.get(`/videoconference/${scopeName}/${scopeId}`))
 		.then(response => res.send(response))
 		.catch(err => next(err));
 });
