ade89d6d29b0eeff62ea6662c88cb44f2c9f29a0
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-02-21 18:11:37 +0100

    linter before hook und travis gleichzeitig glücklich machen

--- controllers/topics.js
+++ controllers/topics.js
@@ -352,7 +352,8 @@ router.get('/add/neweditor', async (req, res, next) => {
 	res.redirect(`/courses/${req.params.courseId}/topics/${lesson._id}?edtr=true`);
 });
 
-const boolean = v => v === 1 || v === 'true' || v === true;
+// eslint-disable-next-line
+const boolean = (v) => v === 1 || v === 'true' || v === true || v === '1';
 router.patch('/:topicId/neweditor', (req, res, next) => {
 	if (req.body.hidden !== undefined) {
 		apiEditor(req).patch(`/helpers/setVisibility/${req.params.topicId}`, {
