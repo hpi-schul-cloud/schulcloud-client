c3ffcc37b164a83adb29e353b17b7f5211217ba6
BlackGhostHD <36112357+BlackGhostHD@users.noreply.github.com>
2020-03-19 17:08:19 +0100

    code cleanup

--- controllers/index.js
+++ controllers/index.js
@@ -52,7 +52,6 @@ router.use('/videoconference', require('./videoconference'));
 router.use('/administration/', require('./administration'));
 
 router.use('/version', require('./version'));
-router.use('/alerts', require('./alerts'));
 
 
 module.exports = router;
