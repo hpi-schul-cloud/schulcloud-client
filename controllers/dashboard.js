b1efacf0432e233e404c1700da1377056aad2e4f
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-03-24 09:37:14 +0100

    replace logger

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,7 +3,7 @@
  */
 
 const express = require('express');
-const logger = require('winston');
+const logger = require('../helpers/logger');
 
 const router = express.Router();
 const moment = require('moment');
