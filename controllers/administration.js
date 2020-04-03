22c9f463aea1fe186ee1a123de3815733cb2fa29
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-04-03 19:10:53 +0200

    Use systems API instead of populate #skip-test-stage

--- controllers/administration.js
+++ controllers/administration.js
@@ -3288,27 +3288,15 @@ router.get(
 	'/systems/ldap/edit/:id',
 	permissionsHelper.permissionsChecker('ADMIN_VIEW'),
 	async (req, res, next) => {
-		// Find LDAP-System
-		const school = await Promise.resolve(
-			api(req).get(`/schools/${res.locals.currentSchool}`, {
-				qs: {
-					$populate: ['systems'],
-				},
-			}),
-		);
-		const system = school.systems.filter(
-			// eslint-disable-next-line no-shadow
-			system => system._id === req.params.id,
-		);
-
-		if (system.length === 1) {
+		try {
+			const system = await Promise.resolve(
+				api(req).get(`/systems/${req.params.id}`),
+			);
 			res.render('administration/ldap-edit', {
 				title: 'LDAP bearbeiten',
-				system: system[0],
+				system: system,
 			});
-		} else {
-			const err = new Error('Not Found');
-			err.status = 404;
+		} catch (err) {
 			next(err);
 		}
 	},
@@ -3318,17 +3306,8 @@ router.post(
 	'/systems/ldap/edit/:id',
 	permissionsHelper.permissionsChecker('ADMIN_VIEW'),
 	async (req, res, next) => {
-		// Find LDAP-System
-		const school = await Promise.resolve(
-			api(req).get(`/schools/${res.locals.currentSchool}`, {
-				qs: {
-					$populate: ['systems'],
-				},
-			}),
-		);
-		const system = school.systems.filter(
-			// eslint-disable-next-line no-shadow
-			system => system._id === req.params.id,
+		const system = await Promise.resolve(
+			api(req).get(`/systems/${req.params.id}`),
 		);
 
 		// Classes acitve
@@ -3347,7 +3326,7 @@ router.post(
 		}
 
 		api(req)
-			.patch(`/systems/${system[0]._id}`, {
+			.patch(`/systems/${system._id}`, {
 				json: {
 					alias: req.body.ldapalias,
 					ldapConfig: {
@@ -3388,7 +3367,7 @@ router.post(
 			})
 			.then(() => {
 				api(req)
-					.get(`/ldap/${system[0]._id}`)
+					.get(`/ldap/${system._id}`)
 					.then((data) => {
 						res.json(data);
 					});
