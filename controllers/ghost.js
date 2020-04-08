d2fb17d4507b51b97c6cabff4d6136f695c87e08
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-04-08 16:53:07 +0200

    lint

--- controllers/ghost.js
+++ controllers/ghost.js
@@ -6,22 +6,23 @@ const router = express.Router();
 
 router.get('/:slug', async (req, res, next) => {
 
-	if(!Configuration.has('GHOST_API_KEY') || !Configuration.has('GHOST_API_URL')){
+	if (!Configuration.has('GHOST_API_KEY') || !Configuration.has('GHOST_API_URL')) {
 		return next(new Error('GHOST_API_URL or/and GHOST_API_KEY are not defined'));
 	}
-		const options = {
-			uri: `${Configuration.get('GHOST_API_URL')}/content/pages/slug/${req.params.slug}/`,
-			qs: {
-				key: Configuration.get('GHOST_API_KEY'),
-				fields: 'html,title',
-			},
-			json: true,
-			timeout: Configuration.get('REQUEST_TIMEOUT_MS'),
-		};
 
-		return request(options)
-				.then((page) => res.json(page))
-				.catch(next);
+	const options = {
+		uri: `${Configuration.get('GHOST_API_URL')}/content/pages/slug/${req.params.slug}/`,
+		qs: {
+			key: Configuration.get('GHOST_API_KEY'),
+			fields: 'html,title',
+		},
+		json: true,
+		timeout: Configuration.get('REQUEST_TIMEOUT_MS'),
+	};
+
+	return request(options)
+		.then((page) => res.json(page))
+		.catch(next);
 
 });
 
--- theme/n21/static/scripts/n21-blog.js
+++ theme/n21/static/scripts/n21-blog.js
@@ -1,7 +1,6 @@
 const stripHtml = require('string-strip-html');
 
 $(document).ready(() => {
-
 	$.ajax({
 		url: '/ghost/startseite-nbc',
 		type: 'GET',
@@ -9,12 +8,12 @@ $(document).ready(() => {
 		contentType: 'application/json',
 		timeout: 8000,
 	})
-	.done((result) => {
-		$('.n21-blog .title').text(result.pages[0].title);
-		$('.n21-blog .content').html(stripHtml(result.pages[0].html, { onlyStripTags: ['script', 'style'] }));
-	})
-	.fail(() => {
-		/* eslint-disable-next-line */
-		console.error('Could not load frontpage content from blog');
-	});
+		.done((result) => {
+			$('.n21-blog .title').text(result.pages[0].title);
+			$('.n21-blog .content').html(stripHtml(result.pages[0].html, { onlyStripTags: ['script', 'style'] }));
+		})
+		.fail(() => {
+			/* eslint-disable-next-line */
+			console.error('Could not load frontpage content from blog');
+		});
 });
