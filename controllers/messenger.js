16f38ebc0b3c2c0b76aadd62bab1ce8bb7476440
Max Klenk <max@klenk.biz>
2020-04-08 12:34:15 +0200

    make use of catch(next)

--- controllers/messenger.js
+++ controllers/messenger.js
@@ -9,12 +9,6 @@ router.use(authHelper.authChecker);
 router.get('/token', (req, res, next) => api(req)
 	.post('/messengerToken', {})
 	.then((result) => res.send(result))
-	.catch((e) => {
-		// eslint-disable-next-line no-console
-		console.error('Failed to get messengerToken', e.error);
-		const error = new Error('Messenger Token nicht verfügbar.');
-		error.status = 404;
-		return next(error);
-	}));
+	.catch(next));
 
 module.exports = router;
