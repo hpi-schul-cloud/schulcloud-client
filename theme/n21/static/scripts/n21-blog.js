43f8532fe3eb4eeac965a4ee4605f612007b1d56
Christian <blackskill90@gmail.com>
2020-04-08 11:53:32 +0200

    n21 frontpage now loading content from n21 blog

---
+++ theme/n21/static/scripts/n21-blog.js
@@ -0,0 +1,36 @@
+const initialized = false;
+$(document).ready(() => {
+	if (initialized) {
+		return;
+	}
+
+	function sanitize(input) {
+		const output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '')
+			.replace(/<style[^>]*?>.*?<\/style>/gi, '')
+			.replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '')
+			.replace(/&nbsp;/g, '');
+		return output;
+	}
+
+	const contentKey = document.querySelector('script[data-contentkey]').dataset.contentkey;
+	console.log(contentKey);
+
+	$.ajax({
+		url: 'https://blog.niedersachsen.cloud/ghost/api/v2/content/pages/slug/startseite-nbc/',
+		data: {
+			key: contentKey,
+			fields: 'html,title',
+		},
+		type: 'GET',
+		dataType: 'json',
+		contentType: 'application/json',
+		timeout: 8000,
+	})
+		.done((result) => {
+			$('.n21-blog .title').text(result.pages[0].title);
+			$('.n21-blog .context').html(sanitize(result.pages[0].html));
+		})
+		.fail(() => {
+			console.error('Could not load front page content from blog');
+		});
+});
