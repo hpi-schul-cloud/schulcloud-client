d4a9fe5fc5f065de02c64c075de995c1c4f3f304
Artur <bialoskorski.artur@gmail.com>
2020-05-12 11:26:38 +0200

    added map with links from ghost

--- static/scripts/sc-blog.js
+++ static/scripts/sc-blog.js
@@ -1,28 +1,38 @@
 const stripHtml = require('string-strip-html');
+const { Configuration } = require('@schul-cloud/commons');
+
 
 function fetchContent() {
 	$('.sc-blog .spinner').show();
 	$('.sc-blog .placeholder').hide();
 
-	$.ajax({
-		url: '/ghost/sc-startseite',
-		type: 'GET',
-		dataType: 'json',
-		contentType: 'application/json',
-		timeout: 8000,
-	})
-		.done((result) => {
-			$('.sc-blog .loading').remove();
-			$('.sc-blog .title').text(result.pages[0].title);
-			result.pages[0].html = result.pages[0].html
-				.replace(/<td>x<[/]td>/g, '<td><i class="fa fa-check"></i></td>');
-			$('.sc-blog .content').html(stripHtml(result.pages[0].html, { onlyStripTags: ['script', 'style'] }));
-			$('.sc-blog .content').css('opacity', '1');
-		})
-		.fail(() => {
-			$('.sc-blog .spinner').hide();
-			$('.sc-blog .placeholder').show();
+	if (Configuration.has('GHOST_PAGES_URLS')) {
+		const Array2 = Configuration.get('GHOST_API_URL');
+
+		Array2.map((url) => {
+			$.ajax({
+				url,
+				type: 'GET',
+				dataType: 'json',
+				contentType: 'application/json',
+				timeout: 8000,
+			})
+				.done((result) => {
+					console.log(result);
+					$('.sc-blog .loading').remove();
+					$('.sc-blog .title').text(result.pages[0].title);
+					result.pages[0].html = result.pages[0].html
+						.replace(/<td>x<[/]td>/g, '<td><i class="fa fa-check"></i></td>');
+					$('.sc-blog .content').html(stripHtml(result.pages[0].html,
+						{ onlyStripTags: ['script', 'style'] }));
+					$('.sc-blog .content').css('opacity', '1');
+				})
+				.fail(() => {
+					$('.sc-blog .spinner').hide();
+					$('.sc-blog .placeholder').show();
+				});
 		});
+	}
 }
 
 $(document).ready(() => {
