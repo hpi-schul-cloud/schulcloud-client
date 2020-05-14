9d3a5929b131597ce41a4b04bf2a0201975e91b1
Artur <bialoskorski.artur@gmail.com>
2020-05-14 14:50:07 +0200

    changed about page to be dependable from ghost

--- static/scripts/about-ghost.js
+++ static/scripts/about-ghost.js
@@ -1,50 +1,31 @@
 const stripHtml = require('string-strip-html');
 
 function fetchContent() {
-	let finalHtml = '';
+	let finalHtml;
 	$('.ghost .spinner').show();
 	$('.ghost .placeholder').hide();
 
-	const Array = [
-		'/ghost/herzlich-willkommen/',
-		'/ghost/explanatory-video/',
-		'/ghost/what-matters/',
-	];
-
-	const changePage = () => {
-		finalHtml = finalHtml
-			.replace(/<td>x<[/]td>/g, '<td><i class="fa fa-check"></i></td>');
-		$('.ghost .content').html(stripHtml(finalHtml,
-			{ onlyStripTags: ['script', 'style'] }));
-		$('.ghost .content').css('opacity', '1');
-	};
-
-	const promiseFunc = new Promise((resolve, reject) => {
-		Array.forEach((url, index) => {
-			$.ajax({
-				url,
-				type: 'GET',
-				dataType: 'json',
-				contentType: 'application/json',
-				timeout: 8000,
-				async: false,
-			})
-				.then((result) => {
-					finalHtml += result.pages[0].html;
-					$('.ghost .loading').remove();
-					if (index === Array.length - 1) {
-						resolve();
-					}
-				})
-				.fail(() => {
-					$('.ghost .spinner').hide();
-					$('.ghost .placeholder').show();
-				});
+	$.ajax({
+		url: '/ghost/landing-page-about/',
+		type: 'GET',
+		dataType: 'json',
+		contentType: 'application/json',
+		timeout: 8000,
+		async: false,
+	})
+		.then((result) => {
+			$('.ghost .loading').remove();
+			finalHtml = result.pages[0].html;
+			finalHtml = finalHtml
+				.replace(/<td>x<[/]td>/g, '<td><i class="fa fa-check"></i></td>');
+			$('.ghost .content').html(stripHtml(finalHtml,
+				{ onlyStripTags: ['script', 'style'] }));
+			$('.ghost .content').css('opacity', '1');
+		})
+		.fail(() => {
+			$('.ghost .spinner').hide();
+			$('.ghost .placeholder').show();
 		});
-	});
-	promiseFunc.then(() => {
-		changePage();
-	});
 }
 
 $(document).ready(() => {
--- static/scripts/sc-blog.js
+++ static/scripts/sc-blog.js
@@ -1,17 +1,15 @@
 const stripHtml = require('string-strip-html');
 
+let production;
+let linksArray;
+
 function fetchContent() {
 	let finalHtml = '';
 	$('.sc-blog .spinner').show();
 	$('.sc-blog .placeholder').hide();
 
-	const Array = [
-		'/ghost/herzlich-willkommen/',
-		'/ghost/explanatory-video/',
-		'/ghost/what-matters/',
-	];
-
 	const changePage = () => {
+		$('.sc-blog .loading').remove();
 		finalHtml = finalHtml
 			.replace(/<td>x<[/]td>/g, '<td><i class="fa fa-check"></i></td>');
 		$('.sc-blog .content').html(stripHtml(finalHtml,
@@ -19,35 +17,58 @@ function fetchContent() {
 		$('.sc-blog .content').css('opacity', '1');
 	};
 
-	const promiseFunc = new Promise((resolve, reject) => {
-		Array.forEach((url, index) => {
-			$.ajax({
-				url,
-				type: 'GET',
-				dataType: 'json',
-				contentType: 'application/json',
-				timeout: 8000,
-				async: false,
+	const promiseFunc = new Promise((resolve) => {
+		$.ajax({
+			url: '/ghost/landing-page/',
+			type: 'GET',
+			dataType: 'json',
+			contentType: 'application/json',
+			timeout: 8000,
+		})
+			.then((result) => {
+				const regexHash = /\#{1}[a-zA-Z--]+/g;
+				const regexAsterisk = /\~{1}[a-zA-Z--]+/g;
+				if (production) {
+					linksArray = result.pages[0].html.match(regexHash);
+				} else {
+					linksArray = result.pages[0].html.match(regexAsterisk);
+				}
+				resolve();
 			})
-				.then((result) => {
-					finalHtml += result.pages[0].html;
-					$('.sc-blog .loading').remove();
-					if (index === Array.length - 1) {
-						resolve();
-					}
+			.fail((error) => {
+				console.log(error);
+			});
+	});
+
+	promiseFunc
+		.then(() => {
+			linksArray.forEach((url, index) => {
+				url = url.substr(1);
+				$.ajax({
+					url: `/ghost/${url}/`,
+					type: 'GET',
+					dataType: 'json',
+					contentType: 'application/json',
+					timeout: 8000,
+					async: false,
 				})
-				.fail(() => {
-					$('.sc-blog .spinner').hide();
-					$('.sc-blog .placeholder').show();
-				});
+					.then((result) => {
+						finalHtml += result.pages[0].html;
+						if (index === linksArray.length - 1) {
+							changePage();
+						}
+					})
+					.fail(() => {
+						$('.sc-blog .spinner').hide();
+						$('.sc-blog .placeholder').show();
+					});
+			});
 		});
-	});
-	promiseFunc.then(() => {
-		changePage();
-	});
 }
 
+
 $(document).ready(() => {
+	({ production } = window);
 	fetchContent();
 	$('.sc-blog .placeholder button').on('click', fetchContent);
 });
