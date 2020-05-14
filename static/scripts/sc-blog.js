5dbcdc6caf82135bc7cc92bf3966a62a28607037
Artur <bialoskorski.artur@gmail.com>
2020-05-14 14:04:46 +0200

    added two regex due to production, fixed downloading content

--- static/scripts/sc-blog.js
+++ static/scripts/sc-blog.js
@@ -1,17 +1,12 @@
 const stripHtml = require('string-strip-html');
 
 let production;
+let linksArray;
 
 function fetchContent() {
 	let finalHtml = '';
 	$('.sc-blog .spinner').show();
 	$('.sc-blog .placeholder').hide();
-
-	// const Array = [
-	// 	'/ghost/herzlich-willkommen/',
-	// 	'/ghost/explanatory-video/',
-	// 	'/ghost/what-matters/',
-	// ];
 
 	const changePage = () => {
 		$('.sc-blog .loading').remove();
@@ -23,50 +18,54 @@ function fetchContent() {
 	};
 
 	const promiseFunc = new Promise((resolve) => {
-		linksArray.forEach((url, index) => {
-			$.ajax({
-				url: `/ghost/${element}/`,
-				type: 'GET',
-				dataType: 'json',
-				contentType: 'application/json',
-				timeout: 8000,
-				async: false,
+		$.ajax({
+			url: '/ghost/landing-page/',
+			type: 'GET',
+			dataType: 'json',
+			contentType: 'application/json',
+			timeout: 8000,
+		})
+			.then((result) => {
+				const regexHash = /\#{1}[a-zA-Z--]+/g;
+				const regexAsterisk = /\~{1}[a-zA-Z--]+/g;
+				if (production) {
+					linksArray = result.pages[0].html.match(regexHash);
+				} else {
+					linksArray = result.pages[0].html.match(regexAsterisk);
+				}
+				resolve();
 			})
-				.then((result) => {
-					finalHtml += result.pages[0].html;
-					if (index === Array.length - 1) {
-						resolve();
-					}
-				})
-				.fail(() => {
-					$('.sc-blog .spinner').hide();
-					$('.sc-blog .placeholder').show();
-				});
-		});
-	});
-	promiseFunc.then(() => {
-		changePage();
+			.fail((error) => {
+				console.log(error);
+			});
 	});
 
-	$.ajax({
-		url: '/ghost/landing-page/',
-		type: 'GET',
-		dataType: 'json',
-		contentType: 'application/json',
-		timeout: 8000,
-	})
-		.then((result) => {
-			const regex = /\#{1}[a-zA-Z--]+/g;
-			const linksArray = result.pages[0].html.match(regex);
-			linksArray.forEach((element) => {
-				element = element.substr(1);
+	promiseFunc
+		.then(() => {
+			linksArray.forEach((url, index) => {
+				url = url.substr(1);
+				$.ajax({
+					url: `/ghost/${url}/`,
+					type: 'GET',
+					dataType: 'json',
+					contentType: 'application/json',
+					timeout: 8000,
+					async: false,
+				})
+					.then((result) => {
+						finalHtml += result.pages[0].html;
+						if (index === linksArray.length - 1) {
+							changePage();
+						}
+					})
+					.fail(() => {
+						$('.sc-blog .spinner').hide();
+						$('.sc-blog .placeholder').show();
+					});
 			});
-			promiseFunc();
-		})
-		.fail((error) => {
-			console.log(error);
 		});
 }
+
 
 $(document).ready(() => {
 	({ production } = window);
