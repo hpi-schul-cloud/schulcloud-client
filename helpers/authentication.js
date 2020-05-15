c6a81ee8d775e5bf43b8082e0818f27b23157b16
Gratza <rg@ciphron.de>
2020-05-15 17:54:11 +0200

    actually use encoded pad id

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -229,7 +229,7 @@ const login = (payload = {}, req, res, next) => {
 const etherpadCookieHelper = (etherpadSession, padId, res) => {
 	const encodedPadId = encodeURI(padId);
 	res.cookie('sessionID', etherpadSession.data.sessionID, {
-		path: `${Configuration.get('ETHERPAD_BASE_PATH')}/p/${padId}`,
+		path: `${Configuration.get('ETHERPAD_BASE_PATH')}/p/${encodedPadId}`,
 		expires: new Date(etherpadSession.data.validUntil * 1000),
 		httpOnly: Configuration.get('COOKIE__HTTP_ONLY'),
 		hostOnly: Configuration.get('COOKIE__HOST_ONLY'),
