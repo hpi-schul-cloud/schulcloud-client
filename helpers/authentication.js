4027bc04381d0b00ba60e8331b48a08135de7f2f
Gratza <rg@ciphron.de>
2020-05-19 14:26:41 +0200

    remove debugging code

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -227,7 +227,7 @@ const login = (payload = {}, req, res, next) => {
 const etherpadCookieHelper = (etherpadSession, padId, res) => {
 	const encodedPadId = encodeURI(padId);
 	res.cookie('sessionID', etherpadSession.data.sessionID, {
-		path: `/p/${encodedPadId}`, //`${Configuration.get('ETHERPAD_BASE_PATH')}/p/${encodedPadId}`,
+		path: `${Configuration.get('ETHERPAD_BASE_PATH')}/p/${encodedPadId}`,
 		expires: new Date(etherpadSession.data.validUntil * 1000),
 		httpOnly: Configuration.get('COOKIE__HTTP_ONLY'),
 		hostOnly: Configuration.get('COOKIE__HOST_ONLY'),
