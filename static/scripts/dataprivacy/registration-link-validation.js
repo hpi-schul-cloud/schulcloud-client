eb8ad4cc2ca922b88103cc83a60f02ceb7cf4112
Artur <bialoskorski.artur@gmail.com>
2020-05-28 15:21:04 +0200

    added styling classes to error page when link is not correct in registration

--- static/scripts/dataprivacy/registration-link-validation.js
+++ static/scripts/dataprivacy/registration-link-validation.js
@@ -3,9 +3,14 @@ window.addEventListener('DOMContentLoaded', () => {
 	// invalid registration detexted
 	*/
 	const message = `
-	<h4>Registrierung fehlerhaft</h4>
-	<p>Diese Registrierung bzw. dieser Registrierungslink scheint nicht valide zu sein. Dies kann mehrere Gründe haben:
-		<ul>
+<main class="container pt-3 mt-5 text-center>
+	<div class="error-header-wrapper">
+	<img class="error-header-image w-100 h-100" src="/images/broken-pen.svg" alt="zerbrochener Stift" />
+	<h1 class="h1 error-header-code text-center">Registrierung fehlerhaft</h1>
+	<p class="error-header-text text-center">
+	Diese Registrierung bzw. dieser Registrierungslink scheint nicht valide zu sein. Dies kann mehrere Gründe haben:
+	</p>
+	<ul>
 			<li>Der Nutzer, dem dieser Registrierungslink gehört, hat sich bzw. wurde bereits registriert.</li>
 			<li>
 				Ein Lehrer oder Administrator hat für den entsprechenden Nutzer
@@ -13,10 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
 			</li>
 			<li>Der Registrierungslink ist sehr alt und nicht mehr korrekt mit dem entsprechenden Nutzer verknüpft.</li>
 		</ul>
-	</p>
 	<p>Bitte kontaktiere einen verantwortlichen Lehrer oder Administrator und frage diesbezüglich nach.
-	Dieser kann weiterhelfen oder auch einen neuen Registrierungslink erstellen, falls nötig.</p>
-	`;
+		Dieser kann weiterhelfen oder auch einen neuen Registrierungslink erstellen, falls nötig.</p>
+`;
 
 	if ($("input[name='invalid']").val() === 'true') {
 		$('#welcome-screen')
