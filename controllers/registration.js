98b6ec1f75cc7eea05d19d50ab94985ad8059d5c
Artur <bialoskorski.artur@gmail.com>
2020-06-01 14:57:56 +0200

    added request to server for check if the id is correct

--- controllers/registration.js
+++ controllers/registration.js
@@ -4,7 +4,11 @@ const { Configuration } = require('@schul-cloud/commons');
 const router = express.Router();
 const api = require('../api');
 
-const { HOST, NODE_ENV, CONSENT_WITHOUT_PARENTS_MIN_AGE_YEARS } = require('../config/global');
+const {
+	HOST,
+	NODE_ENV,
+	CONSENT_WITHOUT_PARENTS_MIN_AGE_YEARS,
+} = require('../config/global');
 const setTheme = require('../helpers/theme');
 const authHelper = require('../helpers/authentication');
 
@@ -12,15 +16,19 @@ let invalid = false;
 const isProduction = NODE_ENV === 'production';
 
 const resetThemeForPrivacyDocuments = async (req, res) => {
-	res.locals.currentSchoolData = await api(req).get(`registrationSchool/${req.params.classOrSchoolId}`);
+	res.locals.currentSchoolData = await api(req).get(
+		`registrationSchool/${req.params.classOrSchoolId}`,
+	);
 	setTheme(res);
 };
 
-const isSecure = (url) => (url.includes('sso') || url.includes('importHash') ? true : false);
+const isSecure = (url) => !!(url.includes('sso') || url.includes('importHash'));
 
 const checkValidRegistration = async (req) => {
 	if (req.query.importHash) {
-		const existingUser = await api(req).get(`/users/linkImport/${req.query.importHash}`);
+		const existingUser = await api(req).get(
+			`/users/linkImport/${req.query.importHash}`,
+		);
 		if (!existingUser.userId) {
 			return true;
 		}
@@ -38,109 +46,123 @@ router.get(['/register', '/register/*'], (req, res, next) => res.render('registr
  */
 router.post('/registration/pincreation', (req, res, next) => {
 	if (req.body && req.body.email) {
-		return api(req).post('/registrationPins/', {
-			json: {
-				email: req.body.email,
-				mailTextForRole: req.body.mailTextForRole,
-			},
-		}).then((result) => {
-			res.sendStatus((result || {}).status || 200);
-		}).catch(next);
+		return api(req)
+			.post('/registrationPins/', {
+				json: {
+					email: req.body.email,
+					mailTextForRole: req.body.mailTextForRole,
+				},
+			})
+			.then((result) => {
+				res.sendStatus((result || {}).status || 200);
+			})
+			.catch(next);
 	}
 	return res.sendStatus(400);
 });
 
-router.post(['/registration/submit', '/registration/submit/:sso/:accountId'], (req, res, next) => {
-	// normalize form data
-	req.body.roles = Array.isArray(req.body.roles) ? req.body.roles : [req.body.roles];
+router.post(['/registration/submit', '/registration/submit/:sso/:accountId'],
+	(req, res, next) => {
+		// normalize form data
+		req.body.roles = Array.isArray(req.body.roles)
+			? req.body.roles
+			: [req.body.roles];
 
-	let skipConsent = false;
-	if (req.body.roles.length > 0) {
-		skipConsent = req.body.roles.some((role) => {
-			let roleName = role.name;
-			if (roleName === 'teacher' || roleName === 'administrator') {
-				roleName = 'employee';
-			}
-			return Configuration.get('SKIP_CONDITIONS_CONSENT').includes(roleName);
-		});
-	}
+		let skipConsent = false;
+		if (req.body.roles.length > 0) {
+			skipConsent = req.body.roles.some((role) => {
+				let roleName = role.name;
+				if (roleName === 'teacher' || roleName === 'administrator') {
+					roleName = 'employee';
+				}
+				return Configuration.get('SKIP_CONDITIONS_CONSENT').includes(
+					roleName,
+				);
+			});
+		}
 
-	return api(req)
-		.post('/registration/', {
-			json: req.body,
-		})
-		.then((response) => {
-			const eMailAdresses = [response.user.email];
-			if (response.parent) {
-				eMailAdresses.push(response.parent.email);
-			}
-			const consentText = skipConsent ? ''
-				: `Wenn du zwischen 14 und ${CONSENT_WITHOUT_PARENTS_MIN_AGE_YEARS} Jahre alt bist,
+		return api(req)
+			.post('/registration/', {
+				json: req.body,
+			})
+			.then((response) => {
+				const eMailAdresses = [response.user.email];
+				if (response.parent) {
+					eMailAdresses.push(response.parent.email);
+				}
+				const consentText = skipConsent
+					? ''
+					: `Wenn du zwischen 14 und ${CONSENT_WITHOUT_PARENTS_MIN_AGE_YEARS} Jahre alt bist,
 bestätige bitte zusätzlich die Einverständniserklärung,
 damit du die ${res.locals.theme.short_title} nutzen kannst.`;
 
-			eMailAdresses.forEach((eMailAdress) => {
-				let passwordText = '';
-				let studentInfotext = '';
-				if (req.body.roles.includes('student')) {
-					passwordText = `Startpasswort: ${req.body.password_1}`;
-					studentInfotext = `Für Schüler: Nach dem ersten Login musst du ein persönliches Passwort festlegen.
+				eMailAdresses.forEach((eMailAdress) => {
+					let passwordText = '';
+					let studentInfotext = '';
+					if (req.body.roles.includes('student')) {
+						passwordText = `Startpasswort: ${req.body.password_1}`;
+						studentInfotext = `Für Schüler: Nach dem ersten Login musst
+						 du ein persönliches Passwort festlegen.
 ${consentText}`;
-				}
-				return api(req).post('/mails/', {
-					json: {
-						email: eMailAdress,
-						subject: `Willkommen in der ${res.locals.theme.title}!`,
-						headers: {},
-						content: {
-							text: `Hallo ${response.user.firstName}
-mit folgenden Anmeldedaten kannst du dich in der ${res.locals.theme.title} einloggen:
+					}
+					return api(req).post('/mails/', {
+						json: {
+							email: eMailAdress,
+							subject: `Willkommen in der ${res.locals.theme.title}!`,
+							headers: {},
+							content: {
+								text: `Hallo ${response.user.firstName}
+mit folgenden Anmeldedaten kannst du dich in der ${
+	res.locals.theme.title
+} einloggen:
 Adresse: ${req.headers.origin || HOST}
 E-Mail: ${response.user.email}
 ${passwordText}
 ${studentInfotext}
 Viel Spaß und einen guten Start wünscht dir dein
 ${res.locals.theme.short_title}-Team`,
+							},
 						},
-					},
+					});
 				});
-			});
-		})
-		.then(() => {
-			if (req.params.sso) {
-				res.cookie(
-					'jwt',
-					req.cookies.jwt,
-					{
+			})
+			.then(() => {
+				if (req.params.sso) {
+					res.cookie('jwt', req.cookies.jwt, {
 						expires: new Date(Date.now() - 100000),
 						httpOnly: false,
 						hostOnly: true,
 						secure: isProduction,
-					},
-				);
-			}
-		})
-		.then(() => {
-			res.sendStatus(200);
-		})
-		.catch((err) => {
-			let message = 'Hoppla, ein unbekannter Fehler ist aufgetreten. Bitte versuche es erneut.';
-			const customMessage = (err.error || {}).message || err.message;
-			if (customMessage) { message = customMessage; }
-			if (err && err.code) {
-				if (err.code === 'ESOCKETTIMEDOUT') {
-					message = `Leider konnte deine Registrierung nicht abgeschlossen werden.
-					Bitte versuche es erneut.`;
+					});
 				}
-			}
-			return res.status(500).send(message);
-		});
-});
+			})
+			.then(() => {
+				res.sendStatus(200);
+			})
+			.catch((err) => {
+				let message = 'Hoppla, ein unbekannter Fehler ist aufgetreten. Bitte versuche es erneut.';
+				const customMessage = (err.error || {}).message || err.message;
+				if (customMessage) {
+					message = customMessage;
+				}
+				if (err && err.code) {
+					if (err.code === 'ESOCKETTIMEDOUT') {
+						message = `Leider konnte deine Registrierung nicht abgeschlossen werden.
+					Bitte versuche es erneut.`;
+					}
+				}
+				return res.status(500).send(message);
+			});
+	},
+);
 
 router.get(['/registration/:classOrSchoolId/byparent', '/registration/:classOrSchoolId/byparent/:sso/:accountId'],
 	async (req, res, next) => {
 		if (!RegExp('^[0-9a-fA-F]{24}$').test(req.params.classOrSchoolId)) {
-			if (req.params.sso && !RegExp('^[0-9a-fA-F]{24}$').test(req.params.accountId)) {
+			if (
+				req.params.sso
+				&& !RegExp('^[0-9a-fA-F]{24}$').test(req.params.accountId)
+			) {
 				return res.sendStatus(400);
 			}
 		}
@@ -157,11 +179,15 @@ router.get(['/registration/:classOrSchoolId/byparent', '/registration/:classOrSc
 		await resetThemeForPrivacyDocuments(req, res);
 
 		if (user.importHash) {
-			const existingUser = await api(req).get(`/users/linkImport/${user.importHash}`);
+			const existingUser = await api(req).get(
+				`/users/linkImport/${user.importHash}`,
+			);
 			Object.assign(user, existingUser);
 		}
 
-		const needConsent = !Configuration.get('SKIP_CONDITIONS_CONSENT').includes('student');
+		const needConsent = !Configuration.get(
+			'SKIP_CONDITIONS_CONSENT',
+		).includes('student');
 		const sectionNumber = needConsent ? 5 : 3;
 
 		return res.render('registration/registration-parent', {
@@ -175,12 +201,16 @@ router.get(['/registration/:classOrSchoolId/byparent', '/registration/:classOrSc
 			invalid,
 			secure,
 		});
-	});
+	},
+);
 
 router.get(['/registration/:classOrSchoolId/bystudent', '/registration/:classOrSchoolId/bystudent/:sso/:accountId'],
 	async (req, res, next) => {
 		if (!RegExp('^[0-9a-fA-F]{24}$').test(req.params.classOrSchoolId)) {
-			if (req.params.sso && !RegExp('^[0-9a-fA-F]{24}$').test(req.params.accountId)) {
+			if (
+				req.params.sso
+				&& !RegExp('^[0-9a-fA-F]{24}$').test(req.params.accountId)
+			) {
 				return res.sendStatus(400);
 			}
 		}
@@ -197,11 +227,15 @@ router.get(['/registration/:classOrSchoolId/bystudent', '/registration/:classOrS
 		await resetThemeForPrivacyDocuments(req, res);
 
 		if (user.importHash) {
-			const existingUser = await api(req).get(`/users/linkImport/${user.importHash}`);
+			const existingUser = await api(req).get(
+				`/users/linkImport/${user.importHash}`,
+			);
 			Object.assign(user, existingUser);
 		}
 
-		const needConsent = !Configuration.get('SKIP_CONDITIONS_CONSENT').includes('student');
+		const needConsent = !Configuration.get(
+			'SKIP_CONDITIONS_CONSENT',
+		).includes('student');
 		const sectionNumber = needConsent ? 4 : 3;
 
 		return res.render('registration/registration-student', {
@@ -215,64 +249,85 @@ router.get(['/registration/:classOrSchoolId/bystudent', '/registration/:classOrS
 			invalid,
 			secure,
 		});
-	});
+	},
+);
 
-router.get(['/registration/:classOrSchoolId/:byRole'], async (req, res, next) => {
-	if (!RegExp('^[0-9a-fA-F]{24}$').test(req.params.classOrSchoolId)) {
-		if (req.params.sso && !RegExp('^[0-9a-fA-F]{24}$').test(req.params.accountId)) {
-			return res.sendStatus(400);
-		}
-	}
-	const secure = isSecure(req.url);
-
-	const user = {};
-	user.importHash = req.query.importHash || req.query.id; // req.query.id is deprecated
-	user.classOrSchoolId = req.params.classOrSchoolId;
-
-	invalid = await checkValidRegistration(req);
-
-	await resetThemeForPrivacyDocuments(req, res);
-
-	if (user.importHash) {
-		const existingUser = await api(req).get(`/users/linkImport/${user.importHash}`);
-		Object.assign(user, existingUser);
-	}
-
-	let needConsent = true;
-	let sectionNumber = 5;
-
-	let roleText;
-	if (req.params.byRole === 'byemployee') {
-		roleText = 'Lehrer*/Admins*';
-		if (Configuration.get('SKIP_CONDITIONS_CONSENT').includes('employee')) {
-			needConsent = false;
-			sectionNumber = 4;
-		}
-	} else {
-		delete user.firstName;
-		delete user.lastName;
-		roleText = 'Experte*';
-	}
-
-	return res.render('registration/registration-employee', {
-		title: `Registrierung - ${roleText}`,
-		hideMenu: true,
-		user,
-		needConsent,
-		sectionNumber,
-		invalid,
-		secure,
-	});
-});
-
-router.get(
-	['/registration/:classOrSchoolId', '/registration/:classOrSchoolId/:sso/:accountId'],
+router.get(['/registration/:classOrSchoolId/:byRole'],
 	async (req, res, next) => {
 		if (!RegExp('^[0-9a-fA-F]{24}$').test(req.params.classOrSchoolId)) {
-			if (req.params.sso && !RegExp('^[0-9a-fA-F]{24}$').test(req.params.accountId)) {
+			if (
+				req.params.sso
+				&& !RegExp('^[0-9a-fA-F]{24}$').test(req.params.accountId)
+			) {
 				return res.sendStatus(400);
 			}
 		}
+		const secure = isSecure(req.url);
+
+		const user = {};
+		user.importHash = req.query.importHash || req.query.id; // req.query.id is deprecated
+		user.classOrSchoolId = req.params.classOrSchoolId;
+
+		invalid = await checkValidRegistration(req);
+
+		await resetThemeForPrivacyDocuments(req, res);
+
+		if (user.importHash) {
+			const existingUser = await api(req).get(
+				`/users/linkImport/${user.importHash}`,
+			);
+			Object.assign(user, existingUser);
+		}
+
+		let needConsent = true;
+		let sectionNumber = 5;
+
+		let roleText;
+		if (req.params.byRole === 'byemployee') {
+			roleText = 'Lehrer*/Admins*';
+			if (
+				Configuration.get('SKIP_CONDITIONS_CONSENT').includes(
+					'employee',
+				)
+			) {
+				needConsent = false;
+				sectionNumber = 4;
+			}
+		} else {
+			delete user.firstName;
+			delete user.lastName;
+			roleText = 'Experte*';
+		}
+
+		return res.render('registration/registration-employee', {
+			title: `Registrierung - ${roleText}`,
+			hideMenu: true,
+			user,
+			needConsent,
+			sectionNumber,
+			invalid,
+			secure,
+		});
+	},
+);
+
+router.get(
+	['/registration/:classOrSchoolId', '/registration/:classOrSchoolId/:sso/:accountId'],
+	async (req, res) => {
+		const validID = () => {
+			router.get(
+				'/registrationlink',
+				async (reqLink, resLink) => {
+					try {
+						if (resLink.schoolId === req.params.classOrSchoolId) {
+							return false;
+						}
+					} catch (error) {
+						return true;
+					}
+				},
+			);
+		};
 		const secure = isSecure(req.url);
 
 		invalid = await checkValidRegistration(req);
@@ -289,6 +344,7 @@ router.get(
 			CONSENT_WITHOUT_PARENTS_MIN_AGE_YEARS,
 			invalid,
 			secure,
+			validID,
 		});
 	},
 );
