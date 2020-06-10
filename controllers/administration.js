846acc66656032190b3ac64c8715497810542085
mwolowyk-cap <64189574+mwolowyk-cap@users.noreply.github.com>
2020-06-10 10:36:26 +0200

    removed unnecessary REST request api in client

--- controllers/administration.js
+++ controllers/administration.js
@@ -3214,18 +3214,6 @@ router.use(
 	},
 );
 
-router.get('/policies/:id', async (req, res, next) => {
-	try {
-		const base64File = await Promise.resolve(
-			api(req).get(`/base64Files/${req.params.id}`),
-		);
-		const fileData = base64File.data;
-		res.json(fileData);
-	} catch (err) {
-		next(err);
-	}
-});
-
 /*
 
 	Change School Year
