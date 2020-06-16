92d5e95929901203849360c1298d13c7c3c39e7e
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-06-16 08:49:42 +0200

    test

--- test/mocha/content/content.test.js
+++ test/mocha/content/content.test.js
@@ -1,51 +1,56 @@
-'use strict';
 
 const assert = require('assert');
 const app = require('../../../app');
 const chai = require('chai');
-const expect = chai.expect;
+
+const { expect } = chai;
 const chaiHttp = require('chai-http');
+const { logger } = require('handlebars');
+const { logError } = require('gulp-sass');
 const loginHelper = require('../helper/login-helper');
+
 chai.use(chaiHttp);
 
-describe('Content tests', function () {
-    before(function (done) {
-        this.server = app.listen(3031);
-        this.server.once('listening', () => {
-            loginHelper.login(app).then(res => {
-                this.agent = res.agent;
-                done();
-            });
-        });
-    });
+describe('Content tests', () => {
+	before(function (done) {
+		this.server = app.listen(3031);
+		this.server.once('listening', () => {
+			loginHelper.login(app).then((res) => {
+				this.agent = res.agent;
+				done();
+			});
+		});
+	});
 
-    after(function (done) {
-        this.server.close(done);
-    });
+	after(function (done) {
+		this.server.close(done);
+	});
 
-    it('GET /content', function () {
-        return new Promise((resolve, reject) => {
-            this.agent
-                .get('/content/')
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.text).to.contain('Lern-Store');
-                    resolve();
-                });
-        });
-    });
-
-    it('GET /content/?q=Mathe', function () {
-        return new Promise((resolve, reject) => {
-            this.agent
-                .get('/content/?q=Mathe')
-                .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
+	it.only('GET /content', function () {
+		return new Promise((resolve, reject) => {
+			this.agent
+				.get('/content/')
+				.end((err, res) => {
+					if (err) logger.err(err);
+					logger.log(res);
+					expect(res.statusCode).to.equal(200);
 					expect(res.text).to.contain('Lern-Store');
-                    expect(res.text).to.contain('Suchergebnisse für "Mathe"');
-                    expect(res.text).not.to.contain('keine Ergebnisse');
-                    resolve();
-                });
-        });
-    });
+					resolve();
+				});
+		});
+	});
+
+	it('GET /content/?q=Mathe', function () {
+		return new Promise((resolve, reject) => {
+			this.agent
+				.get('/content/?q=Mathe')
+				.end((err, res) => {
+					expect(res.statusCode).to.equal(200);
+					expect(res.text).to.contain('Lern-Store');
+					expect(res.text).to.contain('Suchergebnisse für "Mathe"');
+					expect(res.text).not.to.contain('keine Ergebnisse');
+					resolve();
+				});
+		});
+	});
 });
