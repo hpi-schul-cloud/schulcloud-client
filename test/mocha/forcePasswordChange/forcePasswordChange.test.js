26c43d5978026459b6e4f1c00553607a7ab645c8
Robert Strzepka <robert.strzepka@partner.bmwgroup.com>
2020-06-24 20:11:23 +0200

    Fix codacy issues

--- test/mocha/forcePasswordChange/forcePasswordChange.test.js
+++ test/mocha/forcePasswordChange/forcePasswordChange.test.js
@@ -1,37 +1,38 @@
-
 const chai = require('chai');
-
-const { expect } = chai;
 const chaiHttp = require('chai-http');
 const app = require('../../../app');
 const loginHelper = require('../helper/login-helper');
 
+const { expect } = chai;
+
 chai.use(chaiHttp);
 
-describe('forcePasswordChange tests', function test() {
-	before(function (done) {
+describe('forcePasswordChange tests', () => {
+	before((done) => {
 		this.server = app.listen(3031);
 		this.server.once('listening', () => {
-			loginHelper.login(app).then(res => {
+			loginHelper.login(app).then((res) => {
 				this.agent = res.agent;
 				done();
 			});
 		});
 	});
 
-	after(function (done) {
+	after((done) => {
 		this.server.close(done);
 	});
 
-	it('GET /forcePasswordChange', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/forcePasswordChange')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.redirects[0].endsWith('/dashboard')).to.equal(true);
-					resolve();
-				});
-		});
-	});
+	it('GET /forcePasswordChange', () => new Promise((resolve) => {
+		this.agent
+			.get('/forcePasswordChange')
+			.end((err, res) => {
+				expect(res.statusCode)
+					.to
+					.equal(200);
+				expect(res.redirects[0].endsWith('/dashboard'))
+					.to
+					.equal(true);
+				resolve();
+			});
+	}));
 });
