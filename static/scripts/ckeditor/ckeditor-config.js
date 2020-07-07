f34dde76b009b1004fdd4dba2b51f09b1320fe41
Alexander Riese <alexander.riese@capgemini.com>
2020-07-07 15:52:08 +0200

    SC-4673 resort CKEditor config

--- static/scripts/ckeditor/ckeditor-config.js
+++ static/scripts/ckeditor/ckeditor-config.js
@@ -2,20 +2,24 @@
 import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+import ExportPDFPlugin from '@ckeditor/ckeditor5-export-pdf/src/exportpdf';
+import ExportWordPlugin from '@ckeditor/ckeditor5-export-word/src/exportword';
 import FontPlugin from '@ckeditor/ckeditor5-font/src/font';
 import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
+import HighlightPlugin from '@ckeditor/ckeditor5-highlight/src/highlight';
 import HorizontalLinePlugin from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import ImageCaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
+import ImageResizePlugin from '@ckeditor/ckeditor5-image/src/imageresize';
 import ImageStylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageResizePlugin from '@ckeditor/ckeditor5-image/src/imageresize';
 import IndentPlugin from '@ckeditor/ckeditor5-indent/src/indent';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
 import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbedPlugin from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOfficePlugin from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import RemoveFormatPlugin from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 import SpecialCharacterEssentialPlugin from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
 import SpecialCharacterPlugin from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
@@ -26,13 +30,8 @@ import TableCellPropertiesPlugin from '@ckeditor/ckeditor5-table/src/tablecellpr
 import TablePlugin from '@ckeditor/ckeditor5-table/src/table';
 import TablePropertiesPlugin from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbarPlugin from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import TodoListPlugin from '@ckeditor/ckeditor5-list/src/todolist';
-import PasteFromOfficePlugin from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import HighlightPlugin from '@ckeditor/ckeditor5-highlight/src/highlight';
-import ExportPDFPlugin from '@ckeditor/ckeditor5-export-pdf/src/exportpdf';
-import ExportWordPlugin from '@ckeditor/ckeditor5-export-word/src/exportword';
-import MathTypePlugin from '@wiris/mathtype-ckeditor5/src/plugin';
+import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
 
 import FileBrowserPlugin from './plugins/ckeditor5-filebrowser/src/filebrowser';
 
@@ -40,46 +39,45 @@ import FileBrowserPlugin from './plugins/ckeditor5-filebrowser/src/filebrowser';
 const ckeditorConfig = {
 	language: 'de',
 	plugins: [
+		// MathPlugin,
 		BlockQuotePlugin,
 		BoldPlugin,
 		EssentialsPlugin,
+		ExportPDFPlugin,
+		ExportWordPlugin,
+		FileBrowserPlugin,
 		FontPlugin,
 		HeadingPlugin,
+		HighlightPlugin,
 		HorizontalLinePlugin,
 		ImageCaptionPlugin,
 		ImagePlugin,
+		ImageResizePlugin,
 		ImageStylePlugin,
 		ImageToolbarPlugin,
-		ImageResizePlugin,
 		IndentPlugin,
 		ItalicPlugin,
 		LinkPlugin,
 		ListPlugin,
-		// MathPlugin,
 		MediaEmbedPlugin,
 		ParagraphPlugin,
+		PasteFromOfficePlugin,
 		RemoveFormatPlugin,
 		SpecialCharacterEssentialPlugin,
 		SpecialCharacterPlugin,
 		StrikethroughPlugin,
 		SubscriptPlugin,
 		SuperscriptPlugin,
+		TableCellPropertiesPlugin,
 		TablePlugin,
 		TablePropertiesPlugin,
-		TableCellPropertiesPlugin,
-		UnderlinePlugin,
-		FileBrowserPlugin,
 		TableToolbarPlugin,
 		TodoListPlugin,
-		PasteFromOfficePlugin,
-		HighlightPlugin,
-		ExportPDFPlugin,
-		ExportWordPlugin,
-		MathTypePlugin,
+		UnderlinePlugin,
 	],
 	toolbar: [
 		'undo', 'redo', '|',
-		'mediaEmbed', 'MathType', 'ChemType', 'filebrowser', 'insertTable', 'horizontalLine', 'specialCharacters', 'link', '|',
+		'mediaEmbed', 'filebrowser', 'insertTable', 'horizontalLine', 'specialCharacters', 'link', '|',
 		'heading', '|',
 		'numberedList', 'bulletedList', 'todoList', '|',
 		'outdent', 'indent', '|',
