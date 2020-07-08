50976f6b00f262b7186f05d3dcf69a3b65163130
Alexander Riese <alexander.riese@capgemini.com>
2020-07-08 16:27:10 +0200

    SC-4673 adjust CKEditor plugins

--- static/scripts/ckeditor/ckeditor-config.js
+++ static/scripts/ckeditor/ckeditor-config.js
@@ -1,18 +1,13 @@
 // import MathPlugin from 'ckeditor5-math/src/math';
+import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
-import ExportPDFPlugin from '@ckeditor/ckeditor5-export-pdf/src/exportpdf';
-import ExportWordPlugin from '@ckeditor/ckeditor5-export-word/src/exportword';
 import FontPlugin from '@ckeditor/ckeditor5-font/src/font';
 import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
-import HighlightPlugin from '@ckeditor/ckeditor5-highlight/src/highlight';
 import HorizontalLinePlugin from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
-import ImageCaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
 import ImageResizePlugin from '@ckeditor/ckeditor5-image/src/imageresize';
-import ImageStylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import IndentPlugin from '@ckeditor/ckeditor5-indent/src/indent';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
@@ -30,8 +25,8 @@ import TableCellPropertiesPlugin from '@ckeditor/ckeditor5-table/src/tablecellpr
 import TablePlugin from '@ckeditor/ckeditor5-table/src/table';
 import TablePropertiesPlugin from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbarPlugin from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import TodoListPlugin from '@ckeditor/ckeditor5-list/src/todolist';
 import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import CodePlugin from '@ckeditor/ckeditor5-basic-styles/src/code';
 
 import FileBrowserPlugin from './plugins/ckeditor5-filebrowser/src/filebrowser';
 
@@ -40,21 +35,17 @@ const ckeditorConfig = {
 	language: 'de',
 	plugins: [
 		// MathPlugin,
+		AutoformatPlugin,
 		BlockQuotePlugin,
 		BoldPlugin,
+		CodePlugin,
 		EssentialsPlugin,
-		ExportPDFPlugin,
-		ExportWordPlugin,
 		FileBrowserPlugin,
 		FontPlugin,
 		HeadingPlugin,
-		HighlightPlugin,
 		HorizontalLinePlugin,
-		ImageCaptionPlugin,
 		ImagePlugin,
 		ImageResizePlugin,
-		ImageStylePlugin,
-		ImageToolbarPlugin,
 		IndentPlugin,
 		ItalicPlugin,
 		LinkPlugin,
@@ -72,26 +63,19 @@ const ckeditorConfig = {
 		TablePlugin,
 		TablePropertiesPlugin,
 		TableToolbarPlugin,
-		TodoListPlugin,
 		UnderlinePlugin,
 	],
 	toolbar: [
 		'undo', 'redo', '|',
 		'mediaEmbed', 'filebrowser', 'insertTable', 'horizontalLine', 'specialCharacters', 'link', '|',
 		'heading', '|',
-		'numberedList', 'bulletedList', 'todoList', '|',
-		'outdent', 'indent', '|',
-		'blockQuote', 'fontFamily', 'fontSize', 'fontColor', 'fontBackgroundColor', 'highlight',
-		'bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript', '|',
+		'numberedList', 'bulletedList', '|',
+		'blockQuote', 'fontColor', 'fontBackgroundColor',
+		'bold', 'italic', 'underline', 'strikethrough', 'code', 'subscript', 'superscript', '|',
 		'removeFormat', '|',
-		'exportPdf', 'exportWord',
 	],
 	table: {
 		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties'],
-	},
-	image: {
-		toolbar: ['imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight', '|', 'imageTextAlternative'],
-		styles: ['full', 'alignLeft', 'alignRight'],
 	},
 	math: {
 		engine: 'mathjax',
