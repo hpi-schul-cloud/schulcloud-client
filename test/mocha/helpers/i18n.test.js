9ab9ba856b4b196c6825afd6ece9cbdff93cb9ad
Daniel Rieckhof <Daniel.Rieckhof@hpi.uni-potsdam.de>
2020-09-04 14:56:15 +0200

    Improved tests

--- test/mocha/helpers/i18n.test.js
+++ test/mocha/helpers/i18n.test.js
@@ -3,7 +3,7 @@ const { Configuration } = require('@schul-cloud/commons');
 
 const config = {
     defaultLanguage: 'de',
-    fallbackLanguage: 'en',
+    fallbackLanguage: 'de',
     availableLanguages: 'de,en,fr'
 }
 
@@ -13,44 +13,39 @@ const mockReq = {
     }
 };
 
-describe('i18n helpers test', () => {
-    Configuration.set('I18N__DEFAULT_LANGUAGE', config.defaultLanguage);
-    Configuration.set('I18N__FALLBACK_LANGUAGE', config.fallbackLanguage);
-    Configuration.set('I18N__AVAILABLE_LANGUAGES', config.availableLanguages);
+Configuration.set('I18N__DEFAULT_LANGUAGE', config.defaultLanguage);
+Configuration.set('I18N__FALLBACK_LANGUAGE', config.fallbackLanguage);
+Configuration.set('I18N__AVAILABLE_LANGUAGES', config.availableLanguages);
+const i18n = require('../../../helpers/i18n');
 
+describe('i18n helpers test', () => {
     it('Check for defaultLanguage', () => {
-        const { defaultLanguage } = require('../../../helpers/i18n');
-		chai.expect(defaultLanguage).to.equal(config.defaultLanguage, `The default language should be ${config.defaultLanguage}`);
+		chai.expect(i18n.defaultLanguage).to.equal(config.defaultLanguage, `The default language should be ${config.defaultLanguage}`);
     });
 
     it('Check for fallbackLanguage', () => {
-        const { fallbackLanguage } = require('../../../helpers/i18n');
-		chai.expect(fallbackLanguage).to.equal(config.fallbackLanguage, `The fallback language should be ${config.fallbackLanguage}`);
+		chai.expect(i18n.fallbackLanguage).to.equal(config.fallbackLanguage, `The fallback language should be ${config.fallbackLanguage}`);
     });
 
     it('Check for availableLanguages', () => {
         const testAvailableLanguages = config.availableLanguages.split(',')
-        const { availableLanguages } = require('../../../helpers/i18n');
-		chai.expect(availableLanguages).to.eql(testAvailableLanguages, `The available languages should be ${JSON.stringify(testAvailableLanguages)}`);
+		chai.expect(i18n.availableLanguages).to.eql(testAvailableLanguages, `The available languages should be ${JSON.stringify(testAvailableLanguages)}`);
     });
 
     it('Check changeLanguage(en)', async () => {
-        const { i18next, changeLanguage } = require('../../../helpers/i18n');
-        await changeLanguage('en');
-		chai.expect(i18next.language).to.equal('en', 'The language should be set to "en"');
+        await i18n.changeLanguage('en');
+		chai.expect(i18n.i18next.language).to.equal('en', 'The language should be set to "en"');
     });
 
     it('Check changeLanguage(he)', async () => {
-        const { i18next, changeLanguage } = require('../../../helpers/i18n');
-        await changeLanguage('de');
-        await changeLanguage('he');
-		chai.expect(i18next.language).to.equal('de', 'The language should be still "de" because "he" is unknown');
+        await i18n.changeLanguage('de');
+        await i18n.changeLanguage('he');
+		chai.expect(i18n.i18next.language).to.equal('de', 'The language should be still "de" because "he" is unknown');
     });
 
     it('Check getBrowserLanguage()', () => {
-        const { getBrowserLanguage } = require('../../../helpers/i18n');
         const testBrowserLanguage = 'en';
-        const browserLanguage = getBrowserLanguage(mockReq);
+        const browserLanguage = i18n.getBrowserLanguage(mockReq);
 		chai.expect(browserLanguage).to.eql(testBrowserLanguage, `The available languages should be ${JSON.stringify(testBrowserLanguage)}`);
     });
 });
