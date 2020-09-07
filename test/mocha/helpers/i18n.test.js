c3fd8223de0adb510e7b7f266a79c55ba1fe0997
Daniel Rieckhof <Daniel.Rieckhof@hpi.uni-potsdam.de>
2020-09-07 09:13:23 +0200

    Fixed linting issues

--- test/mocha/helpers/i18n.test.js
+++ test/mocha/helpers/i18n.test.js
@@ -20,32 +20,44 @@ const i18n = require('../../../helpers/i18n');
 
 describe('i18n helpers test', () => {
 	it('Check for defaultLanguage', () => {
-		chai.expect(i18n.defaultLanguage).to.equal(config.defaultLanguage, `The default language should be ${config.defaultLanguage}`);
+		chai
+			.expect(i18n.defaultLanguage)
+			.to.equal(config.defaultLanguage, `The default language should be ${config.defaultLanguage}`);
 	});
 
 	it('Check for fallbackLanguage', () => {
-		chai.expect(i18n.fallbackLanguage).to.equal(config.fallbackLanguage, `The fallback language should be ${config.fallbackLanguage}`);
+		chai
+			.expect(i18n.fallbackLanguage)
+			.to.equal(config.fallbackLanguage, `The fallback language should be ${config.fallbackLanguage}`);
 	});
 
 	it('Check for availableLanguages', () => {
 		const testAvailableLanguages = config.availableLanguages.split(',');
-		chai.expect(i18n.availableLanguages).to.eql(testAvailableLanguages, `The available languages should be ${JSON.stringify(testAvailableLanguages)}`);
+		chai
+			.expect(i18n.availableLanguages)
+			.to.eql(testAvailableLanguages, `The available languages should be ${JSON.stringify(testAvailableLanguages)}`);
 	});
 
 	it('Check changeLanguage(en)', async () => {
 		await i18n.changeLanguage('en');
-		chai.expect(i18n.i18next.language).to.equal('en', 'The language should be set to "en"');
+		chai
+			.expect(i18n.i18next.language)
+			.to.equal('en', 'The language should be set to "en"');
 	});
 
 	it('Check changeLanguage(he)', async () => {
 		await i18n.changeLanguage('de');
 		await i18n.changeLanguage('he');
-		chai.expect(i18n.i18next.language).to.equal('de', 'The language should be still "de" because "he" is unknown');
+		chai
+			.expect(i18n.i18next.language)
+			.to.equal('de', 'The language should be still "de" because "he" is unknown');
 	});
 
 	it('Check getBrowserLanguage()', () => {
 		const testBrowserLanguage = 'en';
 		const browserLanguage = i18n.getBrowserLanguage(mockReq);
-		chai.expect(browserLanguage).to.eql(testBrowserLanguage, `The available languages should be ${JSON.stringify(testBrowserLanguage)}`);
+		chai
+			.expect(browserLanguage)
+			.to.eql(testBrowserLanguage, `The available languages should be ${JSON.stringify(testBrowserLanguage)}`);
 	});
 });
