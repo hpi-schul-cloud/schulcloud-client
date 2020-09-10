981ca09da0bd69b0962e0634f1abffbbda5e87ef
Dominik Glandorf <do.gl@gmx.de>
2020-09-10 14:57:34 +0200

    make cookie secure

--- static/scripts/courses.js
+++ static/scripts/courses.js
@@ -374,7 +374,7 @@ $(document).ready(() => {
 		var d = new Date();
 		d.setTime(d.getTime() + (exdays*24*60*60*1000));
 		var expires = "expires="+ d.toUTCString();
-		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax";
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=None;Secure";
 	}
 	setCookie('username', 'Peter', 365);
 });
