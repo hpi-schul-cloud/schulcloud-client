d17f72e26544ac9acbdbe512aa685daa3e28adfd
Daniel Rieckhof <Daniel.Rieckhof@hpi.uni-potsdam.de>
2020-09-22 09:32:16 +0200

    Fixed some linting issues

--- test/mocha/content/content.test.js
+++ test/mocha/content/content.test.js
@@ -1,18 +1,24 @@
-const assert = require('assert');
 const { Configuration } = require('@schul-cloud/commons');
-const app = require('../../../app');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../../app');
 
 const { expect } = chai;
-const chaiHttp = require('chai-http');
 const loginHelper = require('../helper/login-helper');
 
 chai.use(chaiHttp);
 
 const { i18next } = require('../../../helpers/i18n');
 
+const expectedStringContentStore = i18next.t('content.headline.contentStore');
+const expectedStringResultsFound = i18next.t(
+	'content.headline.nSearchResultsFoundWith',
+	{ searchResultsTotal: '', query: 'Mathe' }
+).trim();
+const expectedStringNoResultsFound = i18next.t('content.headline.noResultsFoundWith', { query: 'Mathe' });
+
 describe('Content tests', () => {
-	before(function (done) {
+	before((done) => {
 		this.server = app.listen(3031);
 		this.server.once('listening', () => {
 			loginHelper.login(app).then((res) => {
@@ -22,52 +28,35 @@ describe('Content tests', () => {
 		});
 	});
 
-	after(function (done) {
+	after((done) => {
 		this.server.close(done);
 	});
 
-	it('GET /content', function () {
-		expect(
-			Configuration.get('LERNSTORE_MODE'),
-			'LERNSTORE_MODE not set!',
-		).to.be.not.equal('DISABLED');
-		return new Promise((resolve, reject) => {
-			this.agent.get('/content/').end((err, res) => {
-				expect(res.statusCode).to.equal(200);
-				expect(res.text).to.contain(
-					i18next.t('content.headline.contentStore'),
-				);
-				resolve();
-			});
+	it('GET /content', () => {
+		expect(Configuration.get('LERNSTORE_MODE'), 'LERNSTORE_MODE not set!').to.be.not.equal('DISABLED');
+		return new Promise((resolve) => {
+			this.agent
+				.get('/content/')
+				.end((err, res) => {
+					expect(res.statusCode).to.equal(200);
+					expect(res.text).to.contain(expectedStringContentStore);
+					resolve();
+				});
 		});
 	});
 
-	it('GET /content/?q=Mathe', function () {
-		expect(
-			Configuration.get('LERNSTORE_MODE'),
-			'LERNSTORE_MODE not set!',
-		).to.be.not.equal('DISABLED');
+	it('GET /content/?q=Mathe', () => {
+		expect(Configuration.get('LERNSTORE_MODE'), 'LERNSTORE_MODE not set!').to.be.not.equal('DISABLED');
 		return new Promise((resolve) => {
-			this.agent.get('/content/?q=Mathe').end((err, res) => {
-				expect(res.statusCode).to.equal(200);
-				expect(res.text).to.contain(
-					i18next.t('content.headline.contentStore'),
-				);
-				expect(res.text).to.contain(
-					i18next
-						.t('content.headline.nSearchResultsFoundWith', {
-							searchResultsTotal: '',
-							query: 'Mathe',
-						})
-						.trim(),
-				);
-				expect(res.text).not.to.contain(
-					i18next.t('content.headline.noResultsFoundWith', {
-						query: 'Mathe',
-					}),
-				);
-				resolve();
-			});
+			this.agent
+				.get('/content/?q=Mathe')
+				.end((err, res) => {
+					expect(res.statusCode).to.equal(200);
+					expect(res.text).to.contain(expectedStringContentStore);
+					expect(res.text).to.contain(expectedStringResultsFound);
+					expect(res.text).not.to.contain(expectedStringNoResultsFound);
+					resolve();
+				});
 		});
 	});
 });
--- test/mocha/homework/homework.test.js
+++ test/mocha/homework/homework.test.js
@@ -1,16 +1,15 @@
-const assert = require('assert');
-const app = require('../../../app');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../../app');
 
 const { expect } = chai;
-const chaiHttp = require('chai-http');
 const loginHelper = require('../helper/login-helper');
 
 chai.use(chaiHttp);
 const { i18next } = require('../../../helpers/i18n');
 
 describe('Homework tests', () => {
-	before(function (done) {
+	before((done) => {
 		this.server = app.listen(3031);
 		this.server.once('listening', () => {
 			loginHelper.login(app).then((res) => {
@@ -20,131 +19,111 @@ describe('Homework tests', () => {
 		});
 	});
 
-	after(function (done) {
+	after((done) => {
 		this.server.close(done);
 	});
 
 	// OVERVIEW PAGES
-	it('GET /homework', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain(i18next.t('courses._course.tab_label.tasks'));
-					expect(res.text).to.contain('Mathe');
-					resolve();
-				});
-		});
-	});
-	it('GET /homework/asked', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/asked')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain(i18next.t('global.headline.assignedTasks'));
-					expect(res.text).to.not.contain('btn btn-secondary btn-sm btn-edit');
-					expect(res.text).to.not.contain('btn btn-secondary btn-sm btn-delete');
-					resolve();
-				});
-		});
-	});
-	it('GET /homework/private', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/private')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain(i18next.t('global.link.tasksDrafts'));
-					expect(res.text).to.contain('btn btn-secondary btn-sm btn-edit');
-					expect(res.text).to.contain('btn btn-secondary btn-sm btn-delete');
-					resolve();
-				});
-		});
-	});
-	it('GET /homework/archive', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/archive')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain(i18next.t('homework.headline.archivedTasks'));
-					expect(res.text).to.contain(i18next.t('global.text.noTasks'));
-					resolve();
-				});
-		});
-	});
+	it('GET /homework', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/')
+			.end((err, res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain(i18next.t('courses._course.tab_label.tasks'));
+				expect(res.text).to.contain('Mathe');
+				resolve();
+			});
+	}));
+	it('GET /homework/asked', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/asked')
+			.end((err, res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain(i18next.t('global.headline.assignedTasks'));
+				expect(res.text).to.not.contain('btn btn-secondary btn-sm btn-edit');
+				expect(res.text).to.not.contain('btn btn-secondary btn-sm btn-delete');
+				resolve();
+			});
+	}));
+	it('GET /homework/private', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/private')
+			.end((err, res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain(i18next.t('global.link.tasksDrafts'));
+				expect(res.text).to.contain('btn btn-secondary btn-sm btn-edit');
+				expect(res.text).to.contain('btn btn-secondary btn-sm btn-delete');
+				resolve();
+			});
+	}));
+	it('GET /homework/archive', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/archive')
+			.end((err, res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain(i18next.t('homework.headline.archivedTasks'));
+				expect(res.text).to.contain(i18next.t('global.text.noTasks'));
+				resolve();
+			});
+	}));
 	// DETAIL VIEW
-	it('GET /homework/{users private task}', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/59cce1d381297026d02cdc4b')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain('Private Aufgabe von Marla');
-					resolve();
-				});
-		});
-	});
-	it('GET /homework/{users asked task}', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/59d1f63ce0a06325e8b5288b')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain('Aufgabe an Marla');
-					resolve();
-				});
-		});
-	});
-	it('GET /homework/{teachers private task}', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/59cce4c3c6abf042248e888e')
-				.end((err, res) => {
-					expect(res.statusCode).to.not.equal(200);
-					expect(res.text).to.not.contain('Private Aufgabe von Cord');
-					resolve();
-				});
-		});
-	});
+	it('GET /homework/{users private task}', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/59cce1d381297026d02cdc4b')
+			.end((err, res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain('Private Aufgabe von Marla');
+				resolve();
+			});
+	}));
+	it('GET /homework/{users asked task}', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/59d1f63ce0a06325e8b5288b')
+			.end((err, res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain('Aufgabe an Marla');
+				resolve();
+			});
+	}));
+	it('GET /homework/{teachers private task}', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/59cce4c3c6abf042248e888e')
+			.end((err, res) => {
+				expect(res.statusCode).to.not.equal(200);
+				expect(res.text).to.not.contain('Private Aufgabe von Cord');
+				resolve();
+			});
+	}));
 
 	// CREATE & EDIT TASKS
-	it('GET /homework/new', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/new')
-				.end((err, res) => {
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain(i18next.t('global.button.addTask'));
-					resolve();
-				});
-		});
-	});
+	it('GET /homework/new', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/new')
+			.end((err, res) => {
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain(i18next.t('global.button.addTask'));
+				resolve();
+			});
+	}));
 	// users task
-	it('GET /homework/{users task}/edit', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/59cce2c61113d1132c98dc06/edit')
-				.end((err, res) => {
-					if (err) reject(err);
-					expect(res.statusCode).to.equal(200);
-					expect(res.text).to.contain(i18next.t('homework._task.headline.editTask'));
-					resolve();
-				});
-		});
-	});
+	it('GET /homework/{users task}/edit', () => new Promise((resolve, reject) => {
+		this.agent
+			.get('/homework/59cce2c61113d1132c98dc06/edit')
+			.end((err, res) => {
+				if (err) reject(err);
+				expect(res.statusCode).to.equal(200);
+				expect(res.text).to.contain(i18next.t('homework._task.headline.editTask'));
+				resolve();
+			});
+	}));
 	// teachers task
-	it('GET /homework/{teachers task}/edit', function () {
-		return new Promise((resolve, reject) => {
-			this.agent
-				.get('/homework/59cce3f6c6abf042248e888d/edit')
-				.end((err, res) => {
-					expect(res.statusCode).to.not.equal(200);
-					expect(res.text).to.not.contain(i18next.t('homework._task.headline.editTask'));
-					resolve();
-				});
-		});
-	});
+	it('GET /homework/{teachers task}/edit', () => new Promise((resolve) => {
+		this.agent
+			.get('/homework/59cce3f6c6abf042248e888d/edit')
+			.end((err, res) => {
+				expect(res.statusCode).to.not.equal(200);
+				expect(res.text).to.not.contain(i18next.t('homework._task.headline.editTask'));
+				resolve();
+			});
+	}));
 });
