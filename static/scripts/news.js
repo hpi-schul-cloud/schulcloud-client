aec27ac109bd3ff28d7e8989fd287928eb6be53c
Daniel Rieckhof <Daniel.Rieckhof@hpi.uni-potsdam.de>
2020-09-30 06:41:45 +0200

    Fixed news form validation

--- static/scripts/news.js
+++ static/scripts/news.js
@@ -1,10 +1,8 @@
-$(document).ready(() => {
+$(() => {
 	$('#news-form').on('submit', (e) => {
-		e.preventDefault();
-		if ($('input[name=title]').val().length === 0) {
+		if ($('input[name=title]').val().trim().length === 0) {
 			$.showNotification($t('news.text.newsNeedTitle'), 'danger', 10000);
-		} else {
-			this.submit();
+			e.preventDefault();
 		}
 	});
 });
