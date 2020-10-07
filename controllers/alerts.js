e37be5c64adbbc283bf0047403218008afe6c666
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-10-08 00:43:34 +0200

    small improvement for alert handler

--- controllers/alerts.js
+++ controllers/alerts.js
@@ -5,18 +5,18 @@ const logger = require('../helpers/logger');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-	if (!Configuration.get('FEATURE_ALERTS_ENABLED')) {
-		res.json([]);
-	} else {
-		api(req).get('/alert')
-			.then((alert) => {
-				res.json(alert);
-			}).catch((err) => {
-				logger.error(new Error('Can not get /alert', err));
-				res.json([]);
-			});
-	}
-});
+let handler = (req, res) => {
+	res.json([]);
+};
+if (Configuration.get('FEATURE_ALERTS_ENABLED')) {
+	handler = (req, res) => api(req).get('/alert')
+		.then((alert) => {
+			res.json(alert);
+		}).catch((err) => {
+			logger.error(new Error('Can not get /alert', err));
+			res.json([]);
+		});
+}
+router.get('/', handler);
 
 module.exports = router;
