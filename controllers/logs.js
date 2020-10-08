a700551e0dc8982c0e9e862886e750db2c05555c
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-10-08 12:50:17 +0200

    Add try catch

--- controllers/logs.js
+++ controllers/logs.js
@@ -29,65 +29,69 @@ function getPathFromUrl(url) {
 const sendHandler = (req, res) => {
 	// response and then start process
 	res.sendStatus(200);
+	try {
 
-	const data = req.body;
-	const { context } = data.attributes;
-	data.attributes.url = getPathFromUrl(idCleanup(data.attributes.url));
-	const xApi = {
-		actor: {
-			account: {
-				id: res.locals.currentUser._id,
-				school_id: res.locals.currentSchool,
-				roles: res.locals.currentUser.roles.map((r) => r.name),
+		const data = req.body;
+		const { context } = data.attributes;
+		data.attributes.url = getPathFromUrl(idCleanup(data.attributes.url));
+		const xApi = {
+			actor: {
+				account: {
+					id: res.locals.currentUser._id,
+					school_id: res.locals.currentSchool,
+					roles: res.locals.currentUser.roles.map((r) => r.name),
+				},
+				objectType: 'Agent',
 			},
-			objectType: 'Agent',
-		},
-		verb: {
-			id: 'http://id.tincanapi.com/verb/viewed',
-			display: {
-				'en-US': 'viewed',
+			verb: {
+				id: 'http://id.tincanapi.com/verb/viewed',
+				display: {
+					'en-US': 'viewed',
+				},
 			},
-		},
-		object: {
-			id: data.attributes.url,
-			objectType: 'Activity',
-		},
-		context: {
-			contextActivities: {
-				'queue-time': data.attributes.qt, // queue time (integer)
-
-				'first-paint': context['first-paint'], // cm1 is first paint ms
-				'time-to-interactive': context['time-to-interactive'], // cm2 is time to interactive ms
-				'page-loaded': context['page-loaded'], // page load time ms
-				'dom-interactive-time': context['dom-interactive-time'], // dom interactive time ms
-				'dom-content-loaded': context['dom-content-loaded'], // content load time ms
-				downlink: context.downlink, // download speed in mbit/s
-				'request-start': context['request-start'],
-				'response-start': context['response-start'],
-				'response-end': context['response-end'],
-
-				connection: context.connection, // connection type http://wicg.github.io/netinfo/
-				localhost: data.attributes.url.includes('localhost'),
-				networkProtocol: context.networkProtocol, // http1.1 / http2 / unknown
+			object: {
+				id: data.attributes.url,
+				objectType: 'Activity',
 			},
-		},
-	};
+			context: {
+				contextActivities: {
+					'queue-time': data.attributes.qt, // queue time (integer)
 
-	const options = {
-		method: 'POST',
-		uri: `${INSIGHTS_COLLECTOR_URI}/insights`,
-		body: xApi,
-		json: true,
-	};
-	if (KEEP_ALIVE) {
-		options.headers = {
-			Connection: 'Keep-Alive',
+					'first-paint': context['first-paint'], // cm1 is first paint ms
+					'time-to-interactive': context['time-to-interactive'], // cm2 is time to interactive ms
+					'page-loaded': context['page-loaded'], // page load time ms
+					'dom-interactive-time': context['dom-interactive-time'], // dom interactive time ms
+					'dom-content-loaded': context['dom-content-loaded'], // content load time ms
+					downlink: context.downlink, // download speed in mbit/s
+					'request-start': context['request-start'],
+					'response-start': context['response-start'],
+					'response-end': context['response-end'],
+
+					connection: context.connection, // connection type http://wicg.github.io/netinfo/
+					localhost: data.attributes.url.includes('localhost'),
+					networkProtocol: context.networkProtocol, // http1.1 / http2 / unknown
+				},
+			},
 		};
-	}
 
-	rp(options).catch((err) => {
+		const options = {
+			method: 'POST',
+			uri: `${INSIGHTS_COLLECTOR_URI}/insights`,
+			body: xApi,
+			json: true,
+		};
+		if (KEEP_ALIVE) {
+			options.headers = {
+				Connection: 'Keep-Alive',
+			};
+		}
+
+		rp(options).catch((err) => {
+			logger.error('Error while communicating with Insights', err);
+		});
+	} catch (err) {
 		logger.error('Error while communicating with Insights', err);
-	});
+	}
 };
 
 let handler = (req, res) => res.send(204);
