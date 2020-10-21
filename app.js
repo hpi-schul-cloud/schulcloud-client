d02ffa561d35388d6ab9826dd084879f405e2552
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-10-21 16:50:03 +0200

    remove cookie defaults from session cookie

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const Sentry = require('@sentry/node');
 const { Configuration } = require('@schul-cloud/commons');
 const { tokenInjector, duplicateTokenHandler, csrfErrorHandler } = require('./helpers/csrf');
 const { nonceValueSet } = require('./helpers/csp');
-const { cookieDefaults } = require('./helpers/cookieHelper');
 
 
 const { version } = require('./package.json');
@@ -145,7 +144,7 @@ if (redisUrl) {
 const SIX_HOURS = 1000 * 60 * 60 * 6;
 app.use(session({
 	cookie: {
-		...cookieDefaults,
+		// TODO ...cookieDefaults,
 		maxAge: SIX_HOURS,
 	},
 	rolling: true, // refresh session with every request within maxAge
