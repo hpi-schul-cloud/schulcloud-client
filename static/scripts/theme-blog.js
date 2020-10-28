eb6a5ed3067c6d9c9ec15da6aaf2c5d9b072b82a
Artur <bialoskorski.artur@gmail.com>
2020-10-28 08:40:46 +0100

    fix, added finalhtml which will be placed in content div on homepage

--- static/scripts/theme-blog.js
+++ static/scripts/theme-blog.js
@@ -3,8 +3,19 @@ const stripHtml = require('string-strip-html');
 const ghostTheme = $('#SC_THEME').text();
 
 function fetchContent() {
+	let finalHtml = '';
+
 	$(`.${ghostTheme}-blog .spinner`).show();
 	$(`.${ghostTheme}-blog .placeholder`).hide();
+
+	const changePage = () => {
+		$(`.${ghostTheme}-blog .loading`).remove();
+		finalHtml = finalHtml
+			.replace(/<td>x<[/]td>/g, '<td><i class="fa fa-check"></i></td>');
+		$(`.${ghostTheme}-blog .content`).html(stripHtml(finalHtml,
+			{ onlyStripTags: ['script', 'style'] }));
+		$(`.${ghostTheme}-blog .content`).css('opacity', '1');
+	};
 
 	const themeUrl = ghostTheme === 'n21' ? '/ghost/startseite-nbc' : `/ghost/startseite-${ghostTheme}`;
 
@@ -16,12 +27,8 @@ function fetchContent() {
 		timeout: 8000,
 	})
 		.done((result) => {
-			$(`.${ghostTheme}-blog .title`).text(result.pages[0].title);
-			$(`.${ghostTheme}-blog .content`).html(
-				stripHtml(result.pages[0].html, {
-					onlyStripTags: ['script', 'style'],
-				}),
-			);
+			finalHtml = result.pages[0].html;
+			changePage();
 		})
 		.fail(() => {
 			$(`.${ghostTheme}-blog .spinner`).hide();
