97185c6d2e238c9359bf558a486af86e49c67bde
CaptainUnbrauchbar <f.frankreiter@gmail.com>
2020-11-02 15:18:56 +0100

    grammar fix by switch

--- helpers/theme.js
+++ helpers/theme.js
@@ -6,14 +6,26 @@ const {
 	HOST,
 } = require('../config/global');
 
+//Switch for all instance specific name exceptions (can be added here)
+const instanceSpecificTitle = () => {
+	switch (SC_THEME) {
+		case "n21": 
+			return "Niedersächsischen Bildungscloud";
+		default: 
+			return SC_TITLE;
+	};
+};
+
 const setTheme = (res) => {
 	const documents = defaultDocuments();
 	const baseDir = (res.locals.currentSchoolData || {}).documentBaseDir || documents.documentBaseDir;
+	const n_title = instanceSpecificTitle();
 
 	res.locals.theme = {
 		name: SC_THEME,
 		title: SC_TITLE,
 		short_title: SC_SHORT_TITLE,
+		n_title: n_title,
 		documents: {
 			baseDir,
 			specificFiles: documents.specificFiles(baseDir),
