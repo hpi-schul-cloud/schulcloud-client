c6cf2d4eeffdf8eaf46dcb6f04bffe3af8f8f339
Michael Wolowyk <michael.wolowyk@capgemini.com>
2020-11-06 10:42:42 +0100

    SC-7669 reverted handlebars changes

--- helpers/handlebars/helpers/index.js
+++ helpers/handlebars/helpers/index.js
@@ -189,12 +189,15 @@ const helpers = () => ({
 			.subtract(days, 'days')
 			.format(format);
 	},
-	dateToPicker: (date) => timesHelper.formatDate(date, i18n.getInstance()('format.dateToPicker')),
-	dateTimeToPicker: (date) => timesHelper.formatDate(date, i18n.getInstance()('format.dateTimeToPicker')),
-	i18nDate: (date) => timesHelper.dateToDateString(date),
-	i18nDateTime: (date) => timesHelper.dateToDateTimeString(date, true),
-	timeToString: (date) => timesHelper.timeToString(date),
-	currentYear: () => timesHelper.currentDate().year(),
+	dateToPicker: (date) => timesHelper.moment(date).format(i18n.getInstance()('format.dateToPicker')),
+	dateTimeToPicker: (date) => timesHelper.moment(date).format(i18n.getInstance()('format.dateTimeToPicker')),
+	i18nDate: (date) => timesHelper.moment(date).format(i18n.getInstance()('format.date')),
+	i18nDateTime: (date) => timesHelper.moment(date).format(i18n.getInstance()('format.dateTime')),
+	i18nDateString: (date) => timesHelper.moment(date).format(i18n.getInstance()('format.dateLong')),
+	timeToString: (date) => timesHelper.timeToString(date, i18n.getInstance()('format.dateTime')),
+	currentYear() {
+		return new Date().getFullYear();
+	},
 	concat() {
 		const arg = Array.prototype.slice.call(arguments, 0);
 		arg.pop();
