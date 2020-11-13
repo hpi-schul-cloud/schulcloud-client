a89ab3c00cdb0cd7eb07fc555bc5895e0e468210
Virgil Chiriac <virgil.chiriac@hpi.de>
2020-11-13 11:07:48 +0100

    SC-5179 - Fixes CKEditor on homework to insert images from Course instead of My Files

--- static/scripts/ckeditor/ckeditor.js
+++ static/scripts/ckeditor/ckeditor.js
@@ -1,18 +1,51 @@
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import ckeditorConfig from './ckeditor-config';
 
-window.addEventListener('DOMContentLoaded', async () => {
-	document.querySelectorAll('.ckeditor').forEach(async (element) => {
-		const editor = await ClassicEditor.create(element, ckeditorConfig);
-		editor.model.document.on('change:data', () => {
-			const submitButton = document.querySelector('.ckeditor-submit');
-			if (submitButton) {
-				const editorContent = editor.getData();
-				const editorContainsText = editorContent !== '';
-				submitButton.setAttribute('editorContainsText', editorContainsText);
-				const fileIsUploaded = submitButton.getAttribute('fileIsUploaded');
-				submitButton.disabled = !editorContainsText && !fileIsUploaded;
+const url = window.location.pathname;
+const urlParts = url.split("/");
+
+const setStorageContext = () => {
+	if (urlParts[1] === "homework") {
+		let storageContext;
+		if (urlParts[2] === 'new' || urlParts[3] === "edit") {
+			const course = document.getElementById("coursePicker").value;
+			storageContext = `/files/courses/${course}`;
+			if (!course) {
+				// Show error
+				storageContext = `/files/courses/`;
 			}
-		});
+		} else {
+			storageContext =
+				document.getElementById("courseId").getAttribute("href") ||
+				"courses";
+		}
+		ckeditorConfig.filebrowser.browseUrl = `${storageContext}`;
+	}
+}
+
+const initEditor = async (element) => {
+	setStorageContext();
+	const editor = await ClassicEditor.create(element, ckeditorConfig);
+
+	if (urlParts[1] === "homework" && (urlParts[2] === 'new' || urlParts[3] === "edit")) {
+		document.getElementById("coursePicker").onchange = async function () {
+			await editor.destroy(element);
+			await initEditor(element);
+		};
+	}
+
+	editor.model.document.on("change:data", () => {
+		const submitButton = document.querySelector(".ckeditor-submit");
+		if (submitButton) {
+			const editorContent = editor.getData();
+			const editorContainsText = editorContent !== "";
+			submitButton.setAttribute("editorContainsText", editorContainsText);
+			const fileIsUploaded = submitButton.getAttribute("fileIsUploaded");
+			submitButton.disabled = !editorContainsText && !fileIsUploaded;
+		}
 	});
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+	document.querySelectorAll('.ckeditor').forEach(async (element) => await initEditor(element));
 });
