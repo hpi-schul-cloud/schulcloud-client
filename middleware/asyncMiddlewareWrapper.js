0e77768a4cc677f0a97a581cf49aa6da5580ede1
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-11-23 16:20:22 +0100

    enable promise linter rules

--- middleware/asyncMiddlewareWrapper.js
+++ middleware/asyncMiddlewareWrapper.js
@@ -1,10 +1,11 @@
 /**
  * Express is loosing the control flow for error handling of async middleware functions.
  * All async middlewares need to be wrapped with this method to ensure error handling works as expected.
- * @param {*} middlewareFunction async (req, res, next)
+ * @param {*} middlewareFunction async (req, res, next) - next must be the last argument
  */
 module.exports = (middlewareFunction) => function asyncWrapper(...args) {
 	const fnReturn = middlewareFunction(...args);
 	const next = args[args.length - 1];
+	// eslint-disable-next-line promise/no-callback-in-promise
 	return Promise.resolve(fnReturn).catch(next);
 };
