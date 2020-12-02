03e815923080b5b25cc9ea112bdaf65c9d101c1a
RalfNellesCap <ralf.nelles.ext@capgemini.com>
2020-12-02 10:04:18 +0000

    [OPS-1664] changed docker tag Version to LOWER "v"

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ catch() {
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
   #export DOCKERTAG=latest
-  export DOCKERTAG="master_V$( jq -r '.version' package.json )_latest"
+  export DOCKERTAG="master_v$( jq -r '.version' package.json )_latest"
 elif [ "$TRAVIS_BRANCH" = "develop" ]
 then
   #export DOCKERTAG=latest
@@ -19,7 +19,7 @@ then
 elif [[ "$TRAVIS_BRANCH" =~ ^"release"* ]]
 then
   #export DOCKERTAG=latest
-  export DOCKERTAG="release_V$( jq -r '.version' package.json )_latest"
+  export DOCKERTAG="release_v$( jq -r '.version' package.json )_latest"
 elif [[ "$TRAVIS_BRANCH" =~ ^feature\/[A-Z]+-[0-9]+-[a-zA-Z_]+$ ]]
 then
 	# extract JIRA_TICKET_ID from TRAVIS_BRANCH
