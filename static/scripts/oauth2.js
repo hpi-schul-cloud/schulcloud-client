b68163cf6142ac02e515fa4b86b35c56e3563b44
Dominik Glandorf <do.gl@gmx.de>
2020-12-14 20:52:46 +0100

    use storage access api for depseudo

---
+++ static/scripts/oauth2.js
@@ -0,0 +1,9 @@
+$(() => {
+	document.hasStorageAccess().then((hasAccess) => {
+		if (!hasAccess) {
+			$(document).on('click', () => {
+				document.requestStorageAccess().then(() => window.location.reload());
+			});
+		}
+	});
+});
