84bf4b480702b302c66d11cc92751fb5828dc12a
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-12-23 13:34:48 +0100

    Use format defaults, when translations are not ready.

--- static/scripts/datetime/datetime.js
+++ static/scripts/datetime/datetime.js
@@ -1,9 +1,25 @@
 import moment from 'moment-timezone';
 import getCookie from '../helpers/cookieManager';
 
+const FORMAT = {
+	date: 'DD.MM.YYYY',
+	dateTime: 'DD.MM.YYYY HH:mm',
+	dateLong: 'dddd, DD. MMMM YYYY',
+	dateToPicker: 'DD.MM.YYYY',
+	dateTimeToPicker: 'DD.MM.YYYY HH:mm',
+};
+
+const getTranslatedFormat = (formatProp, translationKey) => {
+	const result = $t(translationKey);
+	if (result === translationKey) {
+		// translations not initialized yet
+		return FORMAT[formatProp];
+	} return result;
+};
+
 const DATETIME_FORMAT = {
-	date: $t('format.dateToPicker'),
-	dateTime: $t('format.dateTimeToPicker'),
+	date: () => getTranslatedFormat('date', 'format.dateToPicker'),
+	dateTime: () => getTranslatedFormat('dateTime', 'format.dateTimeToPicker'),
 };
 
 const calendarTimezone = document.querySelector('html').getAttribute('timezone');
@@ -19,17 +35,17 @@ const now = () => moment();
 
 const toMoment = (date) => (date ? moment(date) : now());
 
-const dateTimeStringToMoment = (date, format = DATETIME_FORMAT.dateTime) => moment(date, format);
+const dateTimeStringToMoment = (date, format = DATETIME_FORMAT.dateTime()) => moment(date, format);
 
 const addStringOffset = (showOffset) => (showOffset && !userHasSchoolTimezone ? `(UTC${getUtcOffset()})` : '');
 
 // eslint-disable-next-line max-len
-const toDateTimeString = (date, showOffset = false, format = DATETIME_FORMAT.dateTime) => `${toMoment(date).format(format)}${addStringOffset(showOffset)}`;
+const toDateTimeString = (date, showOffset = false, format = DATETIME_FORMAT.dateTime()) => `${toMoment(date).format(format)}${addStringOffset(showOffset)}`;
 
 const fromNow = (date) => toMoment(date).fromNow();
 
 const inputRange = ({
-	from, toOffset = 0, toOffsetBase = 'y', format = DATETIME_FORMAT.dateTime,
+	from, toOffset = 0, toOffsetBase = 'y', format = DATETIME_FORMAT.dateTime(),
 }) => {
 	const startDate = toMoment(from).startOf('hour').add(1, 'hour');
 	const endDate = startDate.clone().add(toOffset, toOffsetBase);
