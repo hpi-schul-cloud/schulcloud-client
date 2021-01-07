8104a8f6953868be6632bf7ee66f331e362a80c6
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-01-07 18:53:44 +0100

    remove alerts caching

--- controllers/alerts.js
+++ controllers/alerts.js
@@ -6,17 +6,14 @@ const logger = require('../helpers/logger');
 const router = express.Router();
 
 let handler = (req, res) => {
-	res.set('Cache-Control', 'public, max-age=3600');
 	res.json([]);
 };
 if (Configuration.get('FEATURE_ALERTS_ENABLED')) {
 	handler = (req, res) => api(req).get('/alert')
 		.then((alert) => {
-			res.set('Cache-Control', 'public, max-age=120');
 			res.json(alert);
 		}).catch((err) => {
 			logger.error(new Error('Can not get /alert', err));
-			res.set('Cache-Control', 'public, max-age=60');
 			res.json([]);
 		});
 }
