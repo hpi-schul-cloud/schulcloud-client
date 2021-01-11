b30d903f94fb70079cf688e13166e769fc4fb4a2
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-01-11 21:07:04 +0100

    hash css urls

--- middleware/assets.js
+++ middleware/assets.js
@@ -7,28 +7,27 @@ const {
 	SC_THEME,
 } = require('../config/global');
 
+let staticifyInstance = null;
 const localesDir = path.join(__dirname, '../locales');
 const buildThemeAssetDir = path.join(__dirname, `../build/${SC_THEME}`);
-const buildThemeAssetDirExists = fs.existsSync(buildThemeAssetDir) === true;
 
-const expectAssetsDir = () => {
-	if (!buildThemeAssetDirExists) {
-		throw new Error(`assets dir required, but not found: ${buildThemeAssetDir}. `
-		+ 'Have you executed \'npm run build\' already?');
+/**
+ * initializes the staticify instance lazy which is required in gulp
+ */
+const lazyInitialization = () => {
+	if (staticifyInstance == null) {
+		// expectAssetsDir();
+		// configure static file hashing and caching
+		staticifyInstance = staticify(buildThemeAssetDir, {
+			maxAgeNonHashed: '1d',
+			sendOptions: {
+				// seconds multiplied by 1000 as it takes millis
+				maxAge: Configuration.get('ASSET_CACHING_MAX_AGE_SECONDS') * 1000,
+				etag: false,
+			},
+		});
 	}
 };
-
-expectAssetsDir();
-
-// configure static file hashing and caching
-const staticifyInstance = staticify(buildThemeAssetDir, {
-	maxAgeNonHashed: '1d',
-	sendOptions: {
-		// seconds multiplied by 1000 as it takes millis
-		maxAge: Configuration.get('ASSET_CACHING_MAX_AGE_SECONDS') * 1000,
-		etag: false,
-	},
-});
 
 /**
  * middleware for static assets may use hashed file names
@@ -38,6 +37,7 @@ const staticAssetsMiddleware = (app) => {
 	app.use(express.static(path.join(buildThemeAssetDir)));
 	app.use((req, res, next) => {
 		if (Configuration.get('FEATURE_ASSET_CACHING_ENABLED') === true) {
+			lazyInitialization();
 			return staticifyInstance.middleware(req, res, next);
 		}
 		return next();
@@ -50,9 +50,19 @@ const staticAssetsMiddleware = (app) => {
  */
 const getStaticAssetPath = (staticFilePath) => {
 	if (Configuration.get('FEATURE_ASSET_CACHING_ENABLED') === true) {
+		lazyInitialization();
 		return staticifyInstance.getVersionedPath(staticFilePath);
 	}
 	return staticFilePath;
 };
 
-module.exports = { staticAssetsMiddleware, getStaticAssetPath };
+const rewriteStaticAssetPaths = (content) => {
+	if (Configuration.get('FEATURE_ASSET_CACHING_ENABLED') === true) {
+		lazyInitialization();
+		const contentWithRewrittenUrls = staticifyInstance.replacePaths(content);
+		return contentWithRewrittenUrls;
+	}
+	return content;
+};
+
+module.exports = { staticAssetsMiddleware, getStaticAssetPath, rewriteStaticAssetPaths };
