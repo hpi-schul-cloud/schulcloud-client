962845891a4b5e3093865c5606618d37c3606796
Mohammad Abralur Rahman Akash <59754949+abralurrahman@users.noreply.github.com>
2021-01-13 13:00:48 +0100

    another way show password

--- static/scripts/toggle/toggle.js
+++ static/scripts/toggle/toggle.js
@@ -1,4 +1,14 @@
-function toggle() {
+const password = document.querySelector('#password');
+const show_Password = document.querySelector('#show_Password');
+
+show_Password.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+/*function toggle() {
     var temp = document.getElementById("show_Password");
     if (temp.type === "password") {
         temp.type = "text";
@@ -6,4 +16,4 @@ function toggle() {
     else {
         temp.type = "password";
     }
-}
+}*/
