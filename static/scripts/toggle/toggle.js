4e31fc270ecb8a7c1e9569a7f7267d12a4983ad9
Mohammad Abralur Rahman Akash <59754949+abralurrahman@users.noreply.github.com>
2021-01-13 14:27:17 +0100

    codacy fixing

--- static/scripts/toggle/toggle.js
+++ static/scripts/toggle/toggle.js
@@ -1,10 +1,12 @@
-const password = document.querySelector('#password');
+const settings_current_password = document.querySelector('#settings_current_password');
+const passwordNew = document.querySelector('#passwordNew');
+const password_control = document.querySelector('#password_control');
 const show_Password = document.querySelector('#show_Password');
 
 show_Password.addEventListener('click', function (e) {
     // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
+    const type = settings_current_password.getAttribute('type') === 'password' ? 'text' : 'password';
+    settings_current_password.setAttribute('type', type);
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
 });
