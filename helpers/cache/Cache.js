2bd320b262c353f43d8ff275544204c384a59a7e
Evers <git@it-4d.de>
2021-04-15 16:39:02 +0200

    rename parameter

--- helpers/cache/Cache.js
+++ helpers/cache/Cache.js
@@ -1,19 +1,19 @@
 class Cache {
-	constructor(getFunctions, { updateInvervalSecounds } = {}) {
+	constructor(callback, { updateInvervalSecounds } = {}) {
 		this.lastUpdated = 0;
 		this.updateInvervalSecounds = (updateInvervalSecounds * 1000) || 60000;
 		this.data = null;
-		if (!(typeof getFunctions === 'function')) {
+		if (!(typeof callback === 'function')) {
 			throw new Error('First parameter in "Cache" constructor must be a function.');
 		}
-		this.getFunctions = getFunctions;
+		this.callback = callback;
 	}
 
 	async get(req, res, next) {
 		if (Date.now() > (this.lastUpdated + this.updateInvervalSecounds)) {
 			// please first update time and response old data until request return result
 			this.lastUpdated = Date.now();
-			this.data = this.getFunctions(req, res, next);
+			this.data = this.callback(req, res, next);
 		}
 		return this.data;
 	}
