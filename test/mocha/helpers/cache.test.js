07db7e323f9520330fffe1628db0e11e9deaa9d7
Evers <git@it-4d.de>
2021-04-15 16:12:40 +0200

    Add tests for caching helper.
    Co-Authored-By: Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
    Co-Authored-By: mwolowyk-cap <64189574+mwolowyk-cap@users.noreply.github.com>

---
+++ test/mocha/helpers/cache.test.js
@@ -0,0 +1,72 @@
+const { expect } = require('chai');
+const { result } = require('lodash');
+
+const Cache = require('../../../helpers/cache/Cache');
+
+
+describe('Cache helper class', () => {
+	it('should return a promise', () => {
+		const callback = () => 'text';
+		const cache = new Cache(callback, { updateInvervalSecounds: 60 });
+
+		const resultPromise = cache.get();
+		expect(resultPromise, 'when input is not a promise').to.be.a('promise');
+	});
+
+	it('should return the result first from callback and after it from cache', async () => {
+		const initalData = { a: 0 };
+		const changedData = { b: 1 };
+
+		let returnData = initalData;
+		const callback = async () => returnData;
+
+		const cache = new Cache(callback, { updateInvervalSecounds: 60 });
+
+		const result1 = await cache.get();
+		expect(result1, 'that data called direclty.').eql(initalData);
+		returnData = changedData;
+
+		const result2 = await cache.get();
+		expect(result2, 'that data retrieved from cache.').eql(initalData);
+	});
+
+	it('should recall ressources if update interval time is expired', async () => {
+		const initalData = { a: 0 };
+		const changedData = { b: 1 };
+
+		let returnData = initalData;
+		const callback = async () => returnData;
+
+		// -1 as time force is for test case
+		const cache = new Cache(callback, { updateInvervalSecounds: -1 });
+
+		const result1 = await cache.get();
+		expect(result1, 'that data called direclty.').eql(initalData);
+		returnData = changedData;
+
+		const result2 = await cache.get();
+		expect(result2, 'that data called direclty after cache interval is expired.').eql(changedData);
+	});
+
+	it('should pass 3 parameters to the callback', async () => {
+		let passed1;
+		let passed2;
+		let passed3;
+
+		const input1 = 'param1';
+		const input2 = 'param2';
+		const input3 = 'param3';
+
+		const callback = (a, b, c) => {
+			passed1 = a;
+			passed2 = b;
+			passed3 = c;
+		};
+		const cache = new Cache(callback, { updateInvervalSecounds: 60 });
+
+		await cache.get(input1, input2, input3);
+		expect(passed1).equal(input1);
+		expect(passed2).equal(input2);
+		expect(passed3).equal(input3);
+	});
+});
