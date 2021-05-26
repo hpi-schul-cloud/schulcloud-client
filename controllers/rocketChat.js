9ec543cfcf4417a03627637f30c23eb68431f837
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2021-05-26 10:34:44 +0200

    SC-9071 linter

--- controllers/rocketChat.js
+++ controllers/rocketChat.js
@@ -10,23 +10,28 @@ const ROCKET_CHAT_URI = Configuration.get('ROCKET_CHAT_URI');
 
 router.use(authHelper.authChecker);
 
-router.get('/Iframe', (req, res, next) => api(req).get(`/rocketChat/login/${res.locals.currentUser._id}`).then((result) => {
-	return res.send(`<script type="text/javascript" src="/rocketChat/Iframe-js"></script>`);
-}));
+router.get('/Iframe',
+	(req, res, next) => api(req).get(`/rocketChat/login/${res.locals.currentUser._id}`).then(
+		(result) => res.send('<script type="text/javascript" src="/rocketChat/Iframe-js"></script>'),
+	));
 
-router.get('/Iframe-js', (req, res, next) => api(req).get(`/rocketChat/login/${res.locals.currentUser._id}`).then((result) => {
-	const shortURL = ROCKET_CHAT_URI;
-	const rocketChatURL = `${shortURL}/home`;
-	return res.send(`
-            window.parent.postMessage({
-                event: 'login-with-token',
-                loginToken: '${result.authToken}'
-                }, '${rocketChatURL}');
-            `);
-}));
+router.get('/Iframe-js',
+	(req, res, next) => api(req).get(`/rocketChat/login/${res.locals.currentUser._id}`).then((result) => {
+		const shortURL = ROCKET_CHAT_URI;
+		const rocketChatURL = `${shortURL}/home`;
+		return res.send(`
+                window.parent.postMessage({
+                    event: 'login-with-token',
+                    loginToken: '${result.authToken}'
+                    }, '${rocketChatURL}');
+                `);
+	}));
 
-router.get('/authGet', (req, res, next) => api(req).get(`/rocketChat/login/${res.locals.currentUser._id}`, {}).then((result) => res.send({
-	loginToken: result.authToken,
-})));
+router.get('/authGet',
+	(req, res, next) => api(req).get(`/rocketChat/login/${res.locals.currentUser._id}`, {}).then(
+		(result) => res.send({
+			loginToken: result.authToken,
+		}),
+	));
 
 module.exports = router;
