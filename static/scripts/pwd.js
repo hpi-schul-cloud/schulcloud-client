4e53382a2ad945e178cc25f5ad81a8828a8bfaf7
Murat <murat.merdoglu@hpi.de>
2021-07-20 15:15:53 +0200

    SC-9167: replacing <i> tags to <span>

--- static/scripts/pwd.js
+++ static/scripts/pwd.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
 	$('.show_hide_password button').on('click', (event) => {
 		event.preventDefault();
-		const element = ($(event.target).is(':parent')) ? $(event.target).find('i') : $(event.target);
+		const element = ($(event.target).is(':parent')) ? $(event.target).find('span') : $(event.target);
 		const passwordId = $($(event.target).parents('div')[1]).find('input').attr('id');
 
 		if ($('#' + passwordId).attr('type') == 'text') {
