2fce656e71ef10578f58e1e34a2dc1554ffea691
Murat <murat.merdoglu@hpi.de>
2021-07-21 15:16:58 +0200

    change element to span

--- static/scripts/pwd.js
+++ static/scripts/pwd.js
@@ -3,12 +3,12 @@ $(document).ready(() => {
 		event.preventDefault();
 		if ($('.show_hide_password input').attr('type') == 'text') {
 			$('.show_hide_password input').attr('type', 'password');
-			$('.show_hide_password i').addClass('fa-eye-slash');
-			$('.show_hide_password i').removeClass('fa-eye');
+			$('.show_hide_password span').addClass('fa-eye-slash');
+			$('.show_hide_password span').removeClass('fa-eye');
 		} else if ($('.show_hide_password input').attr('type') == 'password') {
 			$('.show_hide_password input').attr('type', 'text');
-			$('.show_hide_password i').removeClass('fa-eye-slash');
-			$('.show_hide_password i').addClass('fa-eye');
+			$('.show_hide_password span').removeClass('fa-eye-slash');
+			$('.show_hide_password span').addClass('fa-eye');
 		}
 	});
 
