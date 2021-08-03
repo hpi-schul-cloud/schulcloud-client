0f39880c18df0a5c15570c95eac84f2d57ed48d6
Alexander Riese <alexander.riese@capgemini.com>
2021-08-03 12:11:51 +0200

    fix linter issues

--- static/scripts/topic.js
+++ static/scripts/topic.js
@@ -2,29 +2,27 @@ import { resizeIframes } from './helpers/iFrameResize';
 
 resizeIframes();
 
-$(document).ready(function () {
-	$('.btn-create-share').click(function (e) {
+$(document).on('load', function topicHandler() {
+	$('.btn-create-share').on('click', function click(e) {
 		e.stopPropagation();
 		e.preventDefault();
-		let topicId = $(this).attr("data-href");
-		let courseId = $(this).attr("data-courseId");
+		const topicId = $(this).attr('data-href');
+		const courseId = $(this).attr('data-courseId');
 		const $shareModal = $('.share-topic-modal');
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: `/courses/${courseId}/topics/${topicId}/share/`,
-			success: function(data) {
+			success: function success(data) {
 				populateModalForm($shareModal, {
 					title: $t('courses._course.headline.shareCodeGenerated'),
 					closeLabel: $t('global.button.close'),
-					fields: {shareToken: data.shareToken}
+					fields: { shareToken: data.shareToken },
 				});
 				$shareModal.find('.btn-submit').remove();
-				$shareModal.find("input[name='shareToken']").click(function () {
+				$shareModal.find("input[name='shareToken']").on('click', function click() {
 					$(this).select();
 				});
-
 				$shareModal.appendTo('body').modal('show');
-
 			}
 		});
 	});
