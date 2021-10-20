9285578d8cedc4c3ece5fe19727b40f5eeaa522e
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-10-20 11:38:20 +0200

    fixing codacy static code analysis error

--- static/scripts/default.js
+++ static/scripts/default.js
@@ -3,6 +3,12 @@ window.schulcloudColors = {
 	secondaryColor: $('.secondary-color').first().css('color'),
 };
 
-$("meta[name='theme-color']").first().attr('content', window.schulcloudColors.secondaryColor);
-$("meta[name='apple-mobile-web-app-status-bar-style']").first().attr('content', window.schulcloudColors.secondaryColor);
-$("link[rel='mask-item']").first().attr('content', window.schulcloudColors.primaryColor);
+$("meta[name='theme-color']")
+	.first()
+	.attr('content', window.schulcloudColors.secondaryColor);
+$("meta[name='apple-mobile-web-app-status-bar-style']")
+	.first()
+	.attr('content', window.schulcloudColors.secondaryColor);
+$("link[rel='mask-item']")
+	.first()
+	.attr('content', window.schulcloudColors.primaryColor);
