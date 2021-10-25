f0504e09c0d9e67fc36986481f7fd899268dc57b
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-10-25 15:55:37 +0200

    revert

--- scripts/unitTest.sh
+++ scripts/unitTest.sh
@@ -16,88 +16,62 @@ mv ./* ./schulcloud-client # ignore warning...
 # If current branch is hotfix, switch to branch master
 
 # Preconditions
-_switchBranch(){
-	cd $1
-	echo "switching branch..."
-	git checkout $2 > /dev/null 2>&1 || true
-	echo "(new) active branch for $1:"
-	git branch | grep \* | cut -d ' ' -f2
-	if [ -z "$3" ]
-	then
-		echo "No docker tag set for ${1}"
-		echo $3
-	else
-		set -a
-		export $3=`git rev-parse HEAD`
-		printenv | grep $3
-	fi
-	cd ..
-}
+git clone https://github.com/hpi-schul-cloud/schulcloud-server.git schulcloud-server
+cd schulcloud-server
+if [[ "$BRANCH_NAME" =~ '^hotfix.*' ]]
+then
+echo "Originating branch hotfix detected. Force testing against Server master."
+git checkout master
+else
+git checkout "$BRANCH_NAME"
+fi
+echo "Currently active branch for schulcloud-server: $(git branch | grep \* | cut -d ' ' -f2)"
+npm ci
+npm run build
+cd ..
 
-switchBranch(){
-	_switchBranch "$1" "main" "$2"
+git clone https://github.com/hpi-schul-cloud/docker-compose.git docker-compose
+cd docker-compose
+if [[ "$BRANCH_NAME" =~ '^hotfix.*' ]]
+then
+echo "Originating branch hotfix detected. Force testing against Server master."
+git checkout master
+else
+git checkout "$BRANCH_NAME"
+fi
+echo "Currently active branch for docker-compose: $(git branch | grep \* | cut -d ' ' -f2)"
+cd ..
 
-	# if branch exists, try to switch to it
-	_switchBranch "$1" "$BRANCH_NAME" "$2"
-}
+# start rabbitmq
+cd docker-compose
+docker-compose -f docker-compose.end-to-end-tests-Build.yml build rabbitmq
+docker-compose -f docker-compose.end-to-end-tests-Build.yml up -d rabbitmq
+cd ..
 
-fetch(){
-	# clone all required repositories and try to switch to branch with same name as current one
-	switchBranch "schulcloud-client" "CLIENT_DOCKER_TAG"
+# start mongodb
+cd docker-compose
+docker-compose -f docker-compose.end-to-end-tests-Build.yml build server-mongodb
+docker-compose -f docker-compose.end-to-end-tests-Build.yml up -d server-mongodb
+cd ..
 
-	git clone https://github.com/hpi-schul-cloud/schulcloud-server.git schulcloud-server
-	switchBranch "schulcloud-server" "SERVER_DOCKER_TAG"
+# inject seed data
+cd schulcloud-server
+npm run setup
+cd ..
 
-	git clone https://github.com/hpi-schul-cloud/docker-compose.git docker-compose
-	switchBranch "docker-compose"
-}
+# start server within of
+cd docker-compose
+docker-compose -f docker-compose.end-to-end-tests-Build.yml build server
+docker-compose -f docker-compose.end-to-end-tests-Build.yml up -d server
+cd ..
 
-before(){
-	cd docker-compose
-	docker-compose -f compose-files/docker-compose.yml up -d mongodb mongodb-secondary mongodb-arbiter redis rabbit calendar-init
-	sleep 10
-	docker-compose -f compose-files/docker-compose.yml up -d mongosetup calendar-postgres
-	sleep 15
-	docker-compose -f compose-files/docker-compose.yml up -d calendar
-	sleep 15
-	docker-compose -f compose-files/docker-compose.yml up server server-management &
-	cd ..
+echo "waiting max 4 minutes for server to be available"
+npx wait-on http://localhost:3030 -t 240000 --httpTimeout 250 --log
+echo "server is now online"
 
-	echo "waiting max 4 minutes for server-management to be available"
-		npx wait-on http://localhost:3333/api/docs -t 240000 --httpTimeout 250 --log
-		echo "server-management is now online"
-
-	# inject seed data
-	curl -X POST localhost:3333/api/management/database/seed
-
-	echo "waiting max 4 minutes for server to be available"
-	npx wait-on http://localhost:3030 -t 240000 --httpTimeout 250 --log
-	echo "server is now online"
-
-	echo "waiting max 4 minutes for client to be available"
-	npx wait-on http://localhost:3100 -t 240000 --httpTimeout 250 --log
-	echo "client is now online"
-}
-
-main(){
-	# Execute
-	# client packages are needed for mocha
-	cd schulcloud-client
-	npm ci
-	npm run build
-	npm run mocha
-}
-
-set -e
-echo "FETCH..."
-fetch
-echo "FETCH DONE"
-
-echo "BEFORE..."
-before
-echo "BEFORE DONE"
-
-echo "MAIN..."
-main $1
-echo "MAIN DONE"
-set +e
+# Execute
+# client packages are needed for mocha
+cd schulcloud-client
+npm ci
+npm run build
+npm run mocha
