2d4e5697fe2be48e31d37bf97e0459b3a50d02a4
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-11-15 11:56:31 +0100

    removing declined institute name

--- helpers/theme.js
+++ helpers/theme.js
@@ -15,19 +15,6 @@ const instanceSpecificTitle = () => {
 	}
 };
 
-const instanceInstituteGerman = () => {
-	switch (SC_THEME) {
-		case 'n21':
-			return 'der Landesinitiative n-21 Schulen in Niedersachsen online e.V';
-		case 'thr':
-			return 'dem Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien (Thillm)';
-		case 'brb':
-			return 'dem Ministerium für Bildung, Jugend und Sport (MBJS)';
-		default:
-			return 'Dataport';
-	}
-};
-
 const instanceInstitute = () => {
 	switch (SC_THEME) {
 		case 'n21':
@@ -45,7 +32,6 @@ const setTheme = (res) => {
 	const documents = defaultDocuments();
 	const baseDir = (res.locals.currentSchoolData || {}).documentBaseDir || documents.documentBaseDir;
 	const themeTitle = instanceSpecificTitle();
-	const instituteTitleGerman = instanceInstituteGerman();
 	const instituteTitle = instanceInstitute();
 
 	res.locals.theme = {
@@ -53,7 +39,6 @@ const setTheme = (res) => {
 		title: SC_TITLE,
 		short_title: SC_SHORT_TITLE,
 		theme_title: themeTitle,
-		institute_title_german: instituteTitleGerman,
 		institute_title: instituteTitle,
 		documents: {
 			baseDir,
