1ed22fbe55bf36dcf2231804aefe9fc8634f6c50
Mona Weitzenberg <monaweitzenberg@gmail.com>
2021-11-19 16:14:26 +0100

    adjust email contact brb

--- helpers/theme.js
+++ helpers/theme.js
@@ -18,7 +18,7 @@ const instanceSpecificTitle = () => {
 const instanceInstitute = () => {
 	switch (SC_THEME) {
 		case 'n21':
-			return 'Landesinitiative n-21: Schulen in Niedersachsen online e.V';
+			return 'Landesinitiative n-21: Schulen in Niedersachsen online e.V.';
 		case 'thr':
 			return 'Thüringer Institut für Lehrerfortbildung, Lehrplanentwicklung und Medien';
 		case 'brb':
