4435fe9dc92a5d1fb5d688682143be7e466243f5
Mamut <paul.poppe@dataport.de>
2022-04-11 14:59:53 +0200

    BC-1634 - Move code for the nexboard api from package to this repo

---
+++ helpers/nexboard.js
@@ -0,0 +1,120 @@
+const rp = require('request-promise');
+const DEFAULT_BASE_URL = 'https://nexboard.nexenio.com/portal/api/v1/public/';
+
+class Nexboard {
+	constructor(apiKey, userID, url) {
+		this.apiKey = apiKey;
+		this.user = userID;
+		this.url = url || DEFAULT_BASE_URL;
+	}
+
+	getProjectsIds() {
+		const settings = {
+			method: 'GET',
+			uri: `${this.url}projects`,
+			qs: {
+				userId: this.user,
+				token: this.apiKey,
+			},
+			json: true,
+		};
+
+		return rp(settings)
+			.then((res) => {
+				return res.map((e) => {
+					return e.id;
+				});
+			})
+			.catch((err) => {
+				return Promise.reject(new Error(`Could not retrieve ProjectIds - ${err.error.msg}`));
+			});
+	}
+
+	createProject(title, description) {
+		const settings = {
+			method: 'POST',
+			uri: `${this.url}projects`,
+			qs: {
+				token: this.apiKey,
+				userId: this.user,
+			},
+			body: {
+				title,
+				description,
+			},
+			headers: { "Content-Type": 'application/json' },
+			json: true,
+		};
+
+		return rp(settings)
+			.then((res) => {
+				return res;
+			})
+			.catch((err) => {
+				return Promise.reject(new Error(`Could not create new Project - ${err.error.msg}`));
+			});
+	}
+
+	getBoardsByProject(projectId) {
+		const settings = {
+			method: 'GET',
+			uri: `${this.url}projects/${projectId}/boards`,
+			qs: {
+				userId: this.user,
+				token: this.apiKey,
+			},
+			json: true,
+		};
+
+		return rp(settings)
+			.then((res) => {
+				return res;
+			})
+			.catch((err) => {
+				return Promise.reject(new Error(`Could not retrieve Boards from Projcet - ${err.error.msg}`));
+			});
+	}
+
+	getBoard(boardId) {
+		const settings = {
+			method: 'GET',
+			uri: `${this.url}boards/${boardId}`,
+			qs: { token: this.apiKey },
+			json: true,
+		};
+
+		return rp(settings)
+			.then((res) => {
+				return res;
+			})
+			.catch((err) => {
+				return Promise.reject(new Error(`Could not retrieve Board - ${err.error.msg}`));
+			});
+	}
+
+	createBoard(title, description, project, email) {
+		const settings = {
+			method: 'POST',
+			uri: `${this.url}boards`,
+			qs: { token: this.apiKey },
+			body: {
+				title,
+				description,
+				email,
+				projectId: project,
+			},
+			headers: { "Content-Type": 'application/json' },
+			json: true,
+		};
+
+		return rp(settings)
+			.then((res) => {
+				return res;
+			})
+			.catch((err) => {
+				return Promise.reject(new Error(`Could not create a new Board - ${err.error.msg}`));
+			});
+	}
+}
+
+module.exports = Nexboard;
