87747b9f6c542a95cb2ffc86e1013204a9458553
Alexander Weber <alexander.weber@capgemini.com>
2022-05-06 15:16:41 +0200

    changed check to array

--- controllers/tools.js
+++ controllers/tools.js
@@ -6,6 +6,8 @@ const api = require('../api');
 const authHelper = require('../helpers/authentication');
 const ltiCustomer = require('../helpers/ltiCustomer');
 
+const invisibleTools = ['SchulcloudNextcloud'];
+
 const createToolHandler = (req, res, next) => {
 	const context = req.originalUrl.split('/')[1];
 	api(req).post('/ltiTools/', {
@@ -31,7 +33,7 @@ const addToolHandler = (req, res, next) => {
 
 	api(req).get('/ltiTools', { qs: { isTemplate: true } })
 		.then((tools) => {
-			const filterTools = tools.data.filter((tool) => tool.name !== 'SchulcloudNextcloud');
+			const filterTools = tools.data.filter((tool) => !invisibleTools.includes(tool.name));
 			api(req).get(`/${context}/${req.params.courseId}`)
 				.then((course) => {
 					res.render('courses/add-tool', {
