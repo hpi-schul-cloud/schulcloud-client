1ea1800d11d2a56ff544a0a2e913b44ff61ae0a4
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-05-25 14:55:03 +0200

    Use existing secret and map it, secondy try.

--- api.js
+++ api.js
@@ -1,9 +1,8 @@
 const { Configuration } = require('@hpi-schul-cloud/commons');
 const api = require('./helpers/apiHelper');
 
-const xApiKey = Configuration.get('REQUEST_OPTION__CLIENT_API_KEY');
 const timeout = Configuration.get('REQUEST_OPTION__TIMEOUT_MS');
 const keepAlive = Configuration.get('REQUEST_OPTION__KEEP_ALIVE');
 const url = Configuration.get('API_HOST');
 
-module.exports = api(`${url}/`, { keepAlive, xApiKey, timeout });
+module.exports = api(`${url}/`, { keepAlive, timeout });
