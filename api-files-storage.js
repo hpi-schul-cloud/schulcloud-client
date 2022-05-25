d2794e3ad15887857840bcb484305abce1e9ce34
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-05-25 12:47:17 +0200

    Rename envirment variablen namesprace.

--- api-files-storage.js
+++ api-files-storage.js
@@ -1,7 +1,7 @@
 const { Configuration } = require('@hpi-schul-cloud/commons');
 const api = require('./helpers/apiHelper');
 
-const keepAlive = Configuration.get('API__KEEP_ALIVE');
+const keepAlive = Configuration.get('REQUEST_OPTION__KEEP_ALIVE');
 const url = Configuration.get('FILES_STORAGE__SERVICE_BASE_URL');
 const timeout = Configuration.get('FILES_STORAGE__INCOMING_REQUEST_TIMEOUT');
 
