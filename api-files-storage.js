099eec6244277eae0c9a47f211c5de6b3f10e8ef
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-05-25 12:37:29 +0200

    BC-1500 Rename enviements BACKEND_URL, REQUEST_TIMEOUT_MS, API_KEY.

--- api-files-storage.js
+++ api-files-storage.js
@@ -3,6 +3,6 @@ const api = require('./helpers/apiHelper');
 
 const keepAlive = Configuration.get('API__KEEP_ALIVE');
 const url = Configuration.get('FILES_STORAGE__SERVICE_BASE_URL');
-const timeout = Configuration.get('FILES_STORAGE__SERVICE_BASE_URL');
+const timeout = Configuration.get('FILES_STORAGE__INCOMING_REQUEST_TIMEOUT');
 
 module.exports = api(`${url}/api/`, { keepAlive, timeout });
--- api.js
+++ api.js
@@ -4,6 +4,6 @@ const api = require('./helpers/apiHelper');
 const xApiKey = Configuration.get('API__API_KEY');
 const timeout = Configuration.get('API__REQUEST_TIMEOUT_MS');
 const keepAlive = Configuration.get('API__KEEP_ALIVE');
-const url = Configuration.get('BACKEND_URL');
+const url = Configuration.get('API_HOST');
 
-module.exports = api(url, { keepAlive, xApiKey, timeout });
+module.exports = api(`${url}/`, { keepAlive, xApiKey, timeout });
