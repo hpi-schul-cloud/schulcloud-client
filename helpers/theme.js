1293410c4bcc1bd21053a693a121878bc9627b03
Odalys Adam <odalys.adam@dataport.de>
2022-08-05 10:09:18 +0200

    using title instead of short title

--- helpers/theme.js
+++ helpers/theme.js
@@ -2,19 +2,16 @@ const { defaultDocuments } = require('../config/documents');
 const {
 	SC_THEME,
 	SC_TITLE,
-	SC_SHORT_TITLE,
 	HOST,
 	ALERT_STATUS_URL,
 } = require('../config/global');
 
-const instanceSpecificTitle = (short = false) => {
+const instanceSpecificTitle = () => {
 	switch (SC_THEME) {
 		case 'n21':
 			return 'Niedersächsischen Bildungscloud';
-		case 'brb':
-			return 'Schul-Cloud Brandenburg';
 		default:
-			return short ? SC_SHORT_TITLE : SC_TITLE;
+			return SC_TITLE;
 	}
 };
 
@@ -35,13 +32,11 @@ const setTheme = (res) => {
 	const documents = defaultDocuments();
 	const baseDir = (res.locals.currentSchoolData || {}).documentBaseDir || documents.documentBaseDir;
 	const themeTitle = instanceSpecificTitle();
-	const shortThemeTitle = instanceSpecificTitle(true);
 	const instituteTitle = instanceInstitute();
 
 	res.locals.theme = {
 		name: SC_THEME,
 		title: SC_TITLE,
-		short_title: shortThemeTitle,
 		theme_title: themeTitle,
 		institute_title: instituteTitle,
 		documents: {
