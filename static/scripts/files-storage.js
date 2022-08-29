68a97fa2ccc53fbe7e298a4de45dcf4847f9ff8a
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-08-29 12:37:08 +0200

    fixup! fixup! BC-2290 - change redirect logic

--- static/scripts/files-storage.js
+++ static/scripts/files-storage.js
@@ -186,7 +186,8 @@ $(document).ready(() => {
 						$progressBar.fadeOut(50, () => {
 							$form.fadeIn(50);
 							window.localStorage.setItem('afterUploadFiles', 'true');
-							$('#homework-form').find('input[name="referrer"]').val(window.location.pathname + window.location.search);
+							$('#homework-form').find('input[name="referrer"]')
+								.val(window.location.pathname + window.location.search);
 							$('#homework-form').trigger('submit');
 						});
 						progressBarActive = false;
