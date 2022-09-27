68e836fc5136d314d68593133ae7c16681193a07
HeftyTea <timm.hoermann@capgemini.com>
2022-09-27 16:20:58 +0200

    EW-224 Re-introduce OAUTH login flag in client

--- controllers/login.js
+++ controllers/login.js
@@ -17,24 +17,24 @@ const {
 } = require('../helpers');
 const { LoginSchoolsCache } = require('../helpers/cache');
 
-Handlebars.registerHelper('oauthLink', (system) => {
+Handlebars.registerHelper('oauthLink', (oauthConfig, alias) => {
 	const encodedURI = [
-		system.oauthConfig.authEndpoint,
+		oauthConfig.authEndpoint,
 		'?client_id=',
-		system.oauthConfig.clientId,
+		oauthConfig.clientId,
 		'&redirect_uri=',
-		system.oauthConfig.redirectUri,
+		oauthConfig.redirectUri,
 		'&response_type=',
-		system.oauthConfig.responseType,
+		oauthConfig.responseType,
 		'&scope=',
-		system.oauthConfig.scope,
+		oauthConfig.scope,
 	].join('');
 	// provider works for now, but maybe not the best differentiating feature in the future
-	if (system.oauthConfig.provider === 'oauth') {
+	if (oauthConfig.provider === 'oauth') {
 		return encodeURI([
 			encodedURI,
 			'&kc_idp_hint=',
-			system.alias,
+			alias,
 		].join(''));
 	}
 	return encodeURI([encodedURI]);
@@ -132,15 +132,22 @@ router.all('/', async (req, res, next) => {
 		redirectAuthenticated(req, res);
 	} else {
 		const schools = await LoginSchoolsCache.get(req);
+		if (Configuration.get('FEATURE_OAUTH_LOGIN_ENABLED') === true) {
+			const oauthSystems = await getOauthSystems(req);
 
-		const oauthSystems = await getOauthSystems(req);
-
-		res.render('authentication/home', {
-			schools: getNonOauthSchools(schools),
-			systems: [],
-			oauthSystems: oauthSystems.data || [],
-			inline: true,
-		});
+			res.render('authentication/home', {
+				schools: getNonOauthSchools(schools),
+				systems: [],
+				oauthSystems: oauthSystems.data || [],
+				inline: true,
+			});
+		} else {
+			res.render('authentication/home', {
+				schools,
+				inline: true,
+				systems: [],
+			});
+		}
 	}
 });
 
@@ -168,34 +175,44 @@ const renderLogin = async (req, res) => {
 	const schools = await LoginSchoolsCache.get(req);
 	const redirect = redirectHelper.getValidRedirect(req.query && req.query.redirect ? req.query.redirect : '');
 
-	let oauthErrorLogout = false;
-	if (req.query.error) {
-		res.locals.notification = {
-			type: 'danger',
-			message: res.$t(mapErrorCodeToTranslation(req.query.error)),
-		};
-		if (req.query.provider === 'iserv' && req.query.error !== 'sso_oauth_access_denied') {
-			oauthErrorLogout = true;
+
+	if (Configuration.get('FEATURE_OAUTH_LOGIN_ENABLED') === true) {
+		let oauthErrorLogout = false;
+		if (req.query.error) {
+			res.locals.notification = {
+				type: 'danger',
+				message: res.$t(mapErrorCodeToTranslation(req.query.error)),
+			};
+			if (req.query.provider === 'iserv' && req.query.error !== 'sso_oauth_access_denied') {
+				oauthErrorLogout = true;
+			}
+			const strategyOfSchool = req.query.strategy;
+			const idOfSchool = req.query.schoolId;
+			const oauthSystems = await api(req, { version: 'v3' })
+				.get('/system?onlyOauth=true')
+				.catch((err) => logger.error('error loading oauth system list', formatError(err)));
+
+			res.render('authentication/login', {
+				schools: getNonOauthSchools(schools),
+				systems: [],
+				oauthSystems: oauthSystems.data || [],
+				oauthErrorLogout,
+				hideMenu: true,
+				redirect,
+				idOfSchool,
+				strategyOfSchool,
+			});
+
+		} else {
+			res.render('authentication/login', {
+				schools,
+				systems: [],
+				hideMenu: true,
+				redirect,
+			});
 		}
-	}
+	};
 
-	const strategyOfSchool = req.query.strategy;
-	const idOfSchool = req.query.schoolId;
-
-	const oauthSystems = await api(req, { version: 'v3' })
-		.get('/system?onlyOauth=true')
-		.catch((err) => logger.error('error loading oauth system list', formatError(err)));
-
-	res.render('authentication/login', {
-		schools: getNonOauthSchools(schools),
-		systems: [],
-		oauthSystems: oauthSystems.data || [],
-		oauthErrorLogout,
-		hideMenu: true,
-		redirect,
-		idOfSchool,
-		strategyOfSchool,
-	});
 };
 
 router.get('/loginRedirect', (req, res, next) => {
