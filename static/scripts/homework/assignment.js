be8b64378ae0d15f78b8d00db93b3927ce705c73
Tomasz Wiaderek <t.wiaderwek@gmail.com>
2022-12-16 18:05:58 +0100

    BC-1558 fix homework redirect

--- static/scripts/homework/assignment.js
+++ static/scripts/homework/assignment.js
@@ -8,6 +8,18 @@ window.addEventListener('load', () => {
 		$('.usersubmission td:last-of-type i.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
 		$('.usersubmission.active td:last-of-type i.fa-chevron-down')
 			.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+
+		const userInfoId = $(this).attr('id');
+		const urlWithParams = window.location.href.replace('#', '?');
+		const actualId = new URL(urlWithParams).searchParams.get('userInfo');
+		const url = window.location.pathname;
+		if (actualId) {
+			if (actualId === userInfoId) {
+				window.location = `${url}#activetabid=submissions`;
+			}
+		} else if (userInfoId) {
+			window.location = `${url}#activetabid=submissions&userInfo=${userInfoId}`;
+		}
 	});
 
 	// change tab
@@ -19,18 +31,20 @@ window.addEventListener('load', () => {
 		range.find(`>.tabs>#${this.id.replace('-tab-link', '')}.tab-content`).addClass('active');
 	});
 
-	// set initial tab-view by URL-Parameter
-	function getParameterByName() {
-		return decodeURIComponent(window.location.hash.replace('#activetabid=', ''));
-	}
-
-	if (getParameterByName('activetabid')) {
-		const id = getParameterByName('activetabid');
+	const urlWithParams = window.location.href.replace('#', '?');
+	if (new URL(urlWithParams).searchParams.get('activetabid')) {
+		const id = new URL(urlWithParams).searchParams.get('activetabid');
 		const range = $(`#${id}`).closest('.tab-view');
 		range.find('>.tab-links .tab-link.active').removeClass('active');
 		range.find('>.tabs>.tab-content.active').removeClass('active');
 
 		range.find(`>.tab-links>#${id}-tab-link`).addClass('active');
 		$(`#${id}`).addClass('active');
+
+		if (new URL(urlWithParams).searchParams.get('userInfo')) {
+			const userInfoId = new URL(urlWithParams).searchParams.get('userInfo');
+
+			$(`#${userInfoId}`).addClass('active');
+		}
 	}
 });
