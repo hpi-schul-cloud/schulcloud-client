307fece6b94da821739a60748469f2edf04a913d
Tomasz Wiaderek <t.wiaderwek@gmail.com>
2023-01-19 19:13:56 +0100

    remove console log

--- controllers/users.js
+++ controllers/users.js
@@ -38,7 +38,6 @@ router.patch('/language', async (req, res, next) => {
 		setCookie(res, 'USER_LANG', language);
 		return res.json({});
 	} catch (err) {
-		console.log('eldorado');
 		return next(err);
 	}
 });
