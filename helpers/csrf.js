0b90c61d4d726e33b7e40f846c1e8a41afef1efb
Viktoriia <1>
2023-02-20 11:06:51 +0100

    Add clear cookies

--- helpers/csrf.js
+++ helpers/csrf.js
@@ -7,13 +7,13 @@ const tokenInjector = (req, res, next) => {
 };
 
 const duplicateTokenHandler = (req, res, next) => {
-	if (req.body && req.body._csrf && Array.isArray(req.body._csrf)) {
+	if (req.body && Array.isArray(req.body._csrf)) {
 		const allArrayItemsIdentical = req.body._csrf.every((token) => token === req.body._csrf[0]);
 		if (!allArrayItemsIdentical) {
-		  // eslint-disable-next-line max-len
-		  const error = new Error('Bei der Anfrage wurden mehrere Sicherheitstokens (CSRF) mitgesendet. Bitte probiere es erneut.');
-		  error.status = 400;
-		  return next(error);
+			// eslint-disable-next-line max-len
+			const error = new Error('Bei der Anfrage wurden mehrere Sicherheitstokens (CSRF) mitgesendet. Bitte probiere es erneut.');
+			error.status = 400;
+			return next(error);
 		}
 		logger.warn('Die Anfrage enthält mehrere identische Sicherheitstokens (CSRF).');
 
@@ -24,20 +24,21 @@ const duplicateTokenHandler = (req, res, next) => {
 
 const csrfErrorHandler = (err, req, res, next) => {
 	if (err.code === 'EBADCSRFTOKEN') {
-	  // convert body object to array
-	  res.locals.csrfToken = req.csrfToken();
-	  // send base URL for opening in new tab
-	  const baseUrl = req.headers.origin;
-	  const values = Object.keys(req.body).map((name) => ({ name, value: req.body[name] }));
-	  values.push({
-		name: 'csrfErrorcount',
-		value: req.body.csrfErrorcount || 0,
-	  });
-	  // show only a reload button if not all data is present
-	  const simpleView = !baseUrl || values.length === 0;
-	  res.render('lib/csrf', {
-		loggedin: res.locals.loggedin,
-		values,
+		res.clearCookie('XSRF-TOKEN');
+		// convert body object to array
+		res.locals.csrfToken = req.csrfToken();
+		// send base URL for opening in new tab
+		const baseUrl = (req.headers.origin);
+		const values = Object.keys(req.body).map((name) => ({ name, value: req.body[name] }));
+		values.push({
+			name: 'csrfErrorcount',
+			value: '1',
+		});
+		// show only a reload button if not all data is present
+		const simpleView = (!baseUrl || !values);
+		res.render('lib/csrf', {
+			loggedin: res.locals.loggedin,
+			values,
 			previousError: (req.body.csrfErrorcount),
 			baseUrl,
 			simpleView,
@@ -52,4 +53,3 @@ module.exports = {
 	duplicateTokenHandler,
 	csrfErrorHandler,
 };
-F
