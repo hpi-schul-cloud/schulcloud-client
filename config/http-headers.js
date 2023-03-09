207a5808aef9233f48bb365c1cfcde1986857906
Phillip Wirth <phillip.wirth@dataport.de>
2023-03-09 11:12:36 +0100

    more liniting fixes in useless file

--- config/http-headers.js
+++ config/http-headers.js
@@ -21,10 +21,10 @@ const config = {
 		// Default Content-Security-Policy Header for every site
 		// Use 'strict-dynamic' 'nonce-<nonceValue>' (nonceValue auto generated) to create a whitelist
 		corsDefault: {
-			defaultSrc: `'self' data: blob: wss://dbildungscloud.de wss://scchat.dbildungscloud.de https://api.dbildungscloud.de https://scchat.dbildungscloud.de https://s3.hidrive.strato.com https://libreoffice.dbildungscloud.de https://docs.dbildungscloud.de https://etherpad.dbildungscloud.de https://blog.niedersachsen.cloud https://blog.dbildungscloud.de https://sc-content-resources.hpi-schul-cloud.de https://open.hpi.de https://upload.wikimedia.org`,
-			fontSrc: `'self' data:`,
-			styleSrc: `'self' 'unsafe-inline'`,
-			scriptSrc: `'self' 'unsafe-eval'`,
+			defaultSrc: '\'self\' data: blob: wss://dbildungscloud.de wss://scchat.dbildungscloud.de https://api.dbildungscloud.de https://scchat.dbildungscloud.de https://s3.hidrive.strato.com https://libreoffice.dbildungscloud.de https://docs.dbildungscloud.de https://etherpad.dbildungscloud.de https://blog.niedersachsen.cloud https://blog.dbildungscloud.de https://sc-content-resources.hpi-schul-cloud.de https://open.hpi.de https://upload.wikimedia.org',
+			fontSrc: '\'self\' data:',
+			styleSrc: '\'self\' \'unsafe-inline\'',
+			scriptSrc: '\'self\' \'unsafe-eval\'',
 			// Please activate for production
 			// upgradeInsecureRequestsSrc: 'upgrade-insecure-requests',
 			// blockAllMixedContentSrc: 'block-all-mixed-content',
