6aac4311ea18c4e91032197e4876a78b27659877
Nicole Friedo <nicole.friedo@dataport.de>
2023-04-13 17:00:55 +0200

    revert link change

--- helpers/handlebars/middleware.js
+++ helpers/handlebars/middleware.js
@@ -271,7 +271,9 @@ module.exports = (req, res, next) => {
 				icon: // eslint-disable-next-line max-len
 				'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M22 22V11H17V2L2 7V22H22ZM4 8.3736L15 4.91206V11H10V20H4V8.3736ZM15 20H12V13H20V20H17V17H15V20Z"/></svg>',
 				isExternalIcon: true,
-				link: '/administration/school/',
+				link: newSchoolAdminPageAsDefault
+					? '/administration/school-settings/'
+					: '/administration/school/',
 			},
 		],
 	});
