d8d48af6ed703e8528fa23b2fe15a97dae5ad5a9
Michael Linares <michaellinaresdbildungs@gmail.com>
2023-05-02 18:56:11 +0200

    Added functionality to display status events.

--- static/scripts/helpers/AlertMessageController.js
+++ static/scripts/helpers/AlertMessageController.js
@@ -1,14 +1,14 @@
+/* eslint-disable max-len */
 const datetime = require('../datetime/datetime');
-
-const max = 2;
 
 function getIconTag(status) {
 	switch (status) {
 		case 'danger':
 			$('.alert-button').addClass('danger');
 			return '<i class="fa fa-exclamation-circle text-danger"></i>';
-		case 'done':
-			return '<i class="fa fa-check-circle text-success"></i>';
+		case 'info':
+			$('.alert-button').addClass('info');
+			return '<i class="fa fa-info-circle text-info"></i>';
 		default:
 			return '<i class="fa fa-info-circle text-info"></i>';
 	}
@@ -23,110 +23,48 @@ class AlertMessageController {
 	buildMessage(message) {
 		const icon = getIconTag(message.status);
 
-		// show only 150 charckters of message
-		let messageText;
-		if (message.text.length > 113) {
-			messageText = `${message.text.substring(0, 113)}...`;
-		} else {
-			messageText = message.text;
-		}
-
-		// if message includes url
-		let url = '';
-		if (message.url) {
-			url = `
-				<a href="${message.url}" rel="noopener" target="_blank" style="float: right;">
-					${message.url.replace(/(^\w+:|^)\/\//, '')}
-				</a>
-				`;
-		}
-
 		const item = document.createElement('div');
 		if (this.loggedin) {
 			item.className = 'alert-item';
 			item.innerHTML = `
-			<div class="alert-date text-nowrap text-muted">
-					${datetime.fromNow(message.timestamp)}
-			</div>
 			<div class="alert-title">${icon} ${message.title}</div>
 			${message.text}
-			${url}
+			<br>
+			<div class="alert-date text-nowrap text-muted" style="float: left;">Updated: ${datetime.fromNow(message.timestamp)} <span style="margin-right: 2px;"></span>|</div>
+			<div class="alert-date text-nowrap text-muted" style="float: left; margin-left: 5px;">Created: ${datetime.toDateTimeString(message.createdAt)}</div>
 			<div style="clear: both;"></div>`;
 		} else {
 			item.className = 'alert alert-info alert-card';
 			item.innerHTML = `<h6 style="overflow: hidden; text-overflow: ellipsis;">${icon} ${message.title}</h6>
-			<div class="text-muted" style="float: left;">${datetime.toDateTimeString(message.timestamp)}</div> <br>
-			${messageText}
-			${url}
+			${message.text}
+			<br>
+			<div class="text-muted" style="float: left;">Updated: ${datetime.toDateTimeString(message.timestamp)}<span style="margin-right: 2px;"></span>|</div>
+			<div class="text-muted" style="float: left; margin-left: 5px;">Created: ${datetime.toDateTimeString(message.createdAt)}</div>
 			<div style="clear: both;"></div>`;
-		}
-		return item;
-	}
-
-	readMore(length, url) {
-		const item = document.createElement('div');
-		let text = '';
-
-		if (length > 1) {
-			text = $t('alert.text.furtherCases', { amount: length });
-		} else {
-			text = $t('alert.text.furtherCase', { amount: length });
-		}
-
-		if (this.loggedin) {
-			item.className = 'alert-item text-center';
-			item.innerHTML = `
-			<a href="${url}" rel="noopener" target="_blank">
-				${text}
-			</a>`;
-		} else {
-			item.className = 'alert alert-info alert-card';
-			item.innerHTML = `
-			<a href="${url}" rel="noopener" target="_blank">
-				${text}
-			</a>`;
 		}
 		return item;
 	}
 
 	showAlert(messageArray) {
 		if (Array.isArray(messageArray)) {
+			if (!messageArray || messageArray.length === 0) {
+				return; // If messageArray is empty, we do not show the triangle
+			}
 			// keep data in local storage
 			// silently overwrite old data
 			localStorage.setItem('SC-Alerts', JSON.stringify(messageArray));
 			if (this.loggedin) {
 				$('.alert-button').css('visibility', 'hidden');
-				if (messageArray.length >= 1) {
+				if (messageArray) {
 					if (!$('body').hasClass('fullscreen')) {
 						$('.alert-button').css('visibility', 'visible');
 					}
 					$('.alert-button').find('.js-alert-content').empty();
-					messageArray.forEach((message, index) => {
-						if (index < max) {
+					messageArray.forEach((message) => {
+						if (message) {
 							$('.alert-button').find('.js-alert-content').append(this.buildMessage(message));
 						}
 					});
-				}
-			} else {
-				$('.alert-section').empty();
-				if (messageArray.length >= 1) {
-					messageArray.forEach((message, index) => {
-						if (message.status === 'danger' && index < max) {
-							$('.alert-section').append(this.buildMessage(message));
-						}
-					});
-				}
-			}
-			const { length } = messageArray.filter(message => message.status === 'danger');
-			if (messageArray.length > max) {
-				if (this.loggedin) {
-					$('.alert-button').find('.js-alert-content').append(
-						this.readMore(messageArray.length - max, messageArray[max].url),
-					);
-				} else if (length != 0 && length > max) {
-					$('.alert-section').append(
-						this.readMore(length - max, messageArray[max].url),
-					);
 				}
 			}
 		}
