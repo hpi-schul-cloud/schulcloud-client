184edada96733fafebd27649cd66abbd36307987
Max Bischof <maximilian.bischof@dataport.de>
2023-05-04 08:53:29 +0200

    Add error handling for create homework

--- static/scripts/helpers/homework.js
+++ static/scripts/helpers/homework.js
@@ -5,25 +5,29 @@ export async function createHomework(parentType, dropzone) {
 
 	const submissionUrl = isSubmissionFile ? '/submit' : '';
 	const url = `/homework${submissionUrl}/create`;
-	const entity = await $.ajax({
-		url,
-		type: 'post',
-		data: form.serialize(),
-	});
-
-	form.attr('action', `/homework${submissionUrl}/${entity._id}`);
-	$('[name="_method"]').val('patch');
-
-	if (isSubmissionFile) {
-		form.addClass(entity._id);
-	} else {
-		$('#name').val(entity.name);
-		$('#availableDate').val(entity.availableDate);
-
-		const editUrl = dropzone ? `/edit?returnUrl=homework/${entity._id}` : '';
-		const referrer = `/homework/${entity._id}${editUrl}`;
-		$('[name="referrer"]').val(referrer);
+	try{
+		const entity = await $.ajax({
+			url,
+			type: 'post',
+			data: form.serialize(),
+		});
+	
+		form.attr('action', `/homework${submissionUrl}/${entity._id}`);
+		$('[name="_method"]').val('patch');
+	
+		if (isSubmissionFile) {
+			form.addClass(entity._id);
+		} else {
+			$('#name').val(entity.name);
+			$('#availableDate').val(entity.availableDate);
+	
+			const editUrl = dropzone ? `/edit?returnUrl=homework/${entity._id}` : '';
+			const referrer = `/homework/${entity._id}${editUrl}`;
+			$('[name="referrer"]').val(referrer);
+		}
+	
+		return entity._id;
+	} catch (error) {
+		window.location.href = '/error';
 	}
-
-	return entity._id;
 }
