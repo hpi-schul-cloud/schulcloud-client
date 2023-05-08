3169816a1683916a7ddb2f33d9e316a8c723072a
Max Bischof <maximilian.bischof@dataport.de>
2023-05-08 11:50:48 +0200

    On submit return to task details view

--- controllers/homework.js
+++ controllers/homework.js
@@ -190,7 +190,7 @@ const getCreateHandler = (service) => (req, res, next) => {
 		}
 
 		if (referrer === 'tasks' || referrer.includes('rooms')) {
-			referrer = `homework/${data._id}/edit?returnUrl=homework/${data._id}`;
+			referrer = `homework/${data._id}`;
 		}
 		const url = new URL(referrer, base);
 		res.redirect(url);
