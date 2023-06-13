c10c33709a0980ae35941baf50b13a41d2096590
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2023-06-13 12:02:40 +0200

    fix consent

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -81,28 +81,45 @@ const displayScope = (scope, w) => {
 	}
 };
 
-router.get('/consent', csrfProtection, auth.authChecker, (r, w, next) => {
+router.get('/consent', csrfProtection, auth.authChecker, (req, res, next) => {
 	// This endpoint is hit when hydra initiates the consent flow
-	if (r.query.error) {
+	if (req.query.error) {
 		// An error occurred (at hydra)
-		return w.send(`${r.query.error}<br />${r.query.error_description}`);
+		return res.send(`${req.query.error}<br />${req.query.error_description}`);
 	}
-	return api(r, { version: VERSION }).get(`/oauth2/consentRequest/${r.query.consent_challenge}`)
-		.then((consentRequest) => {
-			if (consentRequest.skip) {
-				return acceptConsent(r, w, r.query.consent_challenge, consentRequest.requested_scope);
+	return api(req, { version: VERSION }).get(`/oauth2/consentRequest/${req.query.consent_challenge}`)
+		.then(async (consentRequest) => {
+			const clientId = consentRequest.client.client_id;
+
+			const [ltiTools, ctlTools] = await Promise.all([
+				api(req).get(`/ltiTools?oAuthClientId=${clientId}&isLocal=true`),
+				api(req, { version: 'v3' }).get(`/tools?clientId=${clientId}`),
+			]);
+
+			let skipConsent = false;
+			if (ctlTools.data && ctlTools.data.length === 1) {
+				({ skipConsent } = ctlTools.data[0]);
+			} else if (ltiTools.data && ltiTools.data.length === 1) {
+				({ skipConsent } = ltiTools.data[0]);
+			} else {
+				throw new Error(`Unable to find tool with clientId ${clientId} for consent`);
 			}
-			return w.render('oauth2/consent', {
+
+			if (consentRequest.skip || skipConsent) {
+				return acceptConsent(req, res, req.query.consent_challenge, consentRequest.requested_scope);
+			}
+
+			return res.render('oauth2/consent', {
 				inline: true,
-				title: w.$t('login.oauth2.headline.loginWithSchoolCloud', {
-					title: w.locals.theme.title,
+				title: res.$t('login.oauth2.headline.loginWithSchoolCloud', {
+					title: res.locals.theme.title,
 				}),
 				subtitle: '',
 				client: consentRequest.client.client_name,
-				action: `/oauth2/consent?challenge=${r.query.consent_challenge}`,
-				buttonLabel: w.$t('global.button.accept'),
+				action: `/oauth2/consent?challenge=${req.query.consent_challenge}`,
+				buttonLabel: res.$t('global.button.accept'),
 				scopes: consentRequest.requested_scope.map((scope) => ({
-					display: displayScope(scope, w),
+					display: displayScope(scope, res),
 					value: scope,
 				})),
 			});
