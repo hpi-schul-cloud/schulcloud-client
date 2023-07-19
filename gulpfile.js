74e80440892e922c503f66456b6a4108a9c1ef62
Michael Linares <michaellinaresdbildungs@gmail.com>
2023-07-19 22:32:02 +0200

    Added margin-top to status event triangle.

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ const baseStyles = [
 ];
 
 function themeName() {
-	return process.env.SC_THEME || 'default';
+	return process.env.SC_THEME || 'brb';
 }
 
 const EXIT_ON_ERROR = process.env.GULP_EXIT_ON_ERROR
