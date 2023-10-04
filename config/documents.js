71a73d442cf7fa8ae93f27cfc9a99224b6100201
odalys-dataport <82401838+odalys-dataport@users.noreply.github.com>
2023-10-04 14:39:56 +0200

    BC-4745 - change privacy policy file name for thr/tsc (#3321)

--- config/documents.js
+++ config/documents.js
@@ -5,7 +5,9 @@ const { DOCUMENT_BASE_DIR, SC_THEME } = require('./global');
 const specificFiles = {
 	accessibilityStatement: 'Willkommensordner/Barrierefreiheit/Barrierefreiheitserklaerung.pdf',
 	privacyExemplary: 'Onlineeinwilligung/Datenschutzerklaerung-Muster-Schulen-Onlineeinwilligung.pdf',
-	privacy: 'Onlineeinwilligung/Datenschutzerklaerung-Onlineeinwilligung.pdf',
+	privacy: SC_THEME === 'thr'
+		? 'Onlineeinwilligung/Datenschutzhinweise.pdf'
+		: 'Onlineeinwilligung/Datenschutzerklaerung-Onlineeinwilligung.pdf',
 	termsOfUseExemplary: 'Onlineeinwilligung/Nutzungsordnung-HPI-Schule-Schueler-Onlineeinwilligung.pdf',
 	termsOfUse: 'Onlineeinwilligung/Nutzungsordnung-Onlineeinwilligung.pdf',
 	termsOfUseSchool: SC_THEME === 'thr'
