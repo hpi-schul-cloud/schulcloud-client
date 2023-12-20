08b727316cd1c7eaab0933fbb02d8b6c49c354d3
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-12-20 12:12:01 +0100

    BC-6040 - another fix for `connectRedis` error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const redis = require('redis');
-const { RedisStore } = require('connect-redis');
+const connectRedis = require('connect-redis');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const csurf = require('csurf');
@@ -123,6 +123,7 @@ let sessionStore;
 const redisUrl = REDIS_URI;
 if (redisUrl) {
 	logger.info(`Using Redis session store at '${redisUrl}'.`);
+	const RedisStore = connectRedis.default;
 	const client = redis.createClient({
 		url: redisUrl,
 	});
