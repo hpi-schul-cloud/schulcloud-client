b89ed71ff2ba95aee0145d0f60ffe2166886b1f7
dyedwiper <maximilian.becker@dataport.de>
2024-01-08 11:23:04 +0100

    Update import of RedisStore

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const redis = require('redis');
-const connectRedis = require('connect-redis');
+const RedisStore = require('connect-redis').default;
 const session = require('express-session');
 const methodOverride = require('method-override');
 const csurf = require('csurf');
@@ -123,7 +123,6 @@ let sessionStore;
 const redisUrl = REDIS_URI;
 if (redisUrl) {
 	logger.info(`Using Redis session store at '${redisUrl}'.`);
-	const RedisStore = connectRedis.default;
 	const client = redis.createClient({
 		url: redisUrl,
 	});
