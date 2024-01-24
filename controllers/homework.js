1ad03b098d2541be3b7d11ff039099679aae9daa
Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>
2024-01-24 17:58:41 +0100

    remove creator ID filtering (that allows only the teachers and submitters to see files created by them) in homeworks

--- controllers/homework.js
+++ controllers/homework.js
@@ -595,11 +595,9 @@ router.get('/:assignmentId', (req, res, next) => {
 			}
 
 			const submissionFilesStorageData = _.clone(filesStorage);
-			submissionFilesStorageData.files = filesStorage.files.filter((file) => submitters.has(file.creatorId));
 			submissionFilesStorageData.readonly = readonly || (!isCreator && isTeacher);
 
 			const gradeFilesStorageData = _.clone(filesStorage);
-			gradeFilesStorageData.files = filesStorage.files.filter((file) => teachers.has(file.creatorId));
 			gradeFilesStorageData.readonly = !isTeacher;
 
 			submission.submissionFiles = { filesStorage: submissionFilesStorageData };
