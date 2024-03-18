60d0c4a470b253d474e44a7f7d65f1460cbfea35
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-03-18 16:46:16 +0100

    fix query

--- controllers/courses.js
+++ controllers/courses.js
@@ -209,8 +209,8 @@ const editCourseHandler = (req, res, next) => {
 
 	let syncedWithGroup;
 	let groupPromise;
-	if (req.params.groupId && Configuration.get('FEATURE_SCHULCONNEX_COURSE_SYNC_ENABLED')) {
-		syncedWithGroup = req.params.groupId;
+	if (Configuration.get('FEATURE_SCHULCONNEX_COURSE_SYNC_ENABLED') && req.query.syncWithGroup) {
+		syncedWithGroup = req.query.syncWithGroup;
 		groupPromise = api(req, { version: 'v3' }).get(`/groups/${syncedWithGroup}`);
 	}
 
