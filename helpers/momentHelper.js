4294db4202a7ba97b339ef93cb79c30c3f0650ce
WojciechGrancow <116577704+WojciechGrancow@users.noreply.github.com>
2024-04-08 13:00:33 +0200

    BC-6809 - modify notification about news publication (#3433)
    * modify view in news, add helper
    
    * implement whole disgn required by UX

--- helpers/momentHelper.js
+++ helpers/momentHelper.js
@@ -23,36 +23,44 @@ const relativeTime = {
 };
 */
 
-const createCustomRelativeTimeConfig = (localFile) => ({
-	d: `${localFile['moment.relativeTime.aDay']}`,
-	dd: `%d ${localFile['moment.relativeTime.days']}`,
-	future: `${localFile['moment.relativeTime.futureIn']} %s`,
-	h: `${localFile['moment.relativeTime.anHour']}`,
-	hh: `%d ${localFile['moment.relativeTime.hours']}`,
-	m: `${localFile['moment.relativeTime.aMinute']}`,
-	M: `${localFile['moment.relativeTime.aMonths']}`,
-	mm: `%d ${localFile['moment.relativeTime.minutes']}`,
-	MM: `%d ${localFile['moment.relativeTime.months']}`,
-	past: `%s ${localFile['moment.relativeTime.pastAgo']}`,
-	s: `${localFile['moment.relativeTime.aFewSecondes']}`,
-	ss: `%d ${localFile['moment.relativeTime.seconds']}`,
-	w: `${localFile['moment.relativeTime.aWeek']}`,
-	ww: `%d ${localFile['moment.relativeTime.weeks']}`,
-	y: `${localFile['moment.relativeTime.aYear']}`,
-	yy: `%d ${localFile['moment.relativeTime.years']}`,
-});
+const createCustomRelativeTimeConfig = (localFile, lang) => {
+	const commonConfig = {
+		d: `${localFile['moment.relativeTime.aDay']}`,
+		dd: `%d ${localFile['moment.relativeTime.days']}`,
+		future: `${localFile['moment.relativeTime.futureIn']} %s`,
+		h: `${localFile['moment.relativeTime.anHour']}`,
+		hh: `%d ${localFile['moment.relativeTime.hours']}`,
+		m: `${localFile['moment.relativeTime.aMinute']}`,
+		M: `${localFile['moment.relativeTime.aMonths']}`,
+		mm: `%d ${localFile['moment.relativeTime.minutes']}`,
+		MM: `%d ${localFile['moment.relativeTime.months']}`,
+		past: `%s ${localFile['moment.relativeTime.pastAgo']}`,
+		s: `${localFile['moment.relativeTime.aFewSecondes']}`,
+		ss: `%d ${localFile['moment.relativeTime.seconds']}`,
+		w: `${localFile['moment.relativeTime.aWeek']}`,
+		ww: `%d ${localFile['moment.relativeTime.weeks']}`,
+		y: `${localFile['moment.relativeTime.aYear']}`,
+		yy: `%d ${localFile['moment.relativeTime.years']}`,
+	};
+
+	if (lang === 'de' || lang === 'es') {
+		commonConfig.past = `${localFile['moment.relativeTime.pastAgo']} %s`;
+	}
+
+	return commonConfig;
+};
 
 const selectMomentOptions = (langAttribute) => {
 	const options = {};
 
 	if (langAttribute === 'uk') {
-		options.relativeTime = createCustomRelativeTimeConfig(uk);
+		options.relativeTime = createCustomRelativeTimeConfig(uk, 'uk');
 	}
 	if (langAttribute === 'es') {
-		options.relativeTime = createCustomRelativeTimeConfig(es);
+		options.relativeTime = createCustomRelativeTimeConfig(es, 'es');
 	}
 	if (langAttribute === 'de') {
-		options.relativeTime = createCustomRelativeTimeConfig(de);
+		options.relativeTime = createCustomRelativeTimeConfig(de, 'de');
 	}
 
 	return options;
