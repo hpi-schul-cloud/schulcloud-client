32289e8e58425c955618fbc6b9be42eca1ef511a
MartinSchuhmacher <mschuhmacher@gmx.de>
2024-07-01 13:10:09 +0200

    change position of release notes sidebar item

--- helpers/handlebars/middleware.js
+++ helpers/handlebars/middleware.js
@@ -400,12 +400,6 @@ module.exports = (req, res, next) => {
 			});
 		}
 
-		systemLinks.push({
-			name: res.$t('lib.help_menu.link.releaseNotes'),
-			link: '/help/releases',
-			testId: 'releases',
-		});
-
 		if (SC_THEME !== 'default') {
 			systemLinks.push({
 				link: res.locals.theme.documents.specificFiles.accessibilityStatement,
@@ -414,6 +408,12 @@ module.exports = (req, res, next) => {
 				isExternalLink: true,
 			});
 		}
+
+		systemLinks.push({
+			name: res.$t('lib.help_menu.link.releaseNotes'),
+			link: '/help/releases',
+			testId: 'releases',
+		});
 
 		if (SC_THEME !== 'n21') {
 			systemLinks.push({
