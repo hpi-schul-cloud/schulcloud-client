2ca9d5033e86c880184ff48415933b6e1e74639d
Marvin Öhlerking <103562092+MarvinOehlerkingCap@users.noreply.github.com>
2024-07-18 13:39:29 +0200

    N21-2057 Team invitations with duplicate emails (#3485)

--- controllers/users.js
+++ controllers/users.js
@@ -20,6 +20,34 @@ router.get('/teachersOfSchool', async (req, res, next) => {
 			_id: user._id,
 			firstName: user.firstName,
 			lastName: user.lastName,
+			schoolId: user.schoolId,
+			email: user.email,
+		}));
+
+		return res.json(result);
+	} catch (err) {
+		const error = new Error(res.$t('global.text.invalidRequest'));
+		error.status = 400;
+		return next(error);
+	}
+});
+
+router.get('/teachersWithEmail', async (req, res, next) => {
+	try {
+		const users = await api(req).get('/publicTeachers/', {
+			qs: {
+				$limit: false,
+				email: req.query.email,
+				$sort: 'firstName',
+				$populate: ['schoolId'],
+			},
+		});
+
+		const result = users.data.map((user) => ({
+			_id: user._id,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			schoolName: user.schoolId.name,
 		}));
 
 		return res.json(result);
