8ef6bfd8bf828f24adb1eabc2e001a55681037de
MartinSchuhmacher <mschuhmacher@gmx.de>
2024-08-14 11:38:37 +0200

    renaming ROOMS feature

--- helpers/handlebars/middleware.js
+++ helpers/handlebars/middleware.js
@@ -173,14 +173,14 @@ module.exports = (req, res, next) => {
 		});
 	}
 
-	// Spaces Feature Toggle
-	const spacesEnabled = Configuration.get('FEATURE_SPACES_ENABLED');
-	if (spacesEnabled) {
+	// Rooms Feature Toggle
+	const roomsEnabled = Configuration.get('FEATURE_ROOMS_ENABLED');
+	if (roomsEnabled) {
 		res.locals.sidebarItems.splice(1, 0, {
-			name: res.$t('global.sidebar.link.spaces'),
-			testId: 'Spaces',
+			name: res.$t('global.sidebar.link.rooms'),
+			testId: 'Rooms',
 			icon: 'account-supervisor-circle-outline',
-			link: '/spaces/',
+			link: '/rooms/',
 		});
 	}
 
@@ -350,7 +350,7 @@ module.exports = (req, res, next) => {
 	// team feature toggle
 	const teamsEnabled = FEATURE_TEAMS_ENABLED === 'true';
 	if (teamsEnabled) {
-		res.locals.sidebarItems.splice(spacesEnabled ? 3 : 2, 0, {
+		res.locals.sidebarItems.splice(roomsEnabled ? 3 : 2, 0, {
 			name: res.$t('global.link.teams'),
 			testId: 'Teams',
 			icon: 'account-group-outline',
