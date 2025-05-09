de2a4e26bbc6b54fb8089cc5da1ec5cea8e20f61
Gordon Nicholas <gordon.nicholas@capgemini.com>
2025-05-09 20:11:54 +0200

    N21-2453 optimize api call

--- static/scripts/topbar.js
+++ static/scripts/topbar.js
@@ -1,25 +1,20 @@
 $(document).ready(() => {
 	$('.user-menu-btn').click(() => {
-		getSessionTokenExpiration();
-	});
-
-	function getSessionTokenExpiration() {
 		const route = '/api/v3/oauth/session-token/expiration';
 
-		$.getJSON(route)
-			.done((response) => {
-				const isSessionTokenExpired = new Date() >= new Date(response.expiresAt);
-				if (isSessionTokenExpired) {
-					$('[data-testid=external-logout]').addClass("disabled");
-				} else {
-					$('[data-testid=external-logout]').removeClass("disabled");
-				}
-			})
-			.fail((response) => {
-				if (response.responseJSON && response.responseJSON.error) {
-					const err = response.responseJSON.error;
-					console.error(err.toString());
-				}
-			});
-	}
+		if ($('[data-testid=external-logout]').length > 0) {
+			$.getJSON(route)
+				.done((response) => {
+					const isSessionTokenExpired = new Date() >= new Date(response.expiresAt);
+					if (isSessionTokenExpired) {
+						$('[data-testid=external-logout]').addClass('disabled');
+					} else {
+						$('[data-testid=external-logout]').removeClass('disabled');
+					}
+				})
+				.fail(() => {
+					$('[data-testid=external-logout]').addClass('disabled');
+				});
+		}
+	});
 });
