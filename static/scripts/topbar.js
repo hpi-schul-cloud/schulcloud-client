a88174703bd1df81af501282a1be8824da36e2ad
Gordon Nicholas <gordon.nicholas@capgemini.com>
2025-05-12 08:51:16 +0200

    N21-2453 fix api call

--- static/scripts/topbar.js
+++ static/scripts/topbar.js
@@ -1,19 +1,23 @@
 $(document).ready(() => {
 	$('.user-menu-btn').click(() => {
-		const route = '/api/v3/oauth/session-token/expiration';
+		const isMenuClosing = $('.user-menu').hasClass('open');
+		if (isMenuClosing) {
+			return;
+		}
 
-		if ($('[data-testid=external-logout]').length > 0) {
+		const route = '/api/v3/oauth/session-token/expiration';
+		if ($('#external-logout').length > 0) {
 			$.getJSON(route)
 				.done((response) => {
 					const isSessionTokenExpired = new Date() >= new Date(response.expiresAt);
 					if (isSessionTokenExpired) {
-						$('[data-testid=external-logout]').addClass('disabled');
+						$('#external-logout').addClass('disabled');
 					} else {
-						$('[data-testid=external-logout]').removeClass('disabled');
+						$('#external-logout').removeClass('disabled');
 					}
 				})
 				.fail(() => {
-					$('[data-testid=external-logout]').addClass('disabled');
+					$('#external-logout').addClass('disabled');
 				});
 		}
 	});
