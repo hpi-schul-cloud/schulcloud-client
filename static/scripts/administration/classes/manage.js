242981f3f4394a3f91f4facbd12e924cc64a0510
dyedwiper <maximilian.becker@dataport.de>
2025-11-12 14:16:24 +0100

    Remove unused filter function for classes

--- static/scripts/administration/classes/manage.js
+++ static/scripts/administration/classes/manage.js
@@ -1,32 +1,4 @@
 window.addEventListener('load', () => {
-	document
-		.getElementById('filter_schoolyear')
-		.addEventListener('input', async (event) => {
-			// get filtered classes
-			const yearId = event.target.value;
-			const res = await fetch(
-				`/administration/classes/json?yearId=${yearId}`,
-				{ credentials: 'same-origin' },
-			);
-			const classes = await res.json();
-			const classInput = document.getElementById(
-				'student_from_class_import',
-			);
-
-			// update items
-			if (classes.total === 0) {
-				classInput.innerHTML = `<option value="" disabled>
-					${$t('administration.classes.text.noClassesInYear')}
-				</option>`;
-			} else {
-				classInput.innerHTML = `${classes.data
-					.map(
-						(item) => `<option value="${item._id}">${item.displayName}</option>`,
-					)
-					.join('')}`;
-			}
-		});
-
 	function btnSendLinksEmailsHandler(e) {
 		e.preventDefault();
 		const $this = $(this);
