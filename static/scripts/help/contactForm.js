adb57713af586ad1add4d1c28d7be31b889eebeb
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-12-10 14:29:50 +0100

    BC-9722 - Enhance contact forms: update form action attributes to use data-action and improve error handling

--- static/scripts/help/contactForm.js
+++ static/scripts/help/contactForm.js
@@ -1,26 +1,81 @@
+function showSuccessMessage(message) {
+	$.showNotification($t(message), 'success', 5000);
+}
+
+function showErrorMessage(message) {
+	$.showNotification($t(message), 'danger', 5000);
+}
+
+function showAJAXError(err) {
+	if (err.responseJSON) {
+		const { message } = err.responseJSON;
+		showErrorMessage(
+			`Here should go a very important error message: ${message}`
+		);
+	}
+}
+
+const reloadPage = (msg, timeout = 2000) => {
+	if (msg) {
+		showSuccessMessage(msg);
+	}
+	setTimeout(() => {
+		window.location.reload();
+	}, timeout);
+};
+
+// handle form submissions via AJAX
+function handleFormSubmit(form) {
+	form.addEventListener('submit', async (event) => {
+		event.preventDefault(); // Verhindert die Standard-Form-Übermittlung
+
+		const formData = new FormData(form);
+		const actionUrl = form.getAttribute('data-action');
+
+		$.ajax({
+			data: formData,
+			url: actionUrl,
+			type: 'POST',
+			processData: false,
+			contentType: false,
+			success: () => reloadPage(undefined, 0),
+			error: showAJAXError,
+		});
+	});
+}
+
 function initForm(formContainer) {
 	const teamForm = formContainer.querySelector('.team_form');
 	const wishForm = teamForm.querySelector('.wish_form');
 	const bugForm = teamForm.querySelector('.bug_form');
 
-
 	// handle form change when type changes
-	formContainer.querySelector('#message_type').addEventListener('change', (event) => {
-		const value = event.target.value || event.target.querySelector('[name=message_type]:checked').value;
-		if (value === 'wish') {
-			wishForm.classList.remove('hidden');
-			bugForm.classList.add('hidden');
-		} else {
-			wishForm.classList.add('hidden');
-			bugForm.classList.remove('hidden');
-		}
-	});
+	formContainer
+		.querySelector('#message_type')
+		.addEventListener('change', (event) => {
+			const value =
+				event.target.value ||
+				event.target.querySelector('[name=message_type]:checked').value;
+			if (value === 'wish') {
+				wishForm.classList.remove('hidden');
+				bugForm.classList.add('hidden');
+			} else {
+				wishForm.classList.add('hidden');
+				bugForm.classList.remove('hidden');
+			}
+		});
+
+	// AJAX-Handler für beide Forms aktivieren
+	console.log('init contact forms');
+	handleFormSubmit(wishForm);
+	console.log('init bug forms');
+	handleFormSubmit(bugForm);
+	console.log('contact forms initialized');
 }
 
 function init() {
 	document.querySelectorAll('.contact-form').forEach(initForm);
 }
-
 
 if (!window.contactForm) {
 	window.contactForm = init;
@@ -29,29 +84,42 @@ if (!window.contactForm) {
 
 // accessability radio buttons (keyboard navigation)
 document.querySelectorAll('label').forEach((label) => {
-	if (!label.getAttribute('for')) { return; }
+	if (!label.getAttribute('for')) {
+		return;
+	}
 	const input = document.getElementById(label.getAttribute('for'));
-	if (!input || input.getAttribute('type') !== 'radio') { return; }
+	if (!input || input.getAttribute('type') !== 'radio') {
+		return;
+	}
 	const fieldset = input.closest('fieldset');
 
-	label.addEventListener('keydown', (event) => {
-		if (input.getAttribute('disabled') !== null || input.getAttribute('readonly') !== null) {
-			return true;
-		}
-		// other than spacebar and enter see: https://webaim.org/techniques/keyboard/
-		if (event.keyCode !== 32 && event.keyCode !== 13) {
-			return true;
-		}
-		// check input
-		event.preventDefault();
-		event.stopPropagation();
-		input.checked = true;
+	label.addEventListener(
+		'keydown',
+		(event) => {
+			if (
+				input.getAttribute('disabled') !== null ||
+				input.getAttribute('readonly') !== null
+			) {
+				return true;
+			}
+			// other than spacebar and enter see: https://webaim.org/techniques/keyboard/
+			if (event.keyCode !== 32 && event.keyCode !== 13) {
+				return true;
+			}
+			// check input
+			event.preventDefault();
+			event.stopPropagation();
+			input.checked = true;
 
-		// trigger change event
-		if (fieldset) {
-			const newEvent = new CustomEvent('change', { target: fieldset });
-			fieldset.dispatchEvent(newEvent);
-		}
-		return false;
-	}, false);
+			// trigger change event
+			if (fieldset) {
+				const newEvent = new CustomEvent('change', {
+					target: fieldset,
+				});
+				fieldset.dispatchEvent(newEvent);
+			}
+			return false;
+		},
+		false
+	);
 });
