6320cde91f37e54fea4688d04201ed4d62ab985e
dyedwiper <maximilian.becker@dataport.de>
2025-12-15 11:28:19 +0100

    Adapt api helper to use axios

--- helpers/apiHelper.js
+++ helpers/apiHelper.js
@@ -1,6 +1,27 @@
-const rp = require('request-promise');
+const axios = require('axios');
 
-const api = (baseUrl, { keepAlive, xApiKey, timeout } = {}) => (req, { json = true, version = 'v1', accessToken } = {}) => {
+// The api was initially built with request-promise and used its method signatures.
+// To use axios we have to adapt the options.
+const adaptOptions = (options) => {
+	const adapted = { ...options };
+
+	if ('json' in adapted) {
+		adapted.data = adapted.json;
+		delete adapted.json;
+	} else if ('body' in adapted) {
+		adapted.data = adapted.body;
+		delete adapted.body;
+	}
+
+	if ('qs' in adapted) {
+		adapted.params = adapted.qs;
+		delete adapted.qs;
+	}
+
+	return adapted;
+};
+
+const api = (baseUrl, { keepAlive, xApiKey, timeout } = {}) => (req, { version = 'v1', accessToken } = {}) => {
 	const headers = {};
 
 	if (accessToken) {
@@ -17,16 +38,35 @@ const api = (baseUrl, { keepAlive, xApiKey, timeout } = {}) => (req, { json = tr
 		headers['x-api-key'] = xApiKey;
 	}
 
-	if (json === true) {
-		headers['Content-Type'] = 'application/json';
-	}
-
-	return rp.defaults({
-		baseUrl: new URL(version, baseUrl).href,
+	const axiosInstance = axios.create({
+		baseURL: new URL(version, baseUrl).href,
 		timeout,
-		json,
 		headers,
 	});
+
+	axiosInstance.interceptors.response.use((res) => res.data);
+
+	const methods = ['get', 'post', 'patch', 'put', 'delete'];
+
+	const apiInstance = {};
+
+	methods.forEach((method) => {
+		apiInstance[method] = (url, options) => {
+			const adaptedOptions = adaptOptions(options);
+
+			let methodHandler;
+
+			if (method === 'get' || method === 'delete') {
+				methodHandler = axiosInstance[method](url, adaptedOptions);
+			} else {
+				methodHandler = axiosInstance[method](url, adaptedOptions.data, adaptedOptions);
+			}
+
+			return methodHandler;
+		};
+	});
+
+	return apiInstance;
 };
 
 module.exports = api;
