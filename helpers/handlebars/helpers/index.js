85005d488da0eb28fc0eaf8028a00e1c7aa32ee6
Christian Darsow <hello@darsow.io>
2026-02-03 08:50:54 +0100

    Add bytes to MByte format helper

--- helpers/handlebars/helpers/index.js
+++ helpers/handlebars/helpers/index.js
@@ -301,6 +301,10 @@ const helpers = () => ({
 	},
 	isset: (value) => !!value,
 	getThumbnailIcon: (filename) => filesStorage.getThumbnailIcon(filename),
+	bytesToMB: (bytes) => {
+		const mb = bytes / 1024 / 1024;
+		return Math.round(mb * 10) / 10; // Round to 1 decimal place
+	},
 });
 
 module.exports = helpers;
