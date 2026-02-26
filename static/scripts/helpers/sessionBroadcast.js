c7d878cb92263c84119edffdd1afc3aa465e2e34
Sebastian Mueller <sebastian.mueller@dataport.de>
2026-02-26 10:10:31 +0100

    fix: apply code review suggestions

--- static/scripts/helpers/sessionBroadcast.js
+++ static/scripts/helpers/sessionBroadcast.js
@@ -3,3 +3,11 @@ export const broadcast = new BroadcastChannel('schulcloud-session');
 export const notifyLogout = () => {
 	broadcast.postMessage('logout');
 };
+
+export const subsscribeToLogoutBroadcast = () => {
+	broadcast.onmessage = (event) => {
+		if (event.data === 'logout') {
+			window.location.href = '/logout';
+		}
+	};
+};
