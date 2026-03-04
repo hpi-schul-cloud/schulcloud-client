b80ffa157fb133e87b5b723fa22d9b1f1475e76d
hoeppner.dataport <marcus.hoeppner@dataport.de>
2026-03-04 20:31:43 +0100

    chore: use constant names and comment - as in nuxt-client

--- static/scripts/helpers/sessionBroadcast.js
+++ static/scripts/helpers/sessionBroadcast.js
@@ -1,5 +1,22 @@
-export const broadcast = new BroadcastChannel('schulcloud-session');
+// these constants are also used inside of the nuxt-client to communicate logouts between both application parts
+const BROADCAST_CHANNEL_NAME = 'user-session-channel';
+const BROADCAST_MESSAGE_LOGOUT = 'logout';
+
+export const broadcast = new BroadcastChannel(BROADCAST_CHANNEL_NAME);
 
 export const notifyLogout = () => {
-	broadcast.postMessage('logout');
+	console.log('send logout message to other tabs');
+	broadcast.postMessage(BROADCAST_MESSAGE_LOGOUT);
+};
+
+broadcast.onmessage = (event) => {
+	console.log('Received message:', event.data);
+	if (event.data === BROADCAST_MESSAGE_LOGOUT) {
+		console.log('Received "logout" message, logging out...');
+		window.location.href = '/logout';
+	}
+	if (event.data === 'expired') {
+		console.log('Received "expired" message, showing auto logout modal...');
+		window.location.href = '/logout';
+	}
 };
