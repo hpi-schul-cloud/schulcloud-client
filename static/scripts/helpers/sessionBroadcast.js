f748b32abeb3c92ac764d6b3314a9247204601ba
hoeppner.dataport <marcus.hoeppner@dataport.de>
2026-03-06 13:57:43 +0100

    chore: remove logging

--- static/scripts/helpers/sessionBroadcast.js
+++ static/scripts/helpers/sessionBroadcast.js
@@ -5,18 +5,14 @@ const BROADCAST_MESSAGE_LOGOUT = 'logout';
 export const broadcast = new BroadcastChannel(BROADCAST_CHANNEL_NAME);
 
 export const notifyLogout = () => {
-	console.log('send logout message to other tabs');
 	broadcast.postMessage(BROADCAST_MESSAGE_LOGOUT);
 };
 
 broadcast.onmessage = (event) => {
-	console.log('Received message:', event.data);
 	if (event.data === BROADCAST_MESSAGE_LOGOUT) {
-		console.log('Received "logout" message, logging out...');
 		window.location.href = '/logout';
 	}
 	if (event.data === 'expired') {
-		console.log('Received "expired" message, showing auto logout modal...');
 		window.location.href = '/logout';
 	}
 };
