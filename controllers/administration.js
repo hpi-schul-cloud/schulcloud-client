56a44dbe2145f0b383a4e99d5122a6d4fb87a022
virgilchiriac <virgil.chiriac@dataport.de>
2026-08-04 16:38:44 +0200

    BC-12106 - fix classes administration school year

--- controllers/administration.js
+++ controllers/administration.js
@@ -1035,12 +1035,18 @@ const renderClassEdit = (req, res, next) => {
 			const mode = req.locals.mode;
 			Promise.all(promises).then(
 				([teachers, gradeLevels, currentClass]) => {
-					const schoolyears = getSelectableYears(res.locals.currentSchoolData);
+					const selectableSchoolYears = getSelectableYears(res.locals.currentSchoolData);
+					const selectableSchoolYearsIds = new Set(selectableSchoolYears.map((year) => year._id));
 
-					const allSchoolYears = res.locals.currentSchoolData.years.schoolYears
+					let schoolYears = res.locals.currentSchoolData.years.schoolYears
 						.sort((a, b) => b.startDate.localeCompare(a.startDate));
+					schoolYears = schoolYears.map((year) => {
+						if (!selectableSchoolYearsIds.has(year._id)) {
+							return { ...year, disabled: true };
+						}
+						return year;
+					});
 
-					const lastDefinedSchoolYearId = (allSchoolYears[0] || {})._id;
 					const isAdmin = res.locals.currentUser.permissions.includes(
 						'ADMIN_VIEW',
 					);
@@ -1073,7 +1079,7 @@ const renderClassEdit = (req, res, next) => {
 								g.selected = true;
 							}
 						});
-						schoolyears.forEach((schoolyear) => {
+						schoolYears.forEach((schoolyear) => {
 							if (currentClass.year === schoolyear._id) {
 								schoolyear.selected = true;
 							}
@@ -1089,7 +1095,8 @@ const renderClassEdit = (req, res, next) => {
 						}
 
 						if (currentClass.year) {
-							isUpgradable = (lastDefinedSchoolYearId !== (currentClass.year || {}))
+							isUpgradable = (res.locals.currentSchoolData.years.nextYear || {})._id !== currentClass.year
+								&& selectableSchoolYearsIds.has(currentClass.year)
 								&& currentClass.gradeLevel
 								&& currentClass.gradeLevel !== 13
 								&& !currentClass.successor;
@@ -1118,7 +1125,7 @@ const renderClassEdit = (req, res, next) => {
 						edit: mode !== 'create',
 						isUpgradable,
 						mode,
-						schoolyears,
+						schoolYears,
 						teachers,
 						class: currentClass,
 						gradeLevels,
