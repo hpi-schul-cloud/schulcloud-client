b10de97a5dd1da4aebfa6ca41e23875d8a3ef36c
hoeppner.dataport <marcus.hoeppner@dataport.de>
2026-08-17 16:32:44 +0200

    chore: trigger file list

--- static/scripts/download.js
+++ static/scripts/download.js
@@ -30,34 +30,15 @@ const archiveDownload = (requestBody) => {
 };
 
 const selectiveArchiveDownload = (requestBody) => {
-	const form = document.createElement('form');
-	form.method = 'GET';
-	form.action = '/api/v1/filestorage/files/archive/file-list';
-	form.target = '_blank';
-
-	const ownerIdInput = document.createElement('input');
-	ownerIdInput.type = 'hidden';
-	ownerIdInput.name = 'ownerId';
-	ownerIdInput.value = requestBody.ownerId;
-
-	const ownerTypeInput = document.createElement('input');
-	ownerTypeInput.type = 'hidden';
-	ownerTypeInput.name = 'ownerType';
-	ownerTypeInput.value = requestBody.ownerType;
-
-	const archiveNameInput = document.createElement('input');
-	archiveNameInput.type = 'hidden';
-	archiveNameInput.name = 'archiveName';
-	archiveNameInput.value = requestBody.archiveName;
-
-	form.appendChild(ownerIdInput);
-	form.appendChild(ownerTypeInput);
-	form.appendChild(archiveNameInput);
-
-	document.body.appendChild(form);
-	console.log('Submitting form:', form.action);
-	form.submit();
-	form.remove();
+	$.ajax({
+		url: '/api/v1/filestorage/files/archive/file-list';
+		method: 'GET',
+		contentType: 'application/json',
+	}).done((response) => {
+		console.log(response)
+	}).fail((xhr) => {
+		console.error(xhr.responseJSON?.message ? xhr.responseJSON?.message : 'error')
+	});
 };
 
 export { archiveDownload, selectiveArchiveDownload };
