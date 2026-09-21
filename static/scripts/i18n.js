535203e1bd269d8a379df8c7ff0e984a509f310b
Christian Darsow <christian.darsow@dataport.de>
2026-09-21 15:10:51 +0200

    Drop local storage handling

--- static/scripts/i18n.js
+++ static/scripts/i18n.js
@@ -1,34 +1,21 @@
 import i18next from 'i18next';
-import Backend from 'i18next-chained-backend';
-import LocalStorageBackend from 'i18next-localstorage-backend'; // primary use cache
-import Fetch from 'i18next-fetch-backend'; // fallback xhr load
-import pkg from '../../package.json';
+import Fetch from 'i18next-fetch-backend';
 
 const userLanguage = document.querySelector('html').getAttribute('lang');
+const localeVersionsElement = document.getElementById('locale-versions');
+const localeVersions = localeVersionsElement ? JSON.parse(localeVersionsElement.textContent) : {};
 
 // mock method. used until language keys are loaded. (not perfect, but at least it works for now)
 window.$t = (key) => key;
 
 i18next
-	.use(Backend)
+	.use(Fetch)
 	.init({
 		initImmediate: false,
 		lng: userLanguage,
 		fallbackLng: ['de', 'en'].filter((lng) => lng !== userLanguage),
 		backend: {
-			backends: [
-				LocalStorageBackend, // primary
-				Fetch, // fallback
-			],
-			backendOptions: [
-				{
-					prefix: 'i18next_res_',
-					defaultVersion: `v${pkg.version}`, // forces a language file reload on version change
-				},
-				{
-					loadPath: '/locales/{{lng}}.json',
-				},
-			],
+			loadPath: (lng) => localeVersions[lng] || `/locales/${lng}.json`,
 		},
 	})
 	.then(() => {
