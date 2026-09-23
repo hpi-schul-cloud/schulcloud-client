089604fcb51310b061553b80ad17f22af210a538
Christian Darsow <christian.darsow@dataport.de>
2026-09-23 14:44:04 +0200

    back to old way

--- static/scripts/i18n.js
+++ static/scripts/i18n.js
@@ -3,6 +3,8 @@ import Fetch from 'i18next-fetch-backend';
 
 const userLanguage = document.querySelector('html').getAttribute('lang');
 
+window.$t = (...args) => i18next.t(...args);
+
 const resources = {};
 if (window.i18nLocaleData) {
 	resources[userLanguage] = { translation: window.i18nLocaleData };
@@ -18,7 +20,4 @@ i18next
 		backend: {
 			loadPath: (lng) => `/locales/${lng}.json`,
 		},
-	})
-	.then(() => {
-		window.$t = (...args) => i18next.t(...args);
 	});
